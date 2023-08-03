ab497896dbfd752931646a9e1a6fc5f2139d7525
trentmc <gtrent@gmail.com>
2023-08-03 11:26:18 +0300

    Towards #13: add unit tests

---
+++ pdr_backend/utils/test/test_constants.py
@@ -0,0 +1,15 @@
+from pdr_backend.utils.constants import (
+    ZERO_ADDRESS, 
+    SAPPHIRE_TESTNET_RPC,
+    SAPPHIRE_TESTNET_CHAINID,
+    SAPPHIRE_MAINNET_RPC,
+    SAPPHIRE_MAINNET_CHAINID,
+    )
+
+def test_constants1():
+    assert ZERO_ADDRESS[:3] == "0x0"
+    assert "https://" in SAPPHIRE_TESTNET_RPC
+    assert isinstance(SAPPHIRE_TESTNET_CHAINID, int)
+    assert "https://" in SAPPHIRE_MAINNET_RPC
+    assert isinstance(SAPPHIRE_MAINNET_CHAINID, int)
+
