bed7a4321b1c4a10966ade4fd96998505c08a7cf
trizin <25263018+trizin@users.noreply.github.com>
2023-08-03 08:47:45 +0000

    Add entrypoint script

---
+++ entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+MODULE_NAME=$1
+
+if [ -z "$MODULE_NAME" ]
+then
+    echo "No module specified. Please provide a module name as an argument."
+    exit 1
+fi
+
+if [ ! -d "/app/pdr_backend/$MODULE_NAME" ]
+then
+    echo "Module $MODULE_NAME does not exist."
+    exit 1
+fi
+
+echo "Running main.py for $MODULE_NAME..."
+python /app/pdr_backend/$MODULE_NAME/main.py
