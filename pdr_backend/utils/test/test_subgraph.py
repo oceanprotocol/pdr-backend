cda739f9ea64638fd202cb8e953296d0006bd232
trentmc <gtrent@gmail.com>
2023-08-03 12:08:21 +0300

    utils test_subgraph on happy _and_ bad paths

--- pdr_backend/utils/test/test_subgraph.py
+++ pdr_backend/utils/test/test_subgraph.py
@@ -1,29 +1,33 @@
 import requests
 
+import pytest
+
 from pdr_backend.utils.subgraph import \
     query_subgraph, get_all_interesting_prediction_contracts
 
-def test_subgraph1(monkeypatch):
-    class MockResponse:
-        @property
-        def status_code(self):
-            return 200
+class MockResponse:
+    def __init__(self, status_code):
+        self.status_code = status_code
 
-        @staticmethod
-        def json():
-            return {"mock_key": "mock_response"}
+    @staticmethod
+    def json():
+        return {"mock_key": "mock_response"}
 
-    def mock_post(*args, **kwargs):
-        return MockResponse()
+def mock_post_happy(*args, **kwargs):
+    return MockResponse(status_code=200)
 
-    # apply the monkeypatch for requests.post to mock_post
-    monkeypatch.setattr(requests, "post", mock_post)
-    
-    subgraph_url = "foo"
-    query = "bar"
-    result = query_subgraph(subgraph_url, query)
+def mock_post_bad(*args, **kwargs):
+    return MockResponse(status_code=400)
 
+def test_query_subgraph_happy(monkeypatch):
+    monkeypatch.setattr(requests, "post", mock_post_happy)
+    result = query_subgraph(subgraph_url="foo", query="bar")
     assert result == {"mock_key": "mock_response"}
+
+def test_query_subgraph_bad(monkeypatch):
+    monkeypatch.setattr(requests, "post", mock_post_bad)
+    with pytest.raises(Exception):
+        query_subgraph(subgraph_url="foo", query="bar")
 
 def test_get_all_interesting_prediction_contracts():
     pass
