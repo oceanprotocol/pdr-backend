b53781b149a283ab321692bc73e91dbdc6eb3c22
trentmc <gtrent@gmail.com>
2023-08-04 09:24:28 +0300

    test is_sapphire_network

--- pdr_backend/utils/test/test_contract.py
+++ pdr_backend/utils/test/test_contract.py
@@ -1,5 +1,20 @@
-from pdr_backend.utils.contract import Web3Config, Token, PredictoorContract, FixedRate, get_contract_filename
 
-def test_contract1():
-    pass
+from pdr_backend.utils.constants import (
+    SAPPHIRE_TESTNET_CHAINID,
+    SAPPHIRE_MAINNET_CHAINID,
+)
+from pdr_backend.utils.contract import (
+    is_sapphire_network,
+    send_encrypted_tx,
+    Web3Config,
+    Token,
+    PredictoorContract,
+    FixedRate,
+    get_contract_filename,
+    )
+
+def test_is_sapphire_network():
+    assert not is_sapphire_network(0)
+    assert is_sapphire_network(SAPPHIRE_TESTNET_CHAINID)
+    assert is_sapphire_network(SAPPHIRE_MAINNET_CHAINID)
 
