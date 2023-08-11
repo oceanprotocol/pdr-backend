0eae054869cbd83f0d006439db1c3ff303e06e2a
trizin <25263018+trizin@users.noreply.github.com>
2023-08-11 14:06:04 +0300

    Add main.py

---
+++ pdr_backend/publisher/main.py
@@ -0,0 +1,38 @@
+from pdr_backend.publisher.publish import publish
+
+publish(
+    s_per_epoch= 300,
+    s_per_subscription= 60 * 60 * 24,
+    base= "ETH",
+    quote= "USDT",
+    source= "kraken",
+    trueval_submiter_addr= "0xe2DD09d719Da89e5a3D0F2549c7E24566e947260",
+    feeCollector_addr= "0xe2DD09d719Da89e5a3D0F2549c7E24566e947260",
+    rate= 3,
+    cut= 0.2
+)
+
+publish(
+    s_per_epoch= 300,
+    s_per_subscription= 60 * 60 * 24,
+    base= "BTC",
+    quote= "TUSD",
+    source= "binance",
+    trueval_submiter_addr= "0xe2DD09d719Da89e5a3D0F2549c7E24566e947260",
+    feeCollector_addr= "0xe2DD09d719Da89e5a3D0F2549c7E24566e947260",
+    rate= 3,
+    cut= 0.2
+)
+
+
+publish(
+    s_per_epoch= 300,
+    s_per_subscription= 60 * 60 * 24,
+    base= "XRP",
+    quote= "USDT",
+    source= "binance",
+    trueval_submiter_addr= "0xe2DD09d719Da89e5a3D0F2549c7E24566e947260",
+    feeCollector_addr= "0xe2DD09d719Da89e5a3D0F2549c7E24566e947260",
+    rate= 3,
+    cut= 0.2
+)
