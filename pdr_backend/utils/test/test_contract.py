c86a7d66cd7e5f90c4536b047feb269efa4d6b63
trizin <25263018+trizin@users.noreply.github.com>
2023-08-18 07:11:03 +0000

    Update tests

--- pdr_backend/utils/test/test_contract.py
+++ pdr_backend/utils/test/test_contract.py
@@ -17,10 +17,6 @@ from pdr_backend.utils.constants import (
 )
 
 
-TEST_RPC_URL = "http://127.0.0.1:8545"
-TEST_PRIVATE_KEY = "0xc594c6e5def4bab63ac29eed19a134c130388f74f019bc74b8f4389df2837a58"
-DEVELOPMENT_CHAIN_ID = 8996
-
 @enforce_types
 def test_is_sapphire_network():
     assert not is_sapphire_network(0)
@@ -35,20 +31,20 @@ def test_send_encrypted_tx():
 
 
 @enforce_types
-def test_Web3Config_bad_rpc():
+def test_Web3Config_bad_rpc(private_key):
     with pytest.raises(ValueError):
-        Web3Config(rpc_url=None, private_key=TEST_PRIVATE_KEY)
+        Web3Config(rpc_url=None, private_key=private_key)
 
 
 @enforce_types
-def test_Web3Config_bad_key():
+def test_Web3Config_bad_key(rpc_url):
     with pytest.raises(ValueError):
-        Web3Config(rpc_url=TEST_RPC_URL, private_key="foo")
+        Web3Config(rpc_url=rpc_url, private_key="foo")
 
 
 @enforce_types
-def test_Web3Config_happy_noPrivateKey():
-    c = Web3Config(rpc_url=TEST_RPC_URL, private_key=None)
+def test_Web3Config_happy_noPrivateKey(rpc_url):
+    c = Web3Config(rpc_url=rpc_url, private_key=None)
 
     assert c.w3 is not None
     assert not hasattr(c, "account")
@@ -57,27 +53,26 @@ def test_Web3Config_happy_noPrivateKey():
 
 
 @enforce_types
-def test_Web3Config_happy_havePrivateKey_noKeywords():
-    c = Web3Config(TEST_RPC_URL, TEST_PRIVATE_KEY)
+def test_Web3Config_happy_havePrivateKey_noKeywords(rpc_url, private_key):
+    c = Web3Config(rpc_url, private_key)
     assert c.account
     assert c.owner == c.account.address
-    assert c.private_key == TEST_PRIVATE_KEY
+    assert c.private_key == private_key
 
 
 @enforce_types
-def test_Web3Config_happy_havePrivateKey_withKeywords():
-    c = Web3Config(rpc_url=TEST_RPC_URL, private_key=TEST_PRIVATE_KEY)
+def test_Web3Config_happy_havePrivateKey_withKeywords(rpc_url, private_key):
+    c = Web3Config(rpc_url=rpc_url, private_key=private_key)
     assert c.account
     assert c.owner == c.account.address
-    assert c.private_key == TEST_PRIVATE_KEY
+    assert c.private_key == private_key
 
 
 @enforce_types
-def test_Token():
-    config = Web3Config(TEST_RPC_URL, TEST_PRIVATE_KEY)
-    token_address = get_address(DEVELOPMENT_CHAIN_ID, "Ocean")
+def test_Token(rpc_url, private_key, chain_id):
+    config = Web3Config(rpc_url, private_key)
+    token_address = get_address(chain_id, "Ocean")
     token = Token(config, token_address)
-
 
     accounts = config.w3.eth.accounts
     owner_addr = config.owner
@@ -86,17 +81,16 @@ def test_Token():
     token.contract_instance.functions.mint(owner_addr, 1000000000).transact({"from": owner_addr, "gasPrice": config.w3.eth.gas_price})
 
     allowance_start = token.allowance(owner_addr, alice)
-    assert allowance_start == 0
-
-    token.approve(alice, 100)
-
+    token.approve(alice, 100, True)
     allowance_end = token.allowance(owner_addr, alice)
-    assert allowance_end == 100
+    assert allowance_end - allowance_start == 100
 
     balance_start = token.balanceOf(alice)
     token.transfer(alice, 100, owner_addr)
     balance_end = token.balanceOf(alice)
     assert balance_end - balance_start == 100
 
-
-    
+@pytest.fixture(autouse=True)
+def run_before_each_test():
+    # This setup code will be run before each test
+    print("Setting up!")
