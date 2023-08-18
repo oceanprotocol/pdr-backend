dbd442d4ae8871ba10d532f6e0d7e4bc48706b8d
trizin <25263018+trizin@users.noreply.github.com>
2023-08-18 09:22:36 +0000

    Add more tests!

--- pdr_backend/utils/test/test_contract.py
+++ pdr_backend/utils/test/test_contract.py
@@ -3,6 +3,7 @@ import time
 from pdr_backend.publisher.publish import publish
 from enforce_typing import enforce_types
 import pytest
+from pytest import approx
 
 from pdr_backend.utils.contract import (
     is_sapphire_network,
@@ -126,12 +127,84 @@ def test_buy_many(predictoor_contract):
     receipts = predictoor_contract.buy_many(2, None, True)
     assert len(receipts) == 2
     
+def test_get_exchanges(predictoor_contract):
+    exchanges = predictoor_contract.get_exchanges()
+    assert exchanges[0][0].startswith("0x")
+
+def test_get_stake_token(predictoor_contract, web3_config):
+    stake_token = predictoor_contract.get_stake_token()
+    ocean_address = get_address(web3_config.w3.eth.chain_id, "Ocean")
+    assert stake_token == ocean_address
+    
+def test_get_price(predictoor_contract):
+    price = predictoor_contract.get_price()
+    assert price / 1e18 == approx(3.603)
+
+def test_get_current_epoch(predictoor_contract):
+    current_epoch = predictoor_contract.get_current_epoch()
+    utcnow = int(time.time())
+    assert current_epoch == int(utcnow // 300)
+
+def test_get_current_epoch_ts(predictoor_contract):
+    current_epoch = predictoor_contract.get_current_epoch()
+    utcnow = int(time.time())
+    assert current_epoch == int(utcnow // 300) * 300
+
+def test_get_seconds_per_epoch(predictoor_contract):
+    seconds_per_epoch = predictoor_contract.get_secondsPerEpoch()
+    assert seconds_per_epoch == 300
+
+def test_get_aggpredval(predictoor_contract):
+    current_epoch = predictoor_contract.get_current_epoch_ts()
+    aggpredval = predictoor_contract.get_agg_predval(current_epoch)
+    assert aggpredval == 0
+
+def test_soonest_timestamp_to_predict(predictoor_contract):
+    current_epoch = predictoor_contract.get_current_epoch_ts()
+    soonest_timestamp = predictoor_contract.soonest_timestamp_to_predict(current_epoch)
+    assert soonest_timestamp == current_epoch + 300 * 2
+
+def test_get_trueValSubmitTimeout(predictoor_contract):
+    trueValSubmitTimeout = predictoor_contract.get_trueValSubmitTimeout()
+    assert trueValSubmitTimeout == 4 * 12 * 300
+
+def test_get_block(predictoor_contract):
+    block = predictoor_contract.get_block(0)
+    assert block.number == 0
+
+def test_submit_prediction_aggpredval_payout(predictoor_contract):
+    current_epoch = predictoor_contract.get_current_epoch_ts()
+    soonest_timestamp = predictoor_contract.soonest_timestamp_to_predict(current_epoch)
+    receipt = predictoor_contract.submit_prediction(True, 1, soonest_timestamp, True)
+    assert receipt["status"] == 1
+
+    prediction = predictoor_contract.get_prediction(soonest_timestamp)
+    assert prediction[0] == True
+    assert prediction[1] == 1
+
+    predictoor_contract.config.w3.provider.make_request(
+        "evm_increaseTime", [300 * 2]
+    )
+
+    receipt = predictoor_contract.submit_trueval(
+        True, soonest_timestamp, 0, False, True
+    )
+    assert receipt["status"] == 1
+
+    receipt = predictoor_contract.payout(soonest_timestamp, True)
+    assert receipt["status"] == 1
+
+def test_redeem_unused_slot_revenue(predictoor_contract):
+    current_epoch = predictoor_contract.get_current_epoch_ts()
+    receipt = predictoor_contract.redeem_unused_slot_revenue(current_epoch)
+    assert receipt["status"] == 1
+
 @pytest.mark.parametrize(
     "input_data,expected_output",
     [
         ("short", b"short" + b"0" * 27),
-        ("this is exactly 32 chars", b"this is exactly 32 chars"),
-        ("this is a very long string which is more than 32 chars", b"this is a very long string wh"),
+        ("this is exactly 32 chars", b"this is exactly 32 chars00000000"),
+        ("this is a very long string which is more than 32 chars", b"this is a very long string which"),
     ],
 )
 def test_string_to_bytes32(input_data, expected_output, predictoor_contract):
