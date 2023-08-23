18a785e5518a189b7e6a17c10e9fbd01cf3982ec
trentmc <gtrent@gmail.com>
2023-08-23 17:42:14 +0200

    Fix #77 - Refactor: shrink "env" module and its usage

--- pdr_backend/utils/env.py
+++ pdr_backend/utils/env.py
@@ -1,13 +1,10 @@
 import os
+from os import getenv
 import sys
 
 
-def get_envvar(envvar_name: str, default=None) -> str:
-    return os.environ.get(envvar_name, default)
-
-
 def get_envvar_or_exit(envvar_name: str) -> str:
-    value = get_envvar(envvar_name)
+    value = getenv(envvar_name)
     if value == None:
         print(f"You must set {envvar_name} environment variable")
         sys.exit(1)
@@ -27,16 +24,16 @@ def get_private_key_or_exit() -> str:
 
 
 def get_pair_filter() -> str:
-    return get_envvar("PAIR_FILTER")
+    return getenv("PAIR_FILTER")
 
 
 def get_timeframe_filter() -> str:
-    return get_envvar("TIMEFRAME_FILTER")
+    return getenv("TIMEFRAME_FILTER")
 
 
 def get_source_filter() -> str:
-    return get_envvar("SOURCE_FILTER")
+    return getenv("SOURCE_FILTER")
 
 
 def get_owner_addresses() -> str:
-    return get_envvar("OWNER_ADDRS")
+    return getenv("OWNER_ADDRS")
