a2bcb370278184ed6c6f49c34c2d488ce854df89
trizin <25263018+trizin@users.noreply.github.com>
2023-08-25 09:25:18 +0000

    Add live tests

--- pdr_backend/trueval/test/test_trueval.py
+++ pdr_backend/trueval/test/test_trueval.py
@@ -36,6 +36,39 @@ def test_get_trueval_success():
         result = get_true_val(contract, 1, 2)
         assert result == (True, False)  # 1st True because 200 > 100
 
+def test_get_trueval_live_lowercase_slash():
+    contract = ContractData(
+        name="ETH-USDT",
+        address="0x1",
+        symbol="ETH-USDT",
+        seconds_per_epoch=60,
+        seconds_per_subscription=500,
+        pair="btc/usdt",
+        source="kraken",
+        timeframe="5m",
+        trueval_submit_timeout=100,
+        owner="0xowner",
+    )
+
+    result = get_true_val(contract, 1692943200, 1692943500)
+    assert result == (True, False)
+
+def test_get_trueval_live_lowercase_dash():
+    contract = ContractData(
+        name="ETH-USDT",
+        address="0x1",
+        symbol="ETH-USDT",
+        seconds_per_epoch=60,
+        seconds_per_subscription=500,
+        pair="btc-usdt",
+        source="kraken",
+        timeframe="5m",
+        trueval_submit_timeout=100,
+        owner="0xowner",
+    )
+
+    result = get_true_val(contract, 1692943200, 1692943500)
+    assert result == (True, False)
 
 def test_get_trueval_fail():
     contract = ContractData(
