7e6682eab7c8d553408a7d7d13f28b96aa6c70c6
trizin <25263018+trizin@users.noreply.github.com>
2023-08-25 23:06:50 +0000

    Formatting

--- pdr_backend/predictoor/main.py
+++ pdr_backend/predictoor/main.py
@@ -2,6 +2,7 @@ import sys
 from pdr_backend.predictoor.approach1.main import main as main1
 from pdr_backend.predictoor.approach2.main import main as main2
 
+
 def main():
     if len(sys.argv) == 1:
         main1()
@@ -13,6 +14,7 @@ def main():
             main2()
         else:
             print("Invalid argument. Use '1' for approach1 or '2' for approach2.")
+
 
 if __name__ == "__main__":
     main()
