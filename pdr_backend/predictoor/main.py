d3a302d79b0d9a528b4e0d992ebc07896ae2712f
trizin <25263018+trizin@users.noreply.github.com>
2023-08-26 08:57:26 +0300

    Fix #92: Have unified way to call approach 1 vs 2 (PR #94)

---
+++ pdr_backend/predictoor/main.py
@@ -0,0 +1,20 @@
+import sys
+from pdr_backend.predictoor.approach1.main import main as main1
+from pdr_backend.predictoor.approach2.main import main as main2
+
+
+def main():
+    if len(sys.argv) == 1:
+        main1()
+    else:
+        arg = sys.argv[1]
+        if arg == "1":
+            main1()
+        elif arg == "2":
+            main2()
+        else:
+            print("Invalid argument. Use '1' for approach1 or '2' for approach2.")
+
+
+if __name__ == "__main__":
+    main()
