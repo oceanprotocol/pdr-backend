074c0e9e1dc95e56fe499f58f93a430757c4cdab
trentmc <gtrent@gmail.com>
2023-08-28 10:01:54 +0200

    Fix #99: MODELDIR error in CLI

--- pdr_backend/predictoor/main.py
+++ pdr_backend/predictoor/main.py
@@ -1,20 +1,36 @@
 import sys
-from pdr_backend.predictoor.approach1.main import main as main1
-from pdr_backend.predictoor.approach2.main import main as main2
 
+HELP = """Predictoor runner.
 
-def main():
-    if len(sys.argv) == 1:
-        main1()
-    else:
-        arg = sys.argv[1]
-        if arg == "1":
-            main1()
-        elif arg == "2":
-            main2()
-        else:
-            print("Invalid argument. Use '1' for approach1 or '2' for approach2.")
+Usage: python pdr_backend/predictoor/main.py APPROACH
 
+       where APPROACH=1 does random predictions
+             APPROACH=2 uses a model to predict. Needs MODELDIR specified.
+"""
+
+def _do_help():
+    print(HELP)
+    sys.exit()
+
+def _do_main():
+    if len(sys.argv) <= 1: 
+        _do_help()
+
+    approach = sys.argv[1]
+    match approach:
+        case "1":
+            from pdr_backend.predictoor.approach1.main import main
+            main()
+            
+        case "2":
+            from pdr_backend.predictoor.approach2.main import main
+            main()
+            
+        case "help":
+            _do_help()
+            
+        case _:
+            _do_help()
 
 if __name__ == "__main__":
-    main()
+    _do_main()
