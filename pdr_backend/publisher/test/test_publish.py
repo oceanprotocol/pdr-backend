73dca720ff966cd238c4c1e89004b80ec786c3f6
Calina Cenan <calina@cenan.net>
2023-08-28 05:14:21 +0000

    Fix failing type tests after merge.

--- pdr_backend/publisher/test/test_publish.py
+++ pdr_backend/publisher/test/test_publish.py
@@ -1,5 +1,8 @@
 from unittest.mock import Mock, call
 
+from eth_account import Account
+
+from pdr_backend.models.token import Token
 from pdr_backend.publisher.publish import fund_dev_accounts
 
 
@@ -8,18 +11,19 @@ def test_fund_dev_accounts(monkeypatch):
     monkeypatch.setenv("PREDICTOOR_PRIVATE_KEY", pk)
     monkeypatch.setenv("PREDICTOOR2_PRIVATE_KEY", pk)
 
-    mock_token = Mock()
+    mock_token = Mock(spec=Token)
+    mock_account = Mock(spec=Account)
 
     accounts_to_fund = [
         ("PREDICTOOR_PRIVATE_KEY", 2000),
         ("PREDICTOOR2_PRIVATE_KEY", 3000),
     ]
 
-    fund_dev_accounts(accounts_to_fund, None, mock_token)
+    fund_dev_accounts(accounts_to_fund, mock_account, mock_token)
 
     mock_token.transfer.assert_has_calls(
         [
-            call("0xBE5449a6A97aD46c8558A3356267Ee5D2731ab5e", 2e21, None),
-            call("0xBE5449a6A97aD46c8558A3356267Ee5D2731ab5e", 3e21, None),
+            call("0xBE5449a6A97aD46c8558A3356267Ee5D2731ab5e", 2e21, mock_account),
+            call("0xBE5449a6A97aD46c8558A3356267Ee5D2731ab5e", 3e21, mock_account),
         ]
     )
