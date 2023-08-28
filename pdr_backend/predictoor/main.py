8ef3d9b4b9c3960c42a3dfb0e2637f9aa00be6e0
trentmc <gtrent@gmail.com>
2023-08-28 10:30:12 +0200

    make mypy happy

--- pdr_backend/predictoor/main.py
+++ pdr_backend/predictoor/main.py
@@ -8,29 +8,38 @@ Usage: python pdr_backend/predictoor/main.py APPROACH
              APPROACH=2 uses a model to predict. Needs MODELDIR specified.
 """
 
+
 def _do_help():
     print(HELP)
     sys.exit()
 
+
 def _do_main():
-    if len(sys.argv) <= 1: 
+    if len(sys.argv) <= 1:
         _do_help()
 
     approach = sys.argv[1]
     match approach:
         case "1":
-            from pdr_backend.predictoor.approach1.main import main
+            from pdr_backend.predictoor.approach1.main import (  # pylint: disable=import-outside-toplevel,line-too-long
+                main,
+            )
+
             main()
-            
+
         case "2":
-            from pdr_backend.predictoor.approach2.main import main
+            from pdr_backend.predictoor.approach2.main import (  # pylint: disable=import-outside-toplevel,line-too-long
+                main,
+            )
+
             main()
-            
+
         case "help":
             _do_help()
-            
+
         case _:
             _do_help()
+
 
 if __name__ == "__main__":
     _do_main()
