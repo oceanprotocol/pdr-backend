03ce7030a749d1c99882ee031b81f74f7a2ac5b8
trizin <25263018+trizin@users.noreply.github.com>
2023-09-01 12:22:32 +0000

    Add test for second approach main

--- pdr_backend/trueval/test/test_trueval_main.py
+++ pdr_backend/trueval/test/test_trueval_main.py
@@ -1,19 +1,18 @@
 from unittest.mock import patch, MagicMock
 from pdr_backend.trueval.main import TruevalAgent, main
+from pdr_backend.trueval.trueval_agent_batch import TruevalAgentBatch
 from pdr_backend.trueval.trueval_config import TruevalConfig
+from pdr_backend.util.constants import ZERO_ADDRESS
 
 
-def test_trueval_main(slot):
-    mocked_env = {
-        "SLEEP_TIME": "1",
-        "BATCH_SIZE": "1",
-    }
-
+def test_trueval_main_1(slot):
     mocked_web3_config = MagicMock()
 
-    with patch.dict("os.environ", mocked_env), patch(
+    with patch(
         "pdr_backend.models.base_config.Web3Config", return_value=mocked_web3_config
-    ), patch("time.sleep"), patch.object(
+    ), patch("time.sleep"), patch(
+        "pdr_backend.trueval.main.sys.argv", [0, "1"]
+    ), patch.object(
         TruevalConfig, "get_pending_slots", return_value=[slot]
     ), patch.object(
         TruevalAgent, "process_slot"
@@ -21,3 +20,20 @@ def test_trueval_main(slot):
         main(True)
 
     ps_mock.assert_called_once_with(slot)
+
+
+def test_trueval_main_2():
+    mocked_web3_config = MagicMock()
+
+    with patch(
+        "pdr_backend.models.base_config.Web3Config", return_value=mocked_web3_config
+    ), patch("time.sleep"), patch(
+        "pdr_backend.trueval.main.get_address", return_value=ZERO_ADDRESS
+    ), patch(
+        "pdr_backend.trueval.main.sys.argv", [0, "2"]
+    ), patch.object(
+        TruevalAgentBatch, "take_step"
+    ) as ts_mock:
+        main(True)
+
+    ts_mock.assert_called_once()
