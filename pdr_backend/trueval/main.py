cfa55c04a4635fa61d373efa9469949cd4986621
trizin <25263018+trizin@users.noreply.github.com>
2023-09-01 12:22:57 +0000

    Parse arguments and divide main.py into two approachs

--- pdr_backend/trueval/main.py
+++ pdr_backend/trueval/main.py
@@ -1,15 +1,45 @@
-from pdr_backend.models.predictoor_contract import PredictoorContract
+import sys
 from pdr_backend.trueval.trueval_agent_batch import TruevalAgentBatch
 from pdr_backend.trueval.trueval_config import TruevalConfig
 from pdr_backend.trueval.trueval_agent import TruevalAgent, get_trueval
 from pdr_backend.util.contract import get_address
 
 
+HELP = """Trueval runner.
+
+Usage: python pdr_backend/trueval/main.py APPROACH
+
+       where APPROACH=1 submits truevals one by one
+             APPROACH=2 submits truevals in a batch
+"""
+
+
+def do_help():
+    print(HELP)
+    sys.exit()
+
+
 def main(testing=False):
+    if len(sys.argv) <= 1:
+        do_help()
+    arg1 = sys.argv[1]
     config = TruevalConfig()
-    predictoor_batcher_addr = get_address("PredictoorHelper")
-    t = TruevalAgentBatch(config, get_trueval, predictoor_batcher_addr)
-    t.run(testing)
+
+    if arg1 == "1":
+        t = TruevalAgent(config, get_trueval)
+        t.run(testing)
+
+    elif arg1 == "2":
+        predictoor_batcher_addr = get_address(
+            config.web3_config.w3.eth.chain_id, "PredictoorHelper"
+        )
+        t = TruevalAgentBatch(config, get_trueval, predictoor_batcher_addr)
+        t.run(testing)
+
+    elif arg1 == "help":
+        do_help()
+    else:
+        do_help()
 
 
 if __name__ == "__main__":
