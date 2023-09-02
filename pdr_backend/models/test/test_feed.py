90afb0ef7e88580bd40f8b3ec646545c719bd70f
trentmc <gtrent@gmail.com>
2023-09-02 12:52:23 +0200

    Feed base() and quote() could only handle - but not /. Fixed it.

--- pdr_backend/models/test/test_feed.py
+++ pdr_backend/models/test/test_feed.py
@@ -12,21 +12,21 @@ def test_feed__construct_directly():
         300,
         60,
         "0xowner",
-        "BTC-ETH",
+        "BTC-USDT",
         "1h",
         "binance",
     )
 
     assert feed.name == "Contract Name"
     assert feed.address == "0x12345"
-    assert feed.symbol == "test"
+    assert feed.symbol == "SYM:TEST"
     assert feed.seconds_per_epoch == 300
     assert feed.seconds_per_subscription == 60
     assert feed.owner == "0xowner"
-    assert feed.pair == "BTC-ETH"
+    assert feed.pair == "BTC-USDT"
     assert feed.timeframe == "1h"
     assert feed.source == "binance"
-    assert feed.quote == "ETH"
+    assert feed.quote == "USDT"
     assert feed.base == "BTC"
 
 
@@ -35,11 +35,11 @@ def test_feed__construct_via_dictToFeed():
     feed_dict = {
         "name": "Contract Name",
         "address": "0x12345",
-        "symbol": "test",
+        "symbol": "SYM:TEST",
         "seconds_per_epoch": 300,
         "seconds_per_subscription": 60,
         "owner": "0xowner",
-        "pair": "BTC-ETH",
+        "pair": "BTC-USDT",
         "timeframe": "1h",
         "source": "binance",
     }
@@ -48,12 +48,18 @@ def test_feed__construct_via_dictToFeed():
 
     assert feed.name == "Contract Name"
     assert feed.address == "0x12345"
-    assert feed.symbol == "test"
+    assert feed.symbol == "SYM:TEST"
     assert feed.seconds_per_epoch == 300
     assert feed.seconds_per_subscription == 60
     assert feed.owner == "0xowner"
-    assert feed.pair == "BTC-ETH"
+    assert feed.pair == "BTC-USDT"
     assert feed.timeframe == "1h"
     assert feed.source == "binance"
-    assert feed.quote == "ETH"
     assert feed.base == "BTC"
+    assert feed.quote == "USDT"
+
+    # test pair with "/" (versus "-")
+    feed_dict["pair"] = "BTC/USDT"
+    feed = dictToFeed(feed_dict)
+    assert feed.base == "BTC"
+    assert feed.quote == "USDT"
