fd63aed56e266218605774e42c07d54f979aa528
trizin <25263018+trizin@users.noreply.github.com>
2023-09-04 09:23:01 +0000

    Prettify and enforce types

--- pdr_backend/predictoor/payout.py
+++ pdr_backend/predictoor/payout.py
@@ -1,27 +1,26 @@
 import os
-from typing import List
+from typing import Any, List
+from enforce_typing import enforce_types
 from pdr_backend.models.base_config import BaseConfig
 
 from pdr_backend.models.predictoor_contract import PredictoorContract
 from pdr_backend.util.subgraph import query_pending_payouts
 
 
-def request_payout(predictoor_contract: PredictoorContract, timestamps: List[int]):
-    predictoor_contract.payout_multiple(timestamps, True)
-
-
-def batchify(data, batch_size):
+@enforce_types
+def batchify(data: List[Any], batch_size: int):
     return [data[i : i + batch_size] for i in range(0, len(data), batch_size)]
 
 
+@enforce_types
 def request_payout_batches(
     predictoor_contract: PredictoorContract, batch_size: int, timestamps: List[int]
 ):
     batches = batchify(timestamps, batch_size)
     for batch in batches:
-        request_payout(predictoor_contract, batch)
+        predictoor_contract.payout_multiple(batch, True)
         print(".", end="", flush=True)
-    print("Batch completed")
+    print("\nBatch completed")
 
 
 def do_payout():
