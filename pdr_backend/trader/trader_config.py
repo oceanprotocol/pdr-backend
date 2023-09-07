5ad5bc4b9d31f6ea8e383ca142a17da01d013f9d
Călina Cenan <calina@cenan.net>
2023-09-07 14:20:50 +0300

    Fix #136 - Refactor and test trader (#113)

---
+++ pdr_backend/trader/trader_config.py
@@ -0,0 +1,13 @@
+from os import getenv
+
+from enforce_typing import enforce_types
+
+from pdr_backend.models.base_config import BaseConfig
+
+
+@enforce_types
+class TraderConfig(BaseConfig):
+    def __init__(self):
+        super().__init__()
+
+        self.trader_min_buffer = int(getenv("TRADER_MIN_BUFFER", "60 "))
