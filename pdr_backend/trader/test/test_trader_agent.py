4232bf621eb92c231d3bbcd69d8708889bc72cf9
trizin <25263018+trizin@users.noreply.github.com>
2023-09-07 09:50:24 +0000

    Fix tests

--- pdr_backend/trader/test/test_trader_agent.py
+++ pdr_backend/trader/test/test_trader_agent.py
@@ -61,14 +61,13 @@ def custom_trader(feed, prediction):
 
 def test_process_block_at_feed():
     trader_config = Mock(spec=TraderConfig)
-    trader_config.trader_min_buffer = 60
+    trader_config.trader_min_buffer = 20
     feed = Mock(spec=Feed)
     feed.name = "test feed"
     feed.address = "0x123"
 
     feed.seconds_per_epoch = 60
     predictoor_contract = Mock(spec=PredictoorContract)
-    predictoor_contract.get_current_epoch.return_value = 1
     predictoor_contract.get_agg_predval.return_value = (1, 2)
 
     trader_config.get_feeds.return_value = {"0x123": feed}
@@ -76,25 +75,23 @@ def test_process_block_at_feed():
 
     agent = TraderAgent(trader_config, custom_trader)
 
-    # epoch_s_left = 60 + 60 - 100 = 20, so we should not trade
+    # epoch_s_left = 60 - 55 = 5, so we should not trade
     # because it's too close to the epoch end
-    agent._process_block_at_feed("0x123", 100)
+    agent._process_block_at_feed("0x123", 55)
     assert len(agent.prev_traded_epochs_per_feed["0x123"]) == 0
 
-    # epoch_s_left = 60 + 60 - 20 = 10, so we should trade
-    result = agent._process_block_at_feed("0x123", 20)
+    # epoch_s_left = 60 + 60 - 80 = 40, so we should not trade
+    agent._process_block_at_feed("0x123", 80)
     assert len(agent.prev_traded_epochs_per_feed["0x123"]) == 1
-    assert result == (feed, (1, 2))
 
     # but not again, because we've already traded this epoch
-    agent._process_block_at_feed("0x123", 20)
+    agent._process_block_at_feed("0x123", 80)
     assert len(agent.prev_traded_epochs_per_feed["0x123"]) == 1
 
     # but we should trade again in the next epoch
     predictoor_contract.get_current_epoch.return_value = 2
-    result = agent._process_block_at_feed("0x123", 20)
+    agent._process_block_at_feed("0x123", 20)
     assert len(agent.prev_traded_epochs_per_feed["0x123"]) == 2
-    assert result == (feed, (1, 2))
 
     # prediction is empty, so no trading
     predictoor_contract.get_current_epoch.return_value = 3
@@ -108,6 +105,5 @@ def test_process_block_at_feed():
     agent = TraderAgent(trader_config)
     predictoor_contract.get_agg_predval.return_value = (1, 3)
     predictoor_contract.get_agg_predval.side_effect = None
-    result = agent._process_block_at_feed("0x123", 20)
+    agent._process_block_at_feed("0x123", 20)
     assert len(agent.prev_traded_epochs_per_feed["0x123"]) == 1
-    assert result == (feed)
