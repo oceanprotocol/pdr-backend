bacfdee106d2b848b03daa8ac66cdac81027d531
trizin <25263018+trizin@users.noreply.github.com>
2023-09-20 12:40:11 +0000

    Add fixtures

--- pdr_backend/dfbuyer/test/conftest.py
+++ pdr_backend/dfbuyer/test/conftest.py
@@ -1,1 +1,42 @@
-from pdr_backend.conftest_ganache import *  # pylint: disable=wildcard-import
+from unittest.mock import Mock, patch
+import pytest
+from pdr_backend.conftest_ganache import *
+from pdr_backend.dfbuyer.dfbuyer_agent import DFBuyerAgent
+from pdr_backend.dfbuyer.dfbuyer_config import DFBuyerConfig
+from pdr_backend.models.feed import Feed
+from pdr_backend.util.constants import ZERO_ADDRESS  # pylint: disable=wildcard-import
+
+
+def mock_feed():
+    feed = Mock(spec=Feed)
+    feed.name = "test feed"
+    feed.seconds_per_epoch = 60
+    return feed
+
+
+@pytest.fixture
+def dfbuyer_config():
+    config = DFBuyerConfig()
+    config.get_feeds = Mock()
+    config.get_feeds.return_value = {
+        "0x0000000000000000000000000000000000000000": mock_feed()
+    }
+    return config
+
+
+@pytest.fixture
+def mock_get_address():
+    with patch("pdr_backend.dfbuyer.dfbuyer_agent.get_address") as mock:
+        mock.return_value = ZERO_ADDRESS
+        yield mock
+
+
+@pytest.fixture
+def mock_token():
+    with patch("pdr_backend.dfbuyer.dfbuyer_agent.Token") as mock:
+        yield mock
+
+
+@pytest.fixture
+def dfbuyer_agent(mock_get_address, mock_token, dfbuyer_config):
+    return DFBuyerAgent(dfbuyer_config)
