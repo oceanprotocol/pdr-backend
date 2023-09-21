f2cf1562fa83acc5d24dec09efd4e6148f6c5660
trizin <25263018+trizin@users.noreply.github.com>
2023-09-21 09:59:01 +0000

    Add a test for main.py

--- pdr_backend/dfbuyer/test/test_main.py
+++ pdr_backend/dfbuyer/test/test_main.py
@@ -1,1 +1,15 @@
-import pytest
+from unittest.mock import patch
+
+from pdr_backend.dfbuyer.dfbuyer_agent import DFBuyerAgent
+from pdr_backend.dfbuyer.dfbuyer_config import DFBuyerConfig
+from pdr_backend.dfbuyer.main import main
+
+
+@patch.object(DFBuyerAgent, "run")
+@patch.object(
+    DFBuyerAgent, "__init__", return_value=None
+)  # Mock the constructor to return None
+def test_main(mock_agent_init, mock_agent_run):
+    main()
+    mock_agent_init.assert_called_once()
+    mock_agent_run.assert_called_once()
