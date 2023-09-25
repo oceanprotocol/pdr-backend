d188b52e71dec47a4cf847038366824addb96f2e
trizin <25263018+trizin@users.noreply.github.com>
2023-09-25 12:59:08 +0300

    Refactor main.py

--- pdr_backend/publisher/main.py
+++ pdr_backend/publisher/main.py
@@ -4,88 +4,92 @@ from pdr_backend.util.contract import get_address
 from pdr_backend.util.env import getenv_or_exit
 from pdr_backend.util.web3_config import Web3Config
 
-rpc_url = getenv_or_exit("RPC_URL")
-private_key = getenv_or_exit("PRIVATE_KEY")
+def main():
+    rpc_url = getenv_or_exit("RPC_URL")
+    private_key = getenv_or_exit("PRIVATE_KEY")
 
-web3_config = Web3Config(rpc_url, private_key)
-ocean_address = get_address(web3_config.w3.eth.chain_id, "Ocean")
+    web3_config = Web3Config(rpc_url, private_key)
+    ocean_address = get_address(web3_config.w3.eth.chain_id, "Ocean")
 
-OCEAN = Token(web3_config, ocean_address)
+    OCEAN = Token(web3_config, ocean_address)
 
-accounts_to_fund = [
-    #    account_key_env,   OCEAN_to_send
-    ("PREDICTOOR_PRIVATE_KEY", 2000.0),
-    ("PREDICTOOR2_PRIVATE_KEY", 2000.0),
-    ("PREDICTOOR3_PRIVATE_KEY", 2000.0),
-    ("TRADER_PRIVATE_KEY", 2000.0),
-    ("DFBUYER_PRIVATE_KEY", 10000.0),
-    ("PDR_WEBSOCKET_KEY", 10000.0),
-    ("PDR_MM_USER", 10000.0),
-]
+    accounts_to_fund = [
+        #    account_key_env,   OCEAN_to_send
+        ("PREDICTOOR_PRIVATE_KEY", 2000.0),
+        ("PREDICTOOR2_PRIVATE_KEY", 2000.0),
+        ("PREDICTOOR3_PRIVATE_KEY", 2000.0),
+        ("TRADER_PRIVATE_KEY", 2000.0),
+        ("DFBUYER_PRIVATE_KEY", 10000.0),
+        ("PDR_WEBSOCKET_KEY", 10000.0),
+        ("PDR_MM_USER", 10000.0),
+    ]
 
-if web3_config.w3.eth.chain_id == 8996:
-    fund_dev_accounts(accounts_to_fund, web3_config.owner, OCEAN)
-    publish(
-        s_per_epoch=300,
-        s_per_subscription=60 * 60 * 24,
-        base="ETH",
-        quote="USDT",
-        source="binance",
-        timeframe="5m",
-        trueval_submitter_addr="0xe2DD09d719Da89e5a3D0F2549c7E24566e947260",  # on arge
-        feeCollector_addr="0xe2DD09d719Da89e5a3D0F2549c7E24566e947260",
-        rate=3,
-        cut=0.2,
-        web3_config=web3_config,
-    )
-
-    publish(
-        s_per_epoch=300,
-        s_per_subscription=60 * 60 * 24,
-        base="BTC",
-        quote="TUSD",
-        source="binance",
-        timeframe="5m",
-        trueval_submitter_addr="0xe2DD09d719Da89e5a3D0F2549c7E24566e947260",
-        feeCollector_addr="0xe2DD09d719Da89e5a3D0F2549c7E24566e947260",
-        rate=3,
-        cut=0.2,
-        web3_config=web3_config,
-    )
-
-    publish(
-        s_per_epoch=300,
-        s_per_subscription=60 * 60 * 24,
-        base="XRP",
-        quote="USDT",
-        source="binance",
-        timeframe="5m",
-        trueval_submitter_addr="0xe2DD09d719Da89e5a3D0F2549c7E24566e947260",
-        feeCollector_addr="0xe2DD09d719Da89e5a3D0F2549c7E24566e947260",
-        rate=3,
-        cut=0.2,
-        web3_config=web3_config,
-    )
-
-if web3_config.w3.eth.chain_id == 23295:
-    rate = 3 / (1 + 0.2 + 0.001)
-    pair_list = ["BTC", "ETH", "BNB", "XRP", "ADA", "DOGE", "SOL", "LTC", "TRX", "DOT"]
-    helper_contract = get_address(web3_config.w3.eth.chain_id, "PredictoorHelper")
-
-    for pair in pair_list:
+    if web3_config.w3.eth.chain_id == 8996:
+        fund_dev_accounts(accounts_to_fund, web3_config.owner, OCEAN)
         publish(
             s_per_epoch=300,
             s_per_subscription=60 * 60 * 24,
-            base=pair,
+            base="ETH",
             quote="USDT",
             source="binance",
             timeframe="5m",
-            trueval_submitter_addr=helper_contract,
+            trueval_submitter_addr="0xe2DD09d719Da89e5a3D0F2549c7E24566e947260",  # on arge
             feeCollector_addr="0xe2DD09d719Da89e5a3D0F2549c7E24566e947260",
-            rate=rate,
+            rate=3,
             cut=0.2,
             web3_config=web3_config,
         )
 
-if web3_config.w3.eth.chain_id == 23294:
-    raise NotImplementedError("Mainnet deployment configuration is missing")
+        publish(
+            s_per_epoch=300,
+            s_per_subscription=60 * 60 * 24,
+            base="BTC",
+            quote="TUSD",
+            source="binance",
+            timeframe="5m",
+            trueval_submitter_addr="0xe2DD09d719Da89e5a3D0F2549c7E24566e947260",
+            feeCollector_addr="0xe2DD09d719Da89e5a3D0F2549c7E24566e947260",
+            rate=3,
+            cut=0.2,
+            web3_config=web3_config,
+        )
+
+        publish(
+            s_per_epoch=300,
+            s_per_subscription=60 * 60 * 24,
+            base="XRP",
+            quote="USDT",
+            source="binance",
+            timeframe="5m",
+            trueval_submitter_addr="0xe2DD09d719Da89e5a3D0F2549c7E24566e947260",
+            feeCollector_addr="0xe2DD09d719Da89e5a3D0F2549c7E24566e947260",
+            rate=3,
+            cut=0.2,
+            web3_config=web3_config,
+        )
+
+    if web3_config.w3.eth.chain_id == 23295:
+        rate = 3 / (1 + 0.2 + 0.001)
+        pair_list = ["BTC", "ETH", "BNB", "XRP", "ADA", "DOGE", "SOL", "LTC", "TRX", "DOT"]
+        helper_contract = get_address(web3_config.w3.eth.chain_id, "PredictoorHelper")
+
+        for pair in pair_list:
+            publish(
+                s_per_epoch=300,
+                s_per_subscription=60 * 60 * 24,
+                base=pair,
+                quote="USDT",
+                source="binance",
+                timeframe="5m",
+                trueval_submitter_addr=helper_contract,
+                feeCollector_addr="0xe2DD09d719Da89e5a3D0F2549c7E24566e947260",
+                rate=rate,
+                cut=0.2,
+                web3_config=web3_config,
+            )
+
+    if web3_config.w3.eth.chain_id == 23294:
+        raise NotImplementedError("Mainnet deployment configuration is missing")
+
+if __name__ == "__main__":
+    main()
