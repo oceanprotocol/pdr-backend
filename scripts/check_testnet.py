d71a5f7d99aec8c907fe98cf7a57d120163e8fe8
trizin <25263018+trizin@users.noreply.github.com>
2023-09-26 12:30:35 +0000

    Add dfbuyer to check script

--- scripts/check_testnet.py
+++ scripts/check_testnet.py
@@ -1,9 +1,14 @@
+from collections import defaultdict
+import math
 import sys
 import time
+from typing import Dict
 from pdr_backend.models.base_config import BaseConfig
 from pdr_backend.models.token import Token
 from pdr_backend.util.contract import get_address
-from pdr_backend.util.subgraph import query_subgraph
+from pdr_backend.util.subgraph import get_consume_so_far_per_contract, query_subgraph
+
+WEEK = 86400 * 7
 
 
 def print_stats(contract_dict, field_name, threshold=0.9):
@@ -14,6 +19,54 @@ def print_stats(contract_dict, field_name, threshold=0.9):
     token_name = contract_dict["token"]["name"]
 
     print(f"{token_name}: {with_field}/{count} - {status}")
+
+
+def get_expected_consume(ts: int):
+    amount_per_feed_per_interval = 37000 / 7 / 20
+    week_start = (math.floor(ts / WEEK)) * WEEK
+    time_passed = ts - week_start
+    n_intervals = int(time_passed / 86400)
+    return n_intervals * amount_per_feed_per_interval
+
+
+def check_consume_dfbuyer(address: str):
+    chunk_size = 1000
+    offset = 0
+    consume_per_contract: Dict[str, float] = defaultdict(float)
+    ts = time.time()
+    ts_start = int((ts // WEEK) * WEEK)
+    while True:
+        query = """
+        {
+            predictSubscriptions(where: {user_: {id: "%s"}, timestamp_gt: %s}, first: 1000, skip: %s) {
+                id
+                predictContract {
+                    id
+                }
+                user {
+                    id
+                }
+            }
+        }
+        """ % (
+            address,
+            ts_start,
+            offset,
+        )
+        result = query_subgraph(config.subgraph_url, query, timeout=10.0)
+        data = result["data"]["predictSubscriptions"]
+        offset += chunk_size
+
+        if len(data) == 0:
+            break
+
+        for subscription in data:
+            contract_address = subscription["predictContract"]["id"]
+            consume_per_contract[contract_address] += 3.0
+
+    print(consume_per_contract)
+
+    return consume_per_contract
 
 
 if __name__ == "__main__":
@@ -32,6 +85,7 @@ if __name__ == "__main__":
         "predictoor3": "0x005C414442a892077BD2c1d62B1dE2Fc127E5b9B",
         "trueval": "0x005FD44e007866508f62b04ce9f43dd1d36D0c0c",
         "websocket": "0x008d4866C4071AC9d74D6359604762C7B581D390",
+        "dfbuyer": "0xeA24C440eC55917fFa030C324535fc49B42c2fD7",
     }
 
     ts = int(time.time())
@@ -121,3 +175,23 @@ if __name__ == "__main__":
         if native_warning:
             print(native_warning)
         print()
+
+
+    # ---------------- dfbuyer ----------------
+
+    ts = time.time()
+    ts_start = int((ts // WEEK) * WEEK)
+    sofar = get_consume_so_far_per_contract(
+        config.subgraph_url,
+        addresses["dfbuyer"].lower(),
+        ts_start,
+        [i["id"] for i in result["data"]["predictContracts"]],
+    )
+    expected = get_expected_consume(int(ts))
+    i = 0
+    print(
+        f"Checking consume amounts (dfbuyer), expecting {expected} consume per contract"
+    )
+    for addr, x in sofar.items():
+        result = "PASS" if x >= expected else "FAIL"
+        print(f"    {result}... got: {x} for contract: {addr}")
