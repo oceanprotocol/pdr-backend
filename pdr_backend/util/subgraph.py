3553c3a8bb7502be0015173b016e78f83609a61b
trizin <25263018+trizin@users.noreply.github.com>
2023-10-04 10:10:00 +0000

    Formatting

--- pdr_backend/util/subgraph.py
+++ pdr_backend/util/subgraph.py
@@ -442,6 +442,7 @@ def get_consume_so_far_per_contract(
             break
     return consume_so_far
 
+
 @enforce_types
 def is_block_number_synced(subgraph_url: str, block_number: int) -> bool:
     query = """
@@ -460,6 +461,7 @@ def is_block_number_synced(subgraph_url: str, block_number: int) -> bool:
     except Exception as _:
         return False
     return True
+
 
 @enforce_types
 def wait_till_subgraph_syncs(web3_config: Web3Config, subgraph_url: str):
