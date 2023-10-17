874cafa67accda7c115b3efd9acae8332e238dac
trizin <25263018+trizin@users.noreply.github.com>
2023-10-17 18:52:05 +0000

    Formatting

--- pdr_backend/predictoor/approach3/predictoor_config3.py
+++ pdr_backend/predictoor/approach3/predictoor_config3.py
@@ -13,4 +13,4 @@ class PredictoorConfig3(BasePredictoorConfig):
         self.N_test = 10
         self.signals = ["close"]  # ["open", "high","low", "close", "volume"]
         self.st_timestamp = timestr_to_ut("2023-01-31")  # 2019-09-13_04:00 earliest
-        self.model_ss = "LIN" # PREV, LIN, GPR, SVR, NuSVR, LinearSVR
+        self.model_ss = "LIN"  # PREV, LIN, GPR, SVR, NuSVR, LinearSVR
