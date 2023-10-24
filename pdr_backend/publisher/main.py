9b9855d0114b365350d6328f00f2addaba4dbf07
trizin <25263018+trizin@users.noreply.github.com>
2023-10-24 13:52:17 +0300

    Further refactoring

--- pdr_backend/publisher/main.py
+++ pdr_backend/publisher/main.py
@@ -9,26 +9,31 @@ from pdr_backend.util.web3_config import Web3Config
 def main():
     rpc_url = getenv_or_exit("RPC_URL")
     private_key = getenv_or_exit("PRIVATE_KEY")
-    fee_collector = getenv("FEE_COLLECTOR")
 
+    # pairs to deploy on testnet and mainnet
+    pair_list = ["BTC", "ETH", "BNB", "XRP", "ADA", "DOGE", "SOL", "LTC", "TRX", "DOT"]
+    
+    # token price
+    rate = 3 / (1 + 0.2 + 0.001)
+    
     web3_config = Web3Config(rpc_url, private_key)
-    ocean_address = get_address(web3_config.w3.eth.chain_id, "Ocean")
-
-    OCEAN = Token(web3_config, ocean_address)
-
-    accounts_to_fund = [
-        #    account_key_env,   OCEAN_to_send
-        ("PREDICTOOR_PRIVATE_KEY", 2000.0),
-        ("PREDICTOOR2_PRIVATE_KEY", 2000.0),
-        ("PREDICTOOR3_PRIVATE_KEY", 2000.0),
-        ("TRADER_PRIVATE_KEY", 2000.0),
-        ("DFBUYER_PRIVATE_KEY", 10000.0),
-        ("PDR_WEBSOCKET_KEY", 10000.0),
-        ("PDR_MM_USER", 10000.0),
-    ]
 
     if web3_config.w3.eth.chain_id == 8996:
+        ocean_address = get_address(web3_config.w3.eth.chain_id, "Ocean")
+        OCEAN = Token(web3_config, ocean_address)
+        accounts_to_fund = [
+            #    account_key_env,   OCEAN_to_send
+            ("PREDICTOOR_PRIVATE_KEY", 2000.0),
+            ("PREDICTOOR2_PRIVATE_KEY", 2000.0),
+            ("PREDICTOOR3_PRIVATE_KEY", 2000.0),
+            ("TRADER_PRIVATE_KEY", 2000.0),
+            ("DFBUYER_PRIVATE_KEY", 10000.0),
+            ("PDR_WEBSOCKET_KEY", 10000.0),
+            ("PDR_MM_USER", 10000.0),
+        ]
+
         fund_dev_accounts(accounts_to_fund, web3_config.owner, OCEAN)
+
         publish(
             s_per_epoch=300,
             s_per_subscription=60 * 60 * 24,
@@ -38,7 +43,7 @@ def main():
             timeframe="5m",
             trueval_submitter_addr="0xe2DD09d719Da89e5a3D0F2549c7E24566e947260",  # on arge
             feeCollector_addr="0xe2DD09d719Da89e5a3D0F2549c7E24566e947260",
-            rate=3,
+            rate=rate,
             cut=0.2,
             web3_config=web3_config,
         )
@@ -52,7 +57,7 @@ def main():
             timeframe="5m",
             trueval_submitter_addr="0xe2DD09d719Da89e5a3D0F2549c7E24566e947260",
             feeCollector_addr="0xe2DD09d719Da89e5a3D0F2549c7E24566e947260",
-            rate=3,
+            rate=rate,
             cut=0.2,
             web3_config=web3_config,
         )
@@ -66,16 +71,14 @@ def main():
             timeframe="5m",
             trueval_submitter_addr="0xe2DD09d719Da89e5a3D0F2549c7E24566e947260",
             feeCollector_addr="0xe2DD09d719Da89e5a3D0F2549c7E24566e947260",
-            rate=3,
+            rate=rate,
             cut=0.2,
             web3_config=web3_config,
         )
 
     if web3_config.w3.eth.chain_id == 23295:
-        rate = 3 / (1 + 0.2 + 0.001)
-        pair_list = ["BTC", "ETH", "BNB", "XRP", "ADA", "DOGE", "SOL", "LTC", "TRX", "DOT"]
         helper_contract = get_address(web3_config.w3.eth.chain_id, "PredictoorHelper")
-
+        fee_collector = getenv_or_exit("FEE_COLLECTOR")
         for pair in pair_list:
             publish(
                 s_per_epoch=300,
@@ -85,7 +88,7 @@ def main():
                 source="binance",
                 timeframe="5m",
                 trueval_submitter_addr=helper_contract,
-                feeCollector_addr="0xe2DD09d719Da89e5a3D0F2549c7E24566e947260",
+                feeCollector_addr=fee_collector,
                 rate=rate,
                 cut=0.2,
                 web3_config=web3_config,
@@ -98,16 +101,15 @@ def main():
                 source="binance",
                 timeframe="1h",
                 trueval_submitter_addr=helper_contract,
-                feeCollector_addr="0xe2DD09d719Da89e5a3D0F2549c7E24566e947260",
+                feeCollector_addr=fee_collector,
                 rate=rate,
                 cut=0.2,
                 web3_config=web3_config,
             )
 
     if web3_config.w3.eth.chain_id == 23294:
-        rate = 3 / (1 + 0.2 + 0.001)
-        pair_list = ["BTC", "ETH", "BNB", "XRP", "ADA", "DOGE", "SOL", "LTC", "TRX", "DOT"]
         helper_contract = get_address(web3_config.w3.eth.chain_id, "PredictoorHelper")
+        fee_collector = getenv_or_exit("FEE_COLLECTOR")
         for pair in pair_list:
             publish(
                 s_per_epoch=300,
