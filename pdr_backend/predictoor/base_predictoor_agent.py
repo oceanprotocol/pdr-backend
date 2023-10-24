09bf1e0139bec6cacdf77fc93d2a2ccf36c6c871
trizin <25263018+trizin@users.noreply.github.com>
2023-10-24 13:32:29 +0300

    Add retry mechanism for predictoor `take_step` with error handling.

--- pdr_backend/predictoor/base_predictoor_agent.py
+++ pdr_backend/predictoor/base_predictoor_agent.py
@@ -54,7 +54,16 @@ class BasePredictoorAgent(ABC):
     def run(self):
         print("Starting main loop...")
         while True:
-            self.take_step()
+            max_tries = 5
+            for i in range(max_tries):
+                try:
+                    self.take_step()
+                    break
+                except e as Exception:
+                    if i < max_tries - 1:
+                        print(f"An error: \"{e}\" occured while executing take_step, trying again...")
+                    else:
+                        raise e
 
     def take_step(self):
         w3 = self.config.web3_config.w3
