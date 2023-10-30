06e3ed6f40a1159f29a79ff990c75b9eb12c092b
trizin <25263018+trizin@users.noreply.github.com>
2023-10-30 14:18:43 +0300

    Add decorators module

---
+++ pdr_backend/util/decorators.py
@@ -0,0 +1,21 @@
+import time
+
+@enforce_types
+def retry_function(times=5, delay=2, allowed_exceptions=()):
+    """
+    Decorator for retrying a function if exception occurs.
+    """
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            for _ in range(times):
+                try:
+                    return func(*args, **kwargs)
+                except allowed_exceptions as e:
+                    if _ < times - 1:
+                        time.sleep(delay)
+                        continue
+                    else:
+                        raise e
+        return wrapper
+    return decorator
