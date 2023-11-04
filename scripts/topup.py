4352199bd9567748f7bc4423f3914e5e95831c1c
alexcos20 <alex.coseru@gmail.com>
2023-11-04 07:45:35 +0200

    lint

--- scripts/topup.py
+++ scripts/topup.py
@@ -1,4 +1,4 @@
-import time
+import sys
 from addresses import get_opf_addresses
 from pdr_backend.models.base_config import BaseConfig
 from pdr_backend.models.token import Token, NativeToken
@@ -7,24 +7,29 @@ from pdr_backend.util.contract import get_address
 
 if __name__ == "__main__":
     config = BaseConfig()
-    failed = False  #if there is not enough balance, exit 1 so we know that script failed
+    failed = (
+        False  # if there is not enough balance, exit 1 so we know that script failed
+    )
     addresses = get_opf_addresses(config.web3_config.w3.eth.chain_id)
     ocean_address = get_address(config.web3_config.w3.eth.chain_id, "Ocean")
     ocean_token = Token(config.web3_config, ocean_address)
     rose = NativeToken(config.web3_config)
 
-    owner_ocean_balance=int(ocean_token.balanceOf(config.web3_config.owner))/1e18
-    owner_rose_balance=int(rose.balanceOf(config.web3_config.owner))/1e18
-    print(f"Topup address ({config.web3_config.owner}) has : {owner_ocean_balance:.2f} OCEAN and {owner_rose_balance:.2f} ROSE\n\n")
-    total_ocean=0
-    total_rose=0
+    owner_ocean_balance = int(ocean_token.balanceOf(config.web3_config.owner)) / 1e18
+    owner_rose_balance = int(rose.balanceOf(config.web3_config.owner)) / 1e18
+    print(
+        f"Topup address ({config.web3_config.owner}) has "
+        + "{owner_ocean_balance:.2f} OCEAN and {owner_rose_balance:.2f} ROSE\n\n"
+    )
+    total_ocean = 0
+    total_rose = 0
     for name, value in addresses.items():
         ocean_bal_wei = ocean_token.balanceOf(value)
         rose_bal_wei = rose.balanceOf(value)
 
         ocean_bal = ocean_bal_wei / 1e18
         rose_bal = rose_bal_wei / 1e18
-        
+
         minimum_ocean_bal = 10
         minimum_rose_bal = 10
         topup_ocean_bal = 10
@@ -43,31 +48,31 @@ if __name__ == "__main__":
         # check if we need to transfer
         if minimum_ocean_bal > 0 and ocean_bal < minimum_ocean_bal:
             print(f"\t Transfering {topup_ocean_bal} OCEAN to {value}...")
-            if owner_ocean_balance>topup_ocean_bal:
+            if owner_ocean_balance > topup_ocean_bal:
                 ocean_token.transfer(
                     value,
                     config.web3_config.w3.to_wei(topup_ocean_bal, "ether"),
                     config.web3_config.owner,
                     True,
                 )
-                owner_ocean_balance=owner_ocean_balance-topup_ocean_bal
+                owner_ocean_balance = owner_ocean_balance - topup_ocean_bal
             else:
                 failed = True
                 print("Not enough OCEAN :(")
         if minimum_rose_bal > 0 and rose_bal < minimum_rose_bal:
             print(f"\t Transfering {topup_rose_bal} ROSE to {value}...")
-            if owner_rose_balance>topup_rose_bal:
+            if owner_rose_balance > topup_rose_bal:
                 rose.transfer(
                     value,
                     config.web3_config.w3.to_wei(topup_rose_bal, "ether"),
                     config.web3_config.owner,
                     True,
                 )
-                owner_rose_balance=owner_rose_balance-topup_rose_bal
+                owner_rose_balance = owner_rose_balance - topup_rose_bal
             else:
                 failed = True
                 print("Not enough ROSE :(")
     if failed:
-        exit(1)
+        sys.exit(1)
     else:
-        exit(0)
+        sys.exit(0)
