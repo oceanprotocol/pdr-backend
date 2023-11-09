0cc4f4f5f7e91bbfb04f7b98aa90ca7bee19648e
trizin <25263018+trizin@users.noreply.github.com>
2023-11-09 15:19:58 +0300

    Implement contract functions

--- pdr_backend/models/dfrewards.py
+++ pdr_backend/models/dfrewards.py
@@ -9,8 +9,16 @@ class DFRewards(BaseContract):
     def __init__(self, config: Web3Config, address: str):
         super().__init__(config, address, "DFRewards")
 
-    def claim_rewards(self, address: str):
-        pass
+    def claim_rewards(self, user_addr: str, token_addr: str wait_for_receipt=True):
+        gasPrice = self.config.w3.eth.gas_price
+        tx = self.contract_instance.functions.claimFor(
+            user_addr, token_addr
+        ).transact({"from": self.config.owner, "gasPrice": gasPrice})
+        if not wait_for_receipt:
+            return tx
+        return self.config.w3.eth.wait_for_transaction_receipt(tx)
     
-    def get_available_rewards(self, address:str) -> float:
-        pass
+    def get_claimable_rewards(self, user_addr: str, token_addr: str) -> float:
+        claimable_wei = self.contract_instance.functions.claimable(user_addr, token_addr).call()
+        claimable_rewards = self.config.w3.from_wei(claimable_wei, "ether")
+        return claimable_rewards
