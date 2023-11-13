c466fe85e99c82c6b284d881897485056f785968
trentmc <gtrent@gmail.com>
2023-11-13 19:29:56 +0100

    test addr's of dfrewards and ocean contracts

--- pdr_backend/models/test/test_dfrewards.py
+++ pdr_backend/models/test/test_dfrewards.py
@@ -7,11 +7,14 @@ from pdr_backend.util.web3_config import Web3Config
 
 @enforce_types
 def test_dfrewards(web3_config: Web3Config):
-    dfrewards_address = get_address(web3_config.w3.eth.chain_id, "DFRewards")
-    ocean_address = get_address(web3_config.w3.eth.chain_id, "Ocean")
+    dfrewards_addr = get_address(web3_config.w3.eth.chain_id, "DFRewards")
+    assert isinstance(dfrewards_addr, str)
 
-    contract = DFRewards(web3_config, dfrewards_address)
-    rewards = contract.get_claimable_rewards(web3_config.owner, ocean_address)
+    ocean_addr = get_address(web3_config.w3.eth.chain_id, "Ocean")
+    assert isinstance(dfrewards_addr, str)
+
+    contract = DFRewards(web3_config, dfrewards_addr)
+    rewards = contract.get_claimable_rewards(web3_config.owner, ocean_addr)
     assert rewards == 0
 
-    contract.claim_rewards(web3_config.owner, ocean_address)
+    contract.claim_rewards(web3_config.owner, ocean_addr)
