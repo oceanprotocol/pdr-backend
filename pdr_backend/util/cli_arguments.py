ae9532c71f6d6113ca53750cfc6145519ed6034f
trizin <25263018+trizin@users.noreply.github.com>
2023-11-22 09:17:54 +0000

    Update Trueval arg parser

--- pdr_backend/util/cli_arguments.py
+++ pdr_backend/util/cli_arguments.py
@@ -109,6 +109,6 @@ class _ArgParser_NETWORK_YAML(ArgParser, NETWORK_Mixin, YAML_Mixin):
         self.add_argument_YAML()
 
 
-TruevalArgParser = _ArgParser_NETWORK_YAML
+TruevalArgParser = _ArgParser_APPROACH_NETWORK_YAML
 DfbuyerArgParser = _ArgParser_NETWORK_YAML
 PublisherArgParser = _ArgParser_NETWORK_YAML
