55340c8146fc1c645be89e48a348ffb5706c1b81
trizin <25263018+trizin@users.noreply.github.com>
2023-11-27 10:08:17 +0000

    Black

--- pdr_backend/dfbuyer/main.py
+++ pdr_backend/dfbuyer/main.py
@@ -1,5 +1,10 @@
 if __name__ == "__main__":
-    print("Error: 'main.py' has been removed as of version v0.2.0. Please use pdr cli instead.")
-    print("View the releases page to learn how to migrate: https://github.com/oceanprotocol/pdr-backend/releases")
-    print("Swich back to version v0.1.1 if you'd like to keep using main.py and env vars")
-
+    print(
+        "Error: 'main.py' has been removed as of version v0.2.0. Please use pdr cli instead."
+    )
+    print(
+        "View the releases page to learn how to migrate: https://github.com/oceanprotocol/pdr-backend/releases"
+    )
+    print(
+        "Swich back to version v0.1.1 if you'd like to keep using main.py and env vars"
+    )
