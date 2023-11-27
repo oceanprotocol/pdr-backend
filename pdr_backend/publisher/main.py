0101e817e1fbfb60a4755b1ecd0f18c195c4d691
trizin <25263018+trizin@users.noreply.github.com>
2023-11-27 12:56:40 +0300

    Add main.py back

--- pdr_backend/publisher/main.py
+++ pdr_backend/publisher/main.py
@@ -142,3 +142,9 @@ def publish_assets(web3_pp: Web3PP, publisher_ss: PublisherSS):
                 web3_pp=web3_pp,
             )
         print("Publish done")
+
+
+if __name__ == "__main__":
+    print("Error: 'main.py' has been removed as of version v0.2.0. Please use pdr cli instead.")
+    print("View the releases page to learn how to migrate: https://github.com/oceanprotocol/pdr-backend/releases")
+    print("Swich back to version v0.1.1 if you'd like to keep using main.py and env vars")
