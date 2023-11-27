4c78f72fecaa3aa8a67749ce77ff598f0e1378e9
trentmc <gtrent@gmail.com>
2023-11-27 17:28:40 +0100

    goes with previous commit

--- pdr_backend/models/test/test_base_contract.py
+++ pdr_backend/models/test/test_base_contract.py
@@ -1,31 +1,18 @@
-import time
-
 from enforce_typing import enforce_types
+import pytest
 
 from pdr_backend.util.contract import get_address
 from pdr_backend.models.token import Token
 
 
 @enforce_types
-def test_token(web3_pp, web3_config):
-    token_address = get_address(web3_pp, "Ocean")
-    token = Token(web3_pp, token_address)
+def test_base_contract(web3_pp, web3_config):
+    OCEAN_address = get_address(web3_pp, "Ocean")
 
-    accounts = web3_config.w3.eth.accounts
-    owner_addr = web3_config.owner
-    alice = accounts[1]
+    # success
+    Token(web3_pp, OCEAN_address)
 
-    token.contract_instance.functions.mint(owner_addr, 1000000000).transact(
-        {"from": owner_addr, "gasPrice": web3_config.w3.eth.gas_price}
-    )
-
-    allowance_start = token.allowance(owner_addr, alice)
-    token.approve(alice, allowance_start + 100, True)
-    time.sleep(1)
-    allowance_end = token.allowance(owner_addr, alice)
-    assert allowance_end - allowance_start == 100
-
-    balance_start = token.balanceOf(alice)
-    token.transfer(alice, 100, owner_addr)
-    balance_end = token.balanceOf(alice)
-    assert balance_end - balance_start == 100
+    # catch failure
+    web3_config = web3_pp.web3_config
+    with pytest.raises(ValueError):
+        Token(web3_config, OCEAN_address)
