1b6556c1eb08165c64702a03a556140c49e30af4
trentmc <gtrent@gmail.com>
2023-11-27 17:28:06 +0100

    BaseContract tests that Web3PP type is input

--- pdr_backend/models/base_contract.py
+++ pdr_backend/models/base_contract.py
@@ -8,6 +8,12 @@ from pdr_backend.util.contract import get_contract_abi
 class BaseContract(ABC):
     def __init__(self, web3_pp, address: str, contract_name: str):
         super().__init__()
+        from pdr_backend.ppss.web3_pp import (  # pylint: disable=import-outside-toplevel
+            Web3PP,
+        )
+
+        if not isinstance(web3_pp, Web3PP):
+            raise ValueError(f"web3_pp is {web3_pp.__class__}, not Web3PP")
         self.web3_pp = web3_pp
         self.config = web3_pp.web3_config  # for convenience
         self.contract_address = self.config.w3.to_checksum_address(address)
---
+++ pdr_backend/models/test/test_base_contract.py
@@ -0,0 +1,31 @@
+import time
+
+from enforce_typing import enforce_types
+
+from pdr_backend.util.contract import get_address
+from pdr_backend.models.token import Token
+
+
+@enforce_types
+def test_token(web3_pp, web3_config):
+    token_address = get_address(web3_pp, "Ocean")
+    token = Token(web3_pp, token_address)
+
+    accounts = web3_config.w3.eth.accounts
+    owner_addr = web3_config.owner
+    alice = accounts[1]
+
+    token.contract_instance.functions.mint(owner_addr, 1000000000).transact(
+        {"from": owner_addr, "gasPrice": web3_config.w3.eth.gas_price}
+    )
+
+    allowance_start = token.allowance(owner_addr, alice)
+    token.approve(alice, allowance_start + 100, True)
+    time.sleep(1)
+    allowance_end = token.allowance(owner_addr, alice)
+    assert allowance_end - allowance_start == 100
+
+    balance_start = token.balanceOf(alice)
+    token.transfer(alice, 100, owner_addr)
+    balance_end = token.balanceOf(alice)
+    assert balance_end - balance_start == 100
