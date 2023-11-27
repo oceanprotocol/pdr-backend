8d97753e78490e4c6a5c1428c3a238b1096a24b0
trizin <25263018+trizin@users.noreply.github.com>
2023-11-27 13:10:58 +0300

    Linter

--- pdr_backend/publisher/main.py
+++ pdr_backend/publisher/main.py
@@ -149,7 +149,7 @@ if __name__ == "__main__":
         "Error: 'main.py' has been removed as of version v0.2.0. Please use pdr cli instead."
     )
     print(
-        "View the releases page to learn how to migrate: https://github.com/oceanprotocol/pdr-backend/releases"
+        "View the releases page to learn how to migrate: github.com/oceanprotocol/pdr-backend/releases"
     )
     print(
         "Swich back to version v0.1.1 if you'd like to keep using main.py and env vars"
