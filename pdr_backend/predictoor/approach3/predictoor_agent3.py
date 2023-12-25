81a3178a7dad81c010273457557be8b3c12a2a92
idiom-bytes <idiom.bytes@gmail.com>
2023-12-24 18:04:40 -0800

    Adding small details to further exemplify a multi-predictoor configuration

--- pdr_backend/predictoor/approach3/predictoor_agent3.py
+++ pdr_backend/predictoor/approach3/predictoor_agent3.py
@@ -3,15 +3,36 @@ from typing import Tuple
 
 from enforce_typing import enforce_types
 
+import pandas as pd
 from pdr_backend.aimodel.aimodel_data_factory import AimodelDataFactory
 from pdr_backend.aimodel.aimodel_factory import AimodelFactory
 from pdr_backend.lake.ohlcv_data_factory import OhlcvDataFactory
 from pdr_backend.ppss.data_pp import DataPP
+from pdr_backend.ppss.data_ss import DataSS
 from pdr_backend.predictoor.base_predictoor_agent import BasePredictoorAgent
 
 
 @enforce_types
 class PredictoorAgent3(BasePredictoorAgent):
+    # override init
+    # initialize the OhlcvDataFactory
+    def __init__(self, ppss):
+        super().__init__(ppss)
+
+        self.get_data_components()
+    
+    def get_data_components(self) -> Tuple[DataPP, DataSS, pd.DataFrame]:
+        feed = self.feed
+        d = copy.deepcopy(self.ppss.data_pp.d)
+        d["predict_feeds"] = [f"{feed.source} {feed.pair} c"]
+        data_pp = DataPP(d)
+        data_ss = self.ppss.data_ss.copy_with_yval(data_pp)
+
+        ohlcv_data_factory = OhlcvDataFactory(data_pp, data_ss)
+        mergedohlcv_df = ohlcv_data_factory.get_mergedohlcv_df()
+
+        return data_pp, data_ss, mergedohlcv_df
+
     def get_prediction(
         self, timestamp: int  # pylint: disable=unused-argument
     ) -> Tuple[bool, float]:
@@ -26,17 +47,10 @@ class PredictoorAgent3(BasePredictoorAgent):
           predval -- bool -- if True, it's predicting 'up'. If False, 'down'
           stake -- int -- amount to stake, in units of Eth
         """
-        # Compute data_ss
-        feed = self.feed
-        d = copy.deepcopy(self.ppss.data_pp.d)
-        d["predict_feeds"] = [f"{feed.source} {feed.pair} c"]
-        data_pp = DataPP(d)
-        data_ss = self.ppss.data_ss.copy_with_yval(data_pp)
+        # Get data_pp, data_ss, mergedohlcv_df
+        data_pp, data_ss, mergedohlcv_df = self.get_data_components()
 
         # From data_ss, build X/y
-        pq_data_factory = OhlcvDataFactory(data_pp, data_ss)
-        mergedohlcv_df = pq_data_factory.get_mergedohlcv_df()
-
         model_data_factory = AimodelDataFactory(data_pp, data_ss)
         X, y, _ = model_data_factory.create_xy(mergedohlcv_df, testshift=0)
 
