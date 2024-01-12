1b76e3c7e5a56a964224812dd212cb8d1a76c27b
trizin <25263018+trizin@users.noreply.github.com>
2024-01-12 13:09:09 +0000

    Fix wrong import position in conftest.py

--- system_tests/conftest.py
+++ system_tests/conftest.py
@@ -3,7 +3,7 @@ import sys
 
 # Add the root directory to the path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
-from pdr_backend.conftest_ganache import *  # pylint: disable=wildcard-import
+from pdr_backend.conftest_ganache import *  # pylint: disable=wildcard-import,wrong-import-position
 
 
 @pytest.fixture(scope="session", autouse=True)
