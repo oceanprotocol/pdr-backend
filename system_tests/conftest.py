86ccf97ac360d7f71f47eeb5d0a3bdbdac1b8571
trizin <25263018+trizin@users.noreply.github.com>
2024-01-12 11:10:28 +0000

    Add conftest.py for system tests

---
+++ system_tests/conftest.py
@@ -0,0 +1,17 @@
+import os
+import sys
+
+# Add the root directory to the path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
+from pdr_backend.conftest_ganache import *  # pylint: disable=wildcard-import
+
+
+@pytest.fixture(scope="session", autouse=True)
+def set_test_env_var():
+    previous = os.getenv("TEST")
+    os.environ["TEST"] = "true"
+    yield
+    if previous is None:
+        del os.environ["TEST"]
+    else:
+        os.environ["TEST"] = previous
