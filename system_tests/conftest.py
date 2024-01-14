754926874b7f76fd149e37b16cd498499cef4410
trizin <25263018+trizin@users.noreply.github.com>
2024-01-14 16:09:20 +0000

    Add mock fixtures for SubgraphFeed and PredictoorContract

--- system_tests/conftest.py
+++ system_tests/conftest.py
@@ -1,6 +1,8 @@
 import os
 import sys
 
+from pdr_backend.subgraph.subgraph_feed import SubgraphFeed
+
 # Add the root directory to the path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
 from pdr_backend.conftest_ganache import *  # pylint: disable=wildcard-import,wrong-import-position
@@ -15,3 +17,29 @@ def set_test_env_var():
         del os.environ["TEST"]
     else:
         os.environ["TEST"] = previous
+
+
+@pytest.fixture(scope="session")
+def mock_feeds():
+    feeds = {
+        "0x1": SubgraphFeed(
+            "Feed: binance | BTC/USDT | 5m",
+            "0x1",
+            "BTC",
+            100,
+            300,
+            "0xf",
+            "BTC/USDT",
+            "5m",
+            "binance",
+        )
+    }
+    return feeds
+
+@pytest.fixture(scope="session")
+def mock_predictoor_contract():
+    mock_predictoor_contract = Mock(spec=PredictoorContract)
+    mock_predictoor_contract.payout_multiple.return_value = None
+    mock_predictoor_contract.get_agg_predval.return_value = (12, 23)
+    mock_predictoor_contract.get_current_epoch.return_value = 100
+    return mock_predictoor_contract
