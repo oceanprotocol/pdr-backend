2a3637307053e4b08972bae30a834168832ed1ab
Mustafa Tuncay <mustafaislev@gmail.com>
2024-01-15 15:01:32 +0300

    issue-481: Add Payouts to the Data Factory

--- pdr_backend/lake/gql_data_factory.py
+++ pdr_backend/lake/gql_data_factory.py
@@ -13,6 +13,10 @@ from pdr_backend.lake.table_pdr_subscriptions import (
     get_pdr_subscriptions_df,
     subscriptions_schema,
 )
+from pdr_backend.lake.table_pdr_payouts import (
+    get_pdr_payouts_df,
+    payouts_schema,
+)
 from pdr_backend.ppss.ppss import PPSS
 from pdr_backend.subgraph.subgraph_predictions import get_all_contract_ids_by_owner
 from pdr_backend.util.networkutil import get_sapphire_postfix
@@ -42,6 +46,10 @@ class GQLDataFactory:
             network=network,
         )
         contract_list = [f.lower() for f in contract_list]
+        
+        # For debugging
+        # t_contract_list = [f.lower() for f in contract_list]
+        # contract_list = [t_contract_list[0], t_contract_list[1]]
 
         # configure all tables that will be recorded onto lake
         self.record_config = {
@@ -55,6 +63,13 @@ class GQLDataFactory:
             "pdr_subscriptions": {
                 "fetch_fn": get_pdr_subscriptions_df,
                 "schema": subscriptions_schema,
+                "config": {
+                    "contract_list": contract_list,
+                },
+            },
+            "pdr_payouts": {
+                "fetch_fn": get_pdr_payouts_df,
+                "schema": payouts_schema,
                 "config": {
                     "contract_list": contract_list,
                 },
--- pdr_backend/subgraph/payout.py
+++ pdr_backend/subgraph/payout.py
@@ -1,15 +1,12 @@
+from typing import List
+
 from enforce_typing import enforce_types
+
 
 @enforce_types
 class Payout:
     def __init__(
-        self,
-        ID: str,
-        token: str,
-        user: str,
-        slot: int,
-        timestamp: int,
-        payout: float
+        self, ID: str, token: str, user: str, slot: int, timestamp: int, payout: float
     ) -> None:
         self.ID = ID
         self.user = user
@@ -17,3 +14,75 @@ class Payout:
         self.token = token
         self.slot = slot
         self.payout = payout
+
+
+@enforce_types
+def mock_payout(payout_tuple: tuple) -> Payout:
+    (ID, user, timestamp, token, slot, payout) = payout_tuple
+
+    return Payout(
+        ID=ID, user=user, timestamp=timestamp, token=token, slot=slot, payout=payout
+    )
+
+
+@enforce_types
+def mock_payouts() -> List[Payout]:
+    return [mock_payout(payout_tuple) for payout_tuple in _PAYOUT_TUPS]
+
+
+_PAYOUT_TUPS = [
+    (
+        # pylint: disable=line-too-long
+        "0x18f54cc21b7a2fdd011bea06bba7801b280e3151-1704152700-0xeb18bad7365a40e36a41fb8734eb0b855d13b74f",
+        "0xeb18bad7365a40e36a41fb8734eb0b855d13b74f",
+        1704153558000,
+        "ADA/USDT",
+        1704152700,
+        0.0,
+    ),
+    (
+        # pylint: disable=line-too-long
+        "0x18f54cc21b7a2fdd011bea06bba7801b280e3151-1704152700-0xfb223c3583aa934273173a55c226d598a149441c",
+        "0xfb223c3583aa934273173a55c226d598a149441c",
+        1704153051000,
+        "ADA/USDT",
+        1704152700,
+        3.786517720904995824,
+    ),
+    (
+        # pylint: disable=line-too-long
+        "0x18f54cc21b7a2fdd011bea06bba7801b280e3151-1704153000-0x02e9d2eede4c5347e55346860c8a8988117bde9e",
+        "0x02e9d2eede4c5347e55346860c8a8988117bde9e",
+        1704153351000,
+        "ADA/USDT",
+        1704153000,
+        3.687473663992716148,
+    ),
+    (
+        # pylint: disable=line-too-long
+        "0x18f54cc21b7a2fdd011bea06bba7801b280e3151-1704153000-0x04a5e9f565dfa83c77581d1022b9ef041f55210b",
+        "0x04a5e9f565dfa83c77581d1022b9ef041f55210b",
+        1704153504000,
+        "ADA/USDT",
+        1704153000,
+        6.334665366356455078,
+    ),
+    (
+        # pylint: disable=line-too-long
+        "0x18f54cc21b7a2fdd011bea06bba7801b280e3151-1704153000-0x7149ceca72c61991018ed80788bea3f3f4540c3c",
+        "0x7149ceca72c61991018ed80788bea3f3f4540c3c",
+        1704153534000,
+        "ADA/USDT",
+        1704153000,
+        1.463270654801637113,
+    ),
+    (
+        # pylint: disable=line-too-long
+        "0x18f54cc21b7a2fdd011bea06bba7801b280e3151-1704153000-0xeb18bad7365a40e36a41fb8734eb0b855d13b74f",
+        "0xeb18bad7365a40e36a41fb8734eb0b855d13b74f",
+        1704153558000,
+        "ADA/USDT",
+        1704153000,
+        0.0,
+    ),
+]
