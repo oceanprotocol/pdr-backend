34c0e1034f2230d0f83a62f3a8e76944278a796a
Mustafa Tuncay <mustafaislev@gmail.com>
2024-01-17 14:48:20 +0300

    black fix

--- pdr_backend/lake/test/conftest.py
+++ pdr_backend/lake/test/conftest.py
@@ -4,6 +4,7 @@ from pdr_backend.subgraph.prediction import mock_daily_predictions
 from pdr_backend.subgraph.subscription import mock_subscriptions
 from pdr_backend.subgraph.payout import mock_payouts
 
+
 @pytest.fixture()
 def sample_daily_predictions():
     return mock_daily_predictions()
