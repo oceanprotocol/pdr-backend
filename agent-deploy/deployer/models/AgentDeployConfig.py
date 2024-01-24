e1db1ef6a1a3a17cbaf6c5d3e5559f4a6e2136fa
trizin <25263018+trizin@users.noreply.github.com>
2024-01-24 13:46:32 +0000

    Add AgentDeployConfig model

---
+++ agent-deploy/deployer/models/AgentDeployConfig.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+from typing import List, Optional
+from deployer.models.SingleAgentConfig import SingleAgentConfig
+
+
+@dataclass
+class AgentsDeployConfig:
+    cpu: str
+    memory: str
+    agents: List[SingleAgentConfig]
+    approach: Optional[int] = None
+    pdr_backend_image_source: Optional[str] = None
+    source: Optional[str] = None
+    network: Optional[str] = None
+    s_until_epoch_end: Optional[int] = None
