56ff1138615d6641a8448e8394599b8fb3e3805a
trizin <25263018+trizin@users.noreply.github.com>
2024-01-24 14:02:40 +0000

    Refactor parse_config function to support multiple config types

--- agent-deploy/deployer/config.py
+++ agent-deploy/deployer/config.py
@@ -4,33 +4,42 @@ from deployer.models.AgentDeployConfig import AgentsDeployConfig
 from deployer.models.DeployConfig import DeployConfig
 from deployer.models.PredictoorAgentConfig import PredictoorAgentConfig
 
-def parse_config(file_path: str) -> DeployConfig:
+def parse_config(file_path: str, config_name: str) -> DeployConfig:
     with open(file_path, "r") as file:
-        config_data = yaml.safe_load(file)
+        config_file_data = yaml.safe_load(file)
+    
+    config_data = config_file_data[config_name]
 
-    predictoor_config_data = config_data["predictoor_config"]
-    if not "approach" in predictoor_config_data:
-        predictoor_config_data["approach"] = None
-    predictoor_agents = [
-        PredictoorAgentConfig(**agent) for agent in predictoor_config_data["agents"]
-    ]
-    predictoor_config = AgentsDeployConfig(
-        cpu=predictoor_config_data.get("cpu"),
-        memory=predictoor_config_data.get("memory"),
-        approach=predictoor_config_data.get("approach"),
-        agents=predictoor_agents,
-        pdr_backend_image_source=predictoor_config_data.get("pdr_backend_image_source"),
-        source=predictoor_config_data.get("source"),
-        network=predictoor_config_data.get("network"),
-        s_until_epoch_end=predictoor_config_data.get("s_until_epoch_end"),
+    if config_data.get("type") != "predictoor":
+        raise ValueError(f"Config type {config_data.get('type')} is not supported")
+    
+
+    agents = []
+    if not "approach" in config_data:
+        config_data["approach"] = None
+
+    if config_data.get("type") == "predictoor":
+        agents = [
+            PredictoorAgentConfig(**agent) for agent in config_data["agents"]
+        ]
+
+    agents_deploy_config = AgentsDeployConfig(
+        cpu=config_data.get("cpu"),
+        memory=config_data.get("memory"),
+        approach=config_data.get("approach"),
+        agents=agents,
+        pdr_backend_image_source=config_data.get("pdr_backend_image_source"),
+        source=config_data.get("source"),
+        network=config_data.get("network"),
+        s_until_epoch_end=config_data.get("s_until_epoch_end"),
     )
 
     deploy_config = DeployConfig(
-        predictoor_config=predictoor_config,
-        pdr_backend_image_source=config_data.get(
+        agent_config=agents_deploy_config,
+        pdr_backend_image_source=config_file_data.get(
             "pdr_backend_image_source", "oceanprotocol/pdr-backend:latest"
         ),
-        yaml_path=config_data.get("yaml_path", "./ppss.yaml"),
+        yaml_path=config_file_data.get("yaml_path", "./ppss.yaml"),
     )
     deploy_config.update_defaults()
 
