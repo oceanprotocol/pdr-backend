7306ff0cffbfd1066ed83a07d417bc4e6c7f1e4b
trizin <25263018+trizin@users.noreply.github.com>
2024-01-24 13:46:40 +0000

    Add DeployFile class to handle deployment files

---
+++ agent-deploy/deployer/models/DeployFile.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass
+from deployer.models.DeploymentMethod import DeploymentMethod
+
+
+@dataclass
+class DeployFile:
+    method: DeploymentMethod
+    content: str
+    name: str
+
+    def write(self, dir):
+        file_name = self.name.replace("/", "-")
+        with open(f"{dir}/{file_name}", "w", encoding="utf-8") as f:
+            f.write(self.content)
