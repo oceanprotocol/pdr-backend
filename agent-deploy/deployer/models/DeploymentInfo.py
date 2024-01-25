3d2c2c0038d9548d6e7d86f55c3f099c5a3f9c39
trizin <25263018+trizin@users.noreply.github.com>
2024-01-25 11:58:02 +0000

    Refactor DeploymentInfo class to add read method
    This commit refactors the DeploymentInfo class to add a new read method. The read method allows reading a DeploymentInfo object from a JSON file based on the provided config_name. This improves the flexibility and usability of the class.

--- agent-deploy/deployer/models/DeploymentInfo.py
+++ agent-deploy/deployer/models/DeploymentInfo.py
@@ -9,17 +9,22 @@ class DeploymentInfo:
     foldername: str
     config_name: str
     deployment_method: str
-    is_deployed: bool
     ts_created: int
     deployment_names: List[str]
 
     @property
     def name(self):
-        return f"{self.config_name}-{self.deployment_method}-{self.ts_created}"
+        return f"{self.config_name}-{self.deployment_method}"
 
     def write(self, path):
         with open(f"{path}/{self.name}.json", "w") as f:
             f.write(json.dumps(self.__dict__))
+
+    @classmethod
+    def read(cls, path, config_name):
+        with open(f"{path}/{config_name}.json", "r") as f:
+            config = json.loads(f.read())
+        return cls(**config)
 
     @classmethod
     def from_json(cls, path):
