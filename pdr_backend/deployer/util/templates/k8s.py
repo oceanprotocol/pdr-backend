3712bff91c724eb7a8cb81570604d14198bae89e
trizin <25263018+trizin@users.noreply.github.com>
2024-01-29 11:00:23 +0000

    Refactor run_command_args in get_k8s_predictoor_template function

--- pdr_backend/deployer/util/templates/k8s.py
+++ pdr_backend/deployer/util/templates/k8s.py
@@ -15,8 +15,7 @@ def get_k8s_predictoor_template(
     app = app.lower()
     name = name.lower()
     for arg in run_command[1:]:
-        arg_escaped = arg.replace('"', '\\"')
-        run_command_args += f'          - "{arg_escaped}"\n'
+        run_command_args += f'          - {arg}\n'
     template = f"""apiVersion: apps/v1
 kind: Deployment
 metadata:
