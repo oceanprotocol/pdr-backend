a1d4b971d60141366c2d37876248e4a61c9302e3
trizin <25263018+trizin@users.noreply.github.com>
2024-01-30 10:51:14 +0000

    Cpu and mem optional

--- pdr_backend/deployer/util/models/AgentDeployConfig.py
+++ pdr_backend/deployer/util/models/AgentDeployConfig.py
@@ -6,10 +6,10 @@ from pdr_backend.deployer.util.models.SingleAgentConfig import SingleAgentConfig
 # pylint: disable=too-many-instance-attributes
 @dataclass
 class AgentsDeployConfig:
-    cpu: str
-    memory: str
     agents: List[SingleAgentConfig]
     type: str
+    memory: Optional[str]
+    cpu: Optional[str]
     approach: Optional[int] = None
     pdr_backend_image_source: Optional[str] = None
     source: Optional[str] = None
