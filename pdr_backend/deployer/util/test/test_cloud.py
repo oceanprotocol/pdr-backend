8cef26245ee4c27ceb4d5e36f2d8b9798bb55db7
trizin <25263018+trizin@users.noreply.github.com>
2024-01-31 12:08:18 +0000

    Add tests for cloud module

---
+++ pdr_backend/deployer/util/test/test_cloud.py
@@ -0,0 +1,21 @@
+import pytest
+from unittest.mock import patch
+from pdr_backend.deployer.util.cloud import GCPProvider, AWSProvider, AzureProvider
+
+@patch('pdr_backend.deployer.util.cloud.run_command')
+def test_gcp_provider(mock_run_command):
+    gcp = GCPProvider('zone', 'project_id')
+    gcp.create_container_registry('registry_name')
+    mock_run_command.assert_called()
+
+@patch('pdr_backend.deployer.util.cloud.run_command')
+def test_aws_provider(mock_run_command):
+    aws = AWSProvider('region')
+    aws.create_container_registry('registry_name')
+    mock_run_command.assert_called()
+
+@patch('pdr_backend.deployer.util.cloud.run_command')
+def test_azure_provider(mock_run_command):
+    azure = AzureProvider('subscription_id', 'resource_group')
+    azure.create_container_registry('registry_name')
+    mock_run_command.assert_called()
