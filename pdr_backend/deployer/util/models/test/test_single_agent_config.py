8e4ef5b13f01d53c87b5dba73bb47610d6cb9675
trizin <25263018+trizin@users.noreply.github.com>
2024-02-02 10:44:24 +0000

    Test single agent config

---
+++ pdr_backend/deployer/util/models/test/test_single_agent_config.py
@@ -0,0 +1,19 @@
+import pytest
+from pdr_backend.deployer.util.models.SingleAgentConfig import SingleAgentConfig
+
+
+def test_single_agent_config_initialization():
+    private_key = "0x1"
+    config = SingleAgentConfig(private_key=private_key)
+    assert (
+        config.private_key == private_key
+    ), "Private key should be set correctly on initialization."
+
+
+def test_set_private_key():
+    config = SingleAgentConfig(private_key="initial_key")
+    new_private_key = "new_private_key"
+    config.set_private_key(new_private_key)
+    assert (
+        config.private_key == new_private_key
+    ), "set_private_key should update the private key."
