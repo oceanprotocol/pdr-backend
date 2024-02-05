a4a7b36c5a38864e1fdf04c4932517ba4d0093e8
trizin <25263018+trizin@users.noreply.github.com>
2024-02-05 17:12:58 +0300

    Formatting

--- pdr_backend/analytics/check_network.py
+++ pdr_backend/analytics/check_network.py
@@ -181,7 +181,7 @@ def check_network_main(ppss: PPSS, lookback_hours: int):
 
     dfbuyer_addr = addresses["dfbuyer"].lower()
     # 37500 * 1.201 = rewards + fees = total consume
-    token_amt = (37500 * 1.201)
+    token_amt = 37500 * 1.201
     # If token_amt is not a multiple of 60, adjust it to the next multiple of 60
     if token_amt % 60 != 0:
         token_amt = ((token_amt // 60) + 1) * 60
