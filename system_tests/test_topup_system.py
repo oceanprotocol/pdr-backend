d3be400a2710727aaa0eba99a93e4ce87cf52f69
Calina Cenan <calina@cenan.net>
2024-02-08 09:39:52 +0000

    Fixes to adjusted mocks.

--- system_tests/test_topup_system.py
+++ system_tests/test_topup_system.py
@@ -21,19 +21,20 @@ def test_topup():
     mock_web3_pp.web3_config = mock_web3_config
     mock_web3_pp.web3_config.owner = "0xowner"
 
+    mock_token = MagicMock(spec=Token)
+    balances_arr = [int(5000 * 1e18)] + [int(5 * 1e18)] * 100
+    mock_token.balanceOf.side_effect = balances_arr
+    mock_token.transfer.return_value = True
+    mock_token.name = "OCEAN"
+
     mock_token_rose = MagicMock(spec=NativeToken)
     balances_arr = [int(5000 * 1e18)] + [int(5 * 1e18)] * 100
     mock_token_rose.balanceOf.side_effect = balances_arr
     mock_token_rose.transfer.return_value = True
     mock_token_rose.name = "ROSE"
 
-    mock_token = MagicMock(spec=Token)
-    balances_arr = [int(5000 * 1e18)] + [int(5 * 1e18)] * 100
-    mock_token.balanceOf.side_effect = balances_arr
-    mock_token.transfer.return_value = True
-
     mock_web3_pp.OCEAN_Token = mock_token
-    mock_web3_pp.NativeToken = mock_token
+    mock_web3_pp.NativeToken = mock_token_rose
 
     with patch("pdr_backend.ppss.ppss.Web3PP", return_value=mock_web3_pp), patch(
         "sys.exit"
