12a4cfdbe94086cf3a46b29680a976d62e2f7cc5
Mustafa Tuncay <mustafaislev@gmail.com>
2024-02-14 18:28:44 +0300

    issue572: DuckDB implementation 2

--- pdr_backend/lake/partitioned_data_store.py
+++ pdr_backend/lake/partitioned_data_store.py
@@ -1,17 +1,28 @@
+from typing import Literal
+
 import hashlib
 import os
 import polars as pl
 import duckdb
 from enforce_typing import enforce_types
 
-
 class PartitionedDataStore:
     @enforce_types
     def __init__(self, base_directory=str):
+        """
+        Initialize a PartitionedDataStore instance.
+        @arguments:
+            base_directory - The base directory to store the partitioned Parquet files.
+        """
+
         self.base_directory = base_directory
         self.duckdb_conn = duckdb.connect(
             database=":memory:"
         )  # Keep a persistent connection
+        self.partition_patterns = {
+            "date": "/year=*/month=*/day=*/*.parquet",
+            "address": "/address=*/*.parquet",
+        }
 
     @enforce_types
     def _generate_view_name(self, base_path=str) -> str:
@@ -27,9 +38,9 @@ class PartitionedDataStore:
         return f"dataset_{hash_object.hexdigest()}"
 
     @enforce_types
-    def append_data(self, df: pl.DataFrame, dataset_identifier: str):
+    def create_and_fill_table(self, df: pl.DataFrame, dataset_identifier: str):
         """
-        Append data to an in-memory dataset.
+        Create the dataset and insert data to the in-memory dataset.
         @arguments:
             df - The Polars DataFrame to append.
             dataset_identifier - A unique identifier for the dataset.
@@ -41,55 +52,33 @@ class PartitionedDataStore:
         self.duckdb_conn.register(view_name, df)
 
     @enforce_types
-    def finalize_data(self, dataset_identifier: str):
+    def insert_to_table(self, df: pl.DataFrame, dataset_identifier: str):
         """
-        Finalizes the data by writing the in-memory dataset to partitioned Parquet files.
+        Insert data to an in-memory dataset.
         @arguments:
+            df - The Polars DataFrame to append.
             dataset_identifier - A unique identifier for the dataset.
         """
 
         view_name = self._generate_view_name(self.base_directory + dataset_identifier)
-        query = f"SELECT * FROM {view_name}"
-        result_df = self.duckdb_conn.execute(query).df()
+        # Check if the table exists
+        tables = self.duckdb_conn.execute(
+            "SELECT table_name FROM information_schema.tables WHERE table_schema = 'main'"
+        ).fetchall()
 
-        # Convert to Polars DataFrame for partitioned write
-        df = pl.DataFrame(result_df)
-
-        lazy_df = df.lazy()
-        # Determine partitions based on a timestamp column, assuming 'timestamp' column exists
-        lazy_df = lazy_df.with_columns(
-            [pl.from_epoch("timestamp", time_unit="s").alias("date")]
-        )
-        df = lazy_df.collect()
-
-        partitions = (
-            lazy_df.select(
-                pl.col("date").dt.year().alias("year"),
-                pl.col("date").dt.month().alias("month"),
-                pl.col("date").dt.day().alias("day"),
+        if view_name in [table[0] for table in tables]:
+            values_str = ", ".join(
+                [
+                    f"({', '.join([str(x) for x in row])})"
+                    for row in df.rows()
+                ]
             )
-            .unique()
-            .collect()
-        )
-
-        for partition in partitions.rows():
-            year, month, day = partition
-            partition_path = os.path.join(
-                self.base_directory,
-                dataset_identifier,
-                f"year={year}",
-                f"month={month}",
-            )
-            os.makedirs(partition_path, exist_ok=True)
-            partition_df = df.filter(
-                (pl.col("date").dt.year() == year)
-                & (pl.col("date").dt.month() == month)
-            )
-            file_path = os.path.join(partition_path, f"{day}.parquet")
-            partition_df.write_parquet(file_path)
+            self.duckdb_conn.execute(f"INSERT INTO {view_name} VALUES {values_str}")
+        else:
+            self.create_and_fill_table(df, dataset_identifier)
 
     @enforce_types
-    def query_data(self, dataset_identifier: str, query: str) -> pl.DataFrame:
+    def create_view_and_query_data(self, dataset_identifier: str, query: str) -> pl.DataFrame:
         """
         Execute a SQL query across partitioned Parquet files using DuckDB.
         @arguments:
@@ -105,10 +94,129 @@ class PartitionedDataStore:
 
         # DuckDB automatically recognizes the partition scheme when reading from a directory
         self.duckdb_conn.execute(
-            f"CREATE OR REPLACE VIEW {view_name} AS SELECT * FROM read_parquet('{dataset_path}/*/*/*.parquet', hive_partitioning=1)"  # pylint: disable=line-too-long
+            f"""CREATE OR REPLACE VIEW {view_name} AS 
+            SELECT * FROM read_parquet(
+                '{dataset_path}{self.partition_patterns["date"]}', 
+                hive_partitioning=1
+            )"""
         )
 
         # Execute the provided SQL query
         result_df = self.duckdb_conn.execute(query.format(view_name=view_name)).df()
 
         return pl.DataFrame(result_df)
+
+    @enforce_types
+    def query_data(
+        self,
+        dataset_identifier: str,
+        query: str,
+        partition_type: Literal["date", "address"]
+    ) -> pl.DataFrame:
+        """
+            Execute a SQL query directly on partitioned Parquet files using DuckDB.
+                @arguments:
+                    dataset_identifier - A unique identifier for the dataset.
+                    query - The SQL query to execute. 
+                        The query must contain a {dataset_path} placeholder.
+
+                @returns:
+                    pl.DataFrame - The result of the query.
+        """
+
+        dataset_path = os.path.join(self.base_directory, dataset_identifier)
+
+        # DuckDB automatically recognizes the partition scheme when reading from a directory
+        result_df = self.duckdb_conn.execute(
+            query.format(dataset_path=f"""
+                         read_parquet(
+                            '{dataset_path}{self.partition_patterns[partition_type]}', 
+                            hive_partitioning=1
+                         )""")
+        ).df()
+
+        return pl.DataFrame(result_df)
+
+    @enforce_types
+    def _export_data_with_address_hive(self, dataset_identifier: str, address_column: str):
+        """
+        Finalizes the data by writing the in-memory dataset to partitioned
+        Parquet files using the COPY command.
+        The data is partitioned by the Ethereum address.
+        @arguments:
+            dataset_identifier - A unique identifier for the dataset.
+            address_column - The name of the column containing the Ethereum addresses.
+        """
+
+        view_name = self._generate_view_name(self.base_directory + dataset_identifier)
+
+        # Use the COPY command to write the data to the partitioned Parquet files
+        self.duckdb_conn.execute(f"""COPY (
+                                     SELECT *, {address_column} FROM {view_name}
+                                ) TO '{self.base_directory}' 
+                                (
+                                    FORMAT PARQUET, 
+                                    PARTITION BY ({address_column}), 
+                                    OVERWRITE=TRUE
+                                )"""
+                                )
+
+    @enforce_types
+    def _export_data_with_date_hive(self, dataset_identifier: str, date_column: str = "timestamp"):
+        """
+        Finalizes the data by writing the in-memory dataset to 
+        partitioned Parquet files using the COPY command.
+        @arguments:
+            dataset_identifier - A unique identifier for the dataset.
+        """
+
+        view_name = self._generate_view_name(self.base_directory + dataset_identifier)
+
+        # Use the COPY command to write the data to the partitioned Parquet files
+        self.duckdb_conn.execute(
+            f"""COPY (
+                    SELECT *, 
+                    EXTRACT(year FROM {date_column}) as year, 
+                    EXTRACT(month FROM {date_column}) as month, 
+                    EXTRACT(day FROM {date_column}) as day FROM {view_name}
+                ) TO '{self.base_directory}' 
+                (FORMAT PARQUET, 
+                PARTITION BY (year, month, day), 
+                OVERWRITE=TRUE)"""
+            )
+    
+    @enforce_types
+    def _export_to_target(self, dataset_identifier: str, output_path: str):
+        """
+        Export the in-memory dataset to a single Parquet file.
+        @arguments:
+            dataset_identifier - A unique identifier for the dataset.
+            output_path - The path to the output Parquet file.
+        """
+
+        view_name = self._generate_view_name(self.base_directory + dataset_identifier)
+        self.duckdb_conn.execute(f"COPY {view_name} TO '{output_path}' (FORMAT PARQUET)")
+
+    @enforce_types
+    def export_to_parquet(
+        self,
+        dataset_identifier: str,
+        output_path: str = None,
+        partition_type: Literal["date", "address"] = "date",
+        partition_column: str = None
+    ):
+        """
+        Finalizes the data by writing the in-memory dataset to partitioned Parquet files.
+        @arguments:
+            dataset_identifier - A unique identifier for the dataset.
+            output_path - The path to the output Parquet file.
+            partition_type - The type of partitioning to use.
+            partition_column - The name of the column to partition by.
+        """
+
+        if partition_type == "date":
+            self._export_data_with_date_hive(dataset_identifier, partition_column)
+        elif partition_type == "address":
+            self._export_data_with_address_hive(dataset_identifier, partition_column)
+        else:
+            self._export_to_target(dataset_identifier, output_path)
--- pdr_backend/lake/test/test_partitioned_data_store.py
+++ pdr_backend/lake/test/test_partitioned_data_store.py
@@ -10,13 +10,13 @@ def _get_test_manager(tmpdir):
     return PartitionedDataStore(str(tmpdir))
 
 
-def test_append_data(tmpdir):
+def create_and_fill_table(tmpdir):
     test_manager = _get_test_manager(tmpdir)
     example_df = pl.DataFrame(
         {"timestamp": ["2022-01-01", "2022-02-01", "2022-03-01"], "value": [10, 20, 30]}
     )
     dataset_identifier = "test_append"
-    test_manager.append_data(example_df, dataset_identifier)
+    test_manager.insert_to_table(example_df, dataset_identifier)
 
     # Check if the view is registered
     view_name = test_manager._generate_view_name(str(tmpdir) + dataset_identifier)
@@ -24,10 +24,9 @@ def test_append_data(tmpdir):
         "SELECT table_name FROM information_schema.tables WHERE table_schema = 'main'"
     ).fetchall()
     assert view_name in [table[0] for table in tables]
-    # Since append_data doesn't immediately reflect in the filesystem, this test is limited
+    # Since insert_to_table doesn't immediately reflect in the filesystem, this test is limited
 
-
-def test_finalize_data(tmpdir):
+def test_finalize_data_with_date_hive(tmpdir):
     test_manager = _get_test_manager(tmpdir)
     dataset_identifier = "test_finalize"
     # 1643673600 is 2022-02-01 00:00:00 in Unix time
@@ -36,18 +35,41 @@ def test_finalize_data(tmpdir):
     example_df = pl.DataFrame(
         {"timestamp": [1640995200, 1643673600, 1640995200], "value": [10, 20, 30]}
     )
-    test_manager.append_data(example_df, dataset_identifier)
-    test_manager.finalize_data(dataset_identifier)
+    test_manager.insert_to_table(example_df, dataset_identifier)
+    test_manager.export_to_parquet(
+        dataset_identifier,
+        partition_type="date",
+        partition_column="timestamp"
+    )
 
     # Check if the files were created
     expected_path = os.path.join(
-        str(tmpdir), dataset_identifier, "year=2022", "month=1", "1.parquet"
+        str(tmpdir), dataset_identifier, "year=2022", "month=1", "day=1", "data.parquet"
     )
     print("expected_path", expected_path)
     assert os.path.exists(expected_path), "Parquet file was not created as expected."
 
+def test_finalize_data_with_address_hive(tmpdir):
+    test_manager = _get_test_manager(tmpdir)
+    dataset_identifier = "test_finalize_data_with_address_hive"
 
-def test_query_data(tmpdir):
+    example_df = pl.DataFrame(
+        {"address": ['0xasset1', '0xasset1', '0xasset2'], "value": [10, 20, 30]}
+    )
+    test_manager.insert_to_table(example_df, dataset_identifier)
+    test_manager.export_to_parquet(
+        dataset_identifier,
+        partition_type="address",
+        partition_column="address"
+    )
+
+    # Check if the files were created
+    expected_path = os.path.join(
+        str(tmpdir), dataset_identifier, "address=0xasset1", "data.parquet"
+    )
+    print("expected_path", expected_path)
+   
+def test_create_view_and_query_data(tmpdir):
     test_manager = _get_test_manager(tmpdir)
     dataset_identifier = "test_query"
     # 1643673600 is 2022-02-01 00:00:00 in Unix time
@@ -56,10 +78,17 @@ def test_query_data(tmpdir):
     example_df = pl.DataFrame(
         {"timestamp": [1640995200, 1643673600, 1640995200], "value": [10, 20, 30]}
     )
-    test_manager.append_data(example_df, dataset_identifier)
-    test_manager.finalize_data(dataset_identifier)
+    test_manager.insert_to_table(example_df, dataset_identifier)
+    test_manager.finalize_data_with_date_hive(dataset_identifier)
 
-    query_result = test_manager.query_data(
+    query_result = test_manager.create_view_and_query_data(
         dataset_identifier, "SELECT * FROM {view_name} WHERE value > 15"
     )
     assert len(query_result) == 2, "Query did not return the expected number of rows."
+
+    # Check if the view is registered
+    view_name = test_manager._generate_view_name(str(tmpdir) + dataset_identifier)
+    tables = test_manager.duckdb_conn.execute(
+        "SELECT table_name FROM information_schema.tables WHERE table_schema = 'main'"
+    ).fetchall()
+    assert view_name in [table[0] for table in tables]
