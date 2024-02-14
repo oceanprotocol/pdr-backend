996013ec6c3ed23dde8938c08568389a7a246b11
trizin <25263018+trizin@users.noreply.github.com>
2024-02-14 16:08:06 +0300

    Fix checks

--- pdr_backend/ppss/predictoor_ss.py
+++ pdr_backend/ppss/predictoor_ss.py
@@ -13,8 +13,12 @@ class PredictoorSS(SingleFeedMixin, StrMixin):
     @enforce_types
     def __init__(self, d: dict):
         super().__init__(d, assert_feed_attributes=["timeframe"])
-        self.regressionmodel_ss = RegressionModelSS(d["regressionmodel_ss"])
-        self.classifiermodel_ss = ClassifierModelSS(d["classifiermodel_ss"])
+        if "regressionmodel_ss" in d:
+            self.regressionmodel_ss = RegressionModelSS(d["regressionmodel_ss"])
+        if "classifiermodel_ss" in d:
+            self.classifiermodel_ss = ClassifierModelSS(d["classifiermodel_ss"])
+        if not "classifiermodel_ss" in d and not "regressionmodel_ss" in d:
+            raise ValueError("No model provided")
 
     # --------------------------------
     # yaml properties
