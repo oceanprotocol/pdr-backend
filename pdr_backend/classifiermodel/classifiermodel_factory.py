d60704774e18bf68b824957ee74ca22a3900e551
trizin <25263018+trizin@users.noreply.github.com>
2024-02-14 14:41:16 +0300

    Strip out other approaches

--- pdr_backend/classifiermodel/classifiermodel_factory.py
+++ pdr_backend/classifiermodel/classifiermodel_factory.py
@@ -18,16 +18,4 @@ class ClassifierModelFactory:
 
     def _model(self):
         a = self.classifiermodel_ss.approach
-        if a == "LIN":
-            return linear_model.LinearRegression()
-        if a == "GPR":
-            kernel = 1.0 * RBF(length_scale=1e1, length_scale_bounds=(1e-2, 1e3))
-            return GaussianProcessRegressor(kernel=kernel, alpha=0.0)
-        if a == "SVR":
-            return svm.SVR()
-        if a == "NuSVR":
-            return svm.NuSVR()
-        if a == "LinearSVR":
-            return svm.LinearSVR()
-
-        raise ValueError(a)
+        return linear_model.LinearRegression()
