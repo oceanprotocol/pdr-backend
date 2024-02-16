9b1ca99e427f224babfb6b642d20cb3271e1a984
trizin <25263018+trizin@users.noreply.github.com>
2024-02-16 11:47:03 +0000

    Fix import error in BaseContract constructor

--- pdr_backend/contract/base_contract.py
+++ pdr_backend/contract/base_contract.py
@@ -4,12 +4,13 @@ from typing import Optional
 from enforce_typing import enforce_types
 from sapphirepy import wrapper
 
-from pdr_backend.ppss.web3_pp import Web3PP
 
 @enforce_types
 class BaseContract(ABC):
-    def __init__(self, web3_pp: Web3PP, address: str, contract_name: str, pk_name: Optional[str]):
+    def __init__(self, web3_pp, address: str, contract_name: str, pk_name: Optional[str]):
         super().__init__()
+        from pdr_backend.ppss.web3_pp import Web3PP
+        assert isinstance(web3_pp, Web3PP), "web3_pp must be an instance of Web3PP"
         self.web3_pp = web3_pp
         if pk_name:
             self.config = web3_pp.web3_config_by_env(pk_name)
