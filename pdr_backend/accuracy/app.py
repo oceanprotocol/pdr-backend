3290335ca31244b031c411ba96563e3e635c8106
Calina Cenan <calina@cenan.net>
2024-02-21 10:53:55 +0000

    Add time types to accuracy module.

--- pdr_backend/accuracy/app.py
+++ pdr_backend/accuracy/app.py
@@ -13,6 +13,7 @@ from pdr_backend.subgraph.subgraph_predictions import (
     ContractIdAndSPE,
 )
 from pdr_backend.subgraph.subgraph_slot import fetch_slots_for_all_assets, PredictSlot
+from pdr_backend.util.time_types import UnixTimeSeconds
 
 app = Flask(__name__)
 JSON_FILE_PATH = "pdr_backend/accuracy/output/accuracy_data.json"
@@ -50,7 +51,7 @@ def calculate_prediction_result(
 
 @enforce_types
 def process_single_slot(
-    slot: PredictSlot, end_of_previous_day_timestamp: int
+    slot: PredictSlot, end_of_previous_day_timestamp: UnixTimeSeconds
 ) -> Optional[Tuple[float, float, int, int]]:
     """
     Processes a single slot and calculates the staked amounts for yesterday and today,
@@ -72,7 +73,9 @@ def process_single_slot(
         return None
 
     # split the id to get the slot timestamp
-    timestamp = int(slot.ID.split("-")[1])  # Using dot notation for attribute access
+    timestamp = UnixTimeSeconds(
+        int(slot.ID.split("-")[1])
+    )  # Using dot notation for attribute access
 
     if (
         end_of_previous_day_timestamp - SECONDS_IN_A_DAY
@@ -109,7 +112,7 @@ def process_single_slot(
 
 @enforce_types
 def aggregate_statistics(
-    slots: List[PredictSlot], end_of_previous_day_timestamp: int
+    slots: List[PredictSlot], end_of_previous_day_timestamp: UnixTimeSeconds
 ) -> Tuple[float, float, int, int]:
     """
     Aggregates statistics across all provided slots for an asset.
@@ -152,8 +155,8 @@ def aggregate_statistics(
 def calculate_statistics_for_all_assets(
     asset_ids: List[str],
     contracts_list: List[ContractIdAndSPE],
-    start_ts_param: int,
-    end_ts_param: int,
+    start_ts_param: UnixTimeSeconds,
+    end_ts_param: UnixTimeSeconds,
     network: str = "mainnet",
 ) -> Dict[str, Dict[str, Any]]:
     """
@@ -182,7 +185,9 @@ def calculate_statistics_for_all_assets(
             staked_today,
             correct_predictions_count,
             slots_evaluated,
-        ) = aggregate_statistics(slots, end_ts_param - SECONDS_IN_A_DAY)
+        ) = aggregate_statistics(
+            slots, UnixTimeSeconds(end_ts_param - SECONDS_IN_A_DAY)
+        )
         average_accuracy = (
             0
             if correct_predictions_count == 0
@@ -210,7 +215,9 @@ def calculate_statistics_for_all_assets(
 
 
 @enforce_types
-def calculate_timeframe_timestamps(contract_timeframe: str) -> Tuple[int, int]:
+def calculate_timeframe_timestamps(
+    contract_timeframe: str,
+) -> Tuple[UnixTimeSeconds, UnixTimeSeconds]:
     """
     Calculates and returns a tuple of Unix timestamps for a start and end time
     based on a given contract timeframe. The start time is determined to be either
@@ -225,7 +232,7 @@ def calculate_timeframe_timestamps(contract_timeframe: str) -> Tuple[int, int]:
         Tuple[int, int]: A tuple containing the start and end Unix timestamps.
     """
 
-    end_ts = int(datetime.utcnow().timestamp())
+    end_ts = UnixTimeSeconds(int(datetime.utcnow().timestamp()))
     time_delta = (
         timedelta(weeks=1)
         if contract_timeframe == "5m"
@@ -234,7 +241,7 @@ def calculate_timeframe_timestamps(contract_timeframe: str) -> Tuple[int, int]:
         # if contract_timeframe == "5m"
         # else timedelta(days=1)
     )
-    start_ts = int((datetime.utcnow() - time_delta).timestamp())
+    start_ts = UnixTimeSeconds(int((datetime.utcnow() - time_delta).timestamp()))
 
     return start_ts, end_ts
 
