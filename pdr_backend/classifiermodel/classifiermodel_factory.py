ed2e5347e54d42ffd9a30455943284e7222c78bb
trizin <25263018+trizin@users.noreply.github.com>
2024-02-21 13:24:52 +0000

    Function to scale training data

--- pdr_backend/classifiermodel/classifiermodel_factory.py
+++ pdr_backend/classifiermodel/classifiermodel_factory.py
@@ -1,5 +1,6 @@
 from enforce_typing import enforce_types
 from sklearn import linear_model
+from sklearn.preprocessing import StandardScaler
 
 from pdr_backend.ppss.classifiermodel_ss import ClassifierModelSS
 
@@ -8,6 +9,10 @@ from pdr_backend.ppss.classifiermodel_ss import ClassifierModelSS
 class ClassifierModelFactory:
     def __init__(self, classifiermodel_ss: ClassifierModelSS):
         self.classifiermodel_ss = classifiermodel_ss
+        self.scaler = StandardScaler()
+
+    def scale_train_data(self, X_train):
+        return self.scaler.fit_transform(X_train)
 
     def build(self, X_train, y_train):
         model = self._model()
