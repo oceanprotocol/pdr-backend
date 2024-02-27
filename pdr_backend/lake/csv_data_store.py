1e3892b2f6f0806ed9bd3cd062413d08131181e6
Mustafa Tuncay <mustafaislev@gmail.com>
2024-02-27 21:21:53 +0300

    CSV data handler - part 1

---
+++ pdr_backend/lake/csv_data_store.py
@@ -0,0 +1,268 @@
+import os
+from typing import List, Optional
+import polars as pl
+
+class CSVDataStore:
+    def __init__(self, base_path: str):
+        self.base_path = base_path
+
+    def _get_folder_path(self, dataset_identifier: str) -> str:
+        """
+        Returns the folder path for the given dataset_identifier.
+        If the folder does not exist, it will be created.
+        @args:
+            dataset_identifier: str - identifier of the dataset
+        """
+        folder_path = os.path.join(self.base_path, dataset_identifier)
+        os.makedirs(folder_path, exist_ok=True)
+        return folder_path
+
+    def _create_file_name(
+            self,
+            dataset_identifier: str,
+            start_time: int,
+            end_time: int,
+            row_count: int
+        ) -> str:
+        """
+        Creates a file name using the given dataset_identifier, 
+        start_time, end_time, and row_count.
+        @args:
+            dataset_identifier: str - identifier of the dataset
+            start_time: int - start time of the data TIMESTAMP
+            end_time: int - end time of the data TIMESTAMP
+            row_count: int - number of rows in the data
+        """
+        fill_with_zero_start = 10 - len(str(start_time))
+        fill_with_zero_end = 10 - len(str(end_time))
+        start_time = f"{fill_with_zero_start * '0'}{start_time}"
+        end_time = f"{fill_with_zero_end * '0'}{end_time}"
+
+        return f"{dataset_identifier}_from_{start_time}_to_{end_time}_{row_count}.csv"
+
+    def _create_file_path(
+            self,
+            dataset_identifier: str,
+            start_time: int,
+            end_time: int,
+            row_count: int
+        ) -> str:
+        """
+        Creates the file path for the given dataset_identifier,
+        start_time, end_time, and row_count.
+        @args:
+            dataset_identifier: str - identifier of the dataset
+            start_time: str - start time of the data
+            end_time: str - end time of the data
+            row_count: int - number of rows in the data
+        """
+
+        file_name = self._create_file_name(dataset_identifier, start_time, end_time, row_count)
+        folder_path = self._get_folder_path(dataset_identifier)
+        return os.path.join(folder_path, file_name)
+
+    def _chunk_data(self, data: pl.DataFrame) -> List[pl.DataFrame]:
+        """
+        Splits the given data into chunks of up to 1000 rows each.
+        @args:
+            data: pl.DataFrame - data to be chunked
+        """
+        return [data.slice(i, min(1000, len(data) - i)) for i in range(0, len(data), 1000)]
+
+    def write(self, dataset_identifier: str, data: pl.DataFrame):
+        """
+        Writes the given data to a csv file in the folder
+        corresponding to the given dataset_identifier.
+        @args:
+            data: pl.DataFrame - The data to write, it has to be sorted by timestamp
+            dataset_identifier: str - The dataset identifier
+        """
+
+        last_file_row_count = self._get_last_file_row_count(dataset_identifier)
+        if last_file_row_count is not None:
+            if last_file_row_count < 1000:
+                remaining_rows = 1000 - last_file_row_count
+
+                # get the first remaining_rows rows
+                if len(data) < remaining_rows:
+                    remaining_rows = len(data)
+
+                remaining_data = data.slice(0, remaining_rows)
+
+                last_file_path = self._get_last_file_path(
+                    self._get_folder_path(dataset_identifier))
+                last_file_data = pl.read_csv(last_file_path)
+                last_file_data = last_file_data.vstack(remaining_data)
+
+                t_start_time = last_file_data['timestamp'][0]
+                t_end_time = last_file_data['timestamp'][-1]
+
+                last_file_data.write_csv(last_file_path)
+                # change the name of the file to reflect the new row count
+                new_file_path = self._create_file_path(
+                    dataset_identifier, t_start_time, t_end_time, len(last_file_data))
+
+                print("new_file_path", new_file_path)
+                os.rename(last_file_path, new_file_path)
+
+                data = data.slice(remaining_rows, len(data) - remaining_rows)
+
+        chunks = [
+            data.slice(i, min(1000, len(data) - i))
+            for i in range(0, len(data), 1000)]
+
+        for i, chunk in enumerate(chunks):
+            start_time = int(chunk['timestamp'][0])
+            end_time = int(chunk['timestamp'][-1])
+            file_path = self._create_file_path(dataset_identifier, start_time, end_time, len(chunk))
+            chunk.write_csv(file_path)
+
+    def bulk_write(self, data_list: List[pl.DataFrame], dataset_identifier: str):
+        """
+        Writes the given list of data to csv files in the folder
+        corresponding to the given dataset_identifier.
+        @args:
+            data_list: List[pl.DataFrame] - list of data to be written
+            dataset_identifier: str - identifier of the dataset
+        """
+        for data in data_list:
+            self.write(data, dataset_identifier)
+
+    def _get_file_paths(
+            self,
+            folder_path: str,
+            start_time: str,
+            end_time: str
+        ) -> List[str]:
+        """
+        Returns a list of file paths in the given folder_path 
+        that contain the given start_time and end_time.
+        @args:
+            folder_path: str - path of the folder
+            start_time: str - start time of the data
+            end_time: str - end time of the data
+        @returns:
+            List[str] - list of file paths
+        """
+
+        file_names = os.listdir(folder_path)
+        file_paths = [os.path.join(folder_path, file_name) for file_name in file_names]
+        
+        #find files which has a higher start time and lower end time
+        print("file_paths_aaaa", file_paths)
+        file_paths = [
+            file_path for file_path in file_paths
+            #firstly, take the filename from the path (/path/to/file.csv -> file.csv)
+            #then, split the filename by "_" and take the 4th and 5th elements
+            #then, convert them to int and check if they are in the range
+            if int(file_path.split("/")[-1].split("_")[2]) >= int(start_time)
+            and int(file_path.split("/")[-1].split("_")[4]) <= int(end_time)]
+        
+        return file_paths
+
+    def read(
+            self,
+            dataset_identifier: str,
+            start_time: str,
+            end_time: str
+        ) -> pl.DataFrame:
+        """
+        Reads the data from the csv file in the folder
+        corresponding to the given dataset_identifier, 
+        start_time, and end_time.
+        @args:
+            dataset_identifier: str - identifier of the dataset
+            start_time: str - start time of the data
+            end_time: str - end time of the data
+        @returns:
+            pl.DataFrame - data read from the csv file
+        """
+        folder_path = self._get_folder_path(dataset_identifier)
+        file_paths = self._get_file_paths(folder_path, start_time, end_time)
+        return pl.read_csv(file_paths[0]) if file_paths else pl.DataFrame()
+
+    def read_all(
+            self,
+            dataset_identifier: str
+        ) -> pl.DataFrame:
+        """
+        Reads all the data from the csv files in the folder
+        corresponding to the given dataset_identifier.
+        @args:
+            dataset_identifier: str - identifier of the dataset
+        @returns:
+            pl.DataFrame - data read from the csv files
+        """
+
+        folder_path = self._get_folder_path(dataset_identifier)
+        file_names = os.listdir(folder_path)
+        file_paths = [os.path.join(folder_path, file_name) for file_name in file_names]
+        file_paths.sort()
+        
+        # print("read_all_file_paths", file_paths)
+        if file_paths:
+        # Read the first file to create the DataFrame
+            data = pl.read_csv(file_paths[0])
+            # Read the remaining files and append them to the DataFrame
+            for file_path in file_paths[1:]:
+                data = data.vstack(pl.read_csv(file_path))
+            return data
+        else:
+            return pl.DataFrame()
+
+    def _get_last_file_path(
+            self,
+            folder_path: str
+        ) -> str:
+        """
+        Returns the path of the last file in the given folder_path.
+        @args:
+            folder_path: str - path of the folder
+        @returns:
+            str - path of the last file
+        """
+
+        file_names = sorted(os.listdir(folder_path))
+        return os.path.join(folder_path, file_names[-1]) if file_names else ""
+
+    def get_last_timestamp(
+            self,
+            dataset_identifier: str
+        ) -> Optional[int]:
+        """
+        Returns the last timestamp from the csv files in the folder
+        corresponding to the given dataset_identifier.
+        @args:
+            dataset_identifier: str - identifier of the dataset
+        @returns:
+            Optional[int] - last timestamp from the csv files
+        """
+        folder_path = self._get_folder_path(dataset_identifier)
+        last_file_path = self._get_last_file_path(folder_path)
+        if len(last_file_path):
+            return int(last_file_path.split("_")[3])
+        else:
+            return None
+
+    def _get_last_file_row_count(self, dataset_identifier: str) -> Optional[int]:
+        """
+        Returns the row count of the last file for the given dataset_identifier.
+        @args:
+            dataset_identifier: str - The dataset identifier
+        @returns:
+            row_count: Optional[int] - The row count of the last file
+        """
+
+        folder_path = self._get_folder_path(dataset_identifier)
+        file_names = os.listdir(folder_path)
+
+        # Sort by file name
+        file_names.sort()
+        if len(file_names) == 0:
+            return None
+
+        last_file_path = os.path.join(folder_path, file_names[-1])
+
+        # parse the row count from the file name
+        row_count = int(last_file_path.split("_")[-1].split(".")[0])
+        return row_count
---
+++ pdr_backend/lake/test/test_csv_data_store.py
@@ -0,0 +1,153 @@
+import polars as pl
+import os
+from pdr_backend.lake.csv_data_store import CSVDataStore
+
+def _get_test_manager(tmpdir):
+    return CSVDataStore(str(tmpdir))
+
+def _clean_up(tmpdir):
+    for root, dirs, files in os.walk(tmpdir):
+        for file in files:
+            os.remove(os.path.join(root, file))
+        for dir in dirs:
+            # clean up the directory
+            _clean_up(os.path.join(root, dir))
+            os.rmdir(os.path.join(root, dir))
+
+def test_get_folder_path(tmpdir):
+    manager = _get_test_manager(tmpdir)
+    folder_path = manager._get_folder_path("test")
+    assert folder_path == f"{tmpdir}/test"
+
+def test_create_file_name(tmpdir):
+    manager = _get_test_manager(tmpdir)
+    file_name = manager._create_file_name("test", 0, 1, 2)
+    print("file_name", file_name)
+    assert file_name == "test_from_0000000000_to_0000000001_2.csv"
+
+def test_get_file_paths(tmpdir):
+    manager = _get_test_manager(tmpdir)
+    file_name_1 = manager._create_file_name("test", 0, 20, 20)
+    file_name_2 = manager._create_file_name("test", 21, 40, 20)
+    file_name_3 = manager._create_file_name("test", 41, 60, 20)
+    file_name_4 = manager._create_file_name("test", 61, 80, 20)
+
+    files = [file_name_1, file_name_2, file_name_3, file_name_4]
+
+    folder_path = manager._get_folder_path("test")
+    
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+
+    for file in files:
+        # create empty files
+        with open(
+            os.path.join(folder_path, file)
+            , "w") as f:
+            pass
+
+    # check if empty files are created
+    for file in files:
+        assert os.path.exists(folder_path + "/" + file)
+
+    file_paths = manager._get_file_paths(folder_path, 21, 60)
+
+    for file_path in file_paths:
+        assert file_path in [folder_path + "/" + file_name_2, folder_path + "/" + file_name_3]
+
+    _clean_up(tmpdir)
+
+def test_create_file_path(tmpdir):
+    manager = _get_test_manager(tmpdir)
+    file_path = manager._create_file_path("test", 1, 2, 2)
+    assert file_path == f"{tmpdir}/test/test_from_0000000001_to_0000000002_2.csv"
+
+def test_read(tmpdir):
+    manager = _get_test_manager(tmpdir)
+    file_path = manager._create_file_path("test", 1, 2, 2)
+
+    with open(file_path, "w") as file:
+        file.write("a,b,c\n1,2,3\n4,5,6")
+
+    data = manager.read("test", 1, 2)
+    assert data.equals(pl.DataFrame({"a": [1, 4], "b": [2, 5], "c": [3, 6]}))
+
+    _clean_up(tmpdir)
+
+def test_read_all(tmpdir):
+    manager = _get_test_manager(tmpdir)
+
+    file_path_1 = manager._create_file_path("test", 0, 20, 2)
+    file_path_2 = manager._create_file_path("test", 21, 41, 2)
+
+    with open(file_path_1, "w") as file:
+        file.write("a,b,c\n1,2,3\n4,5,6")
+
+    with open(file_path_2, "w") as file:
+        file.write("a,b,c\n7,8,9\n10,11,12")
+
+    data = manager.read_all("test")
+    assert data['a'].to_list() == [1, 4, 7, 10]
+    assert data['b'].to_list() == [2, 5, 8, 11]
+    assert data['c'].to_list() == [3, 6, 9, 12]
+
+    _clean_up(tmpdir)
+
+def test_get_last_file_path(tmpdir):
+    manager = _get_test_manager(tmpdir)
+    file_path_1 = manager._create_file_path("test", 0, 20, 2)
+    file_path_2 = manager._create_file_path("test", 21, 41, 2)
+    file_path_3 = manager._create_file_path("test", 42, 62, 2)
+    file_path_4 = manager._create_file_path("test", 63, 83, 2)
+    
+    files = [file_path_1, file_path_2, file_path_3, file_path_4]
+
+    folder_path = manager._get_folder_path("test")
+
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+
+    for file in files:
+        # create empty files
+        with open(
+            os.path.join(folder_path, file)
+            , "w") as f:
+            pass    
+
+
+    assert manager._get_last_file_path(f"{tmpdir}/test") == os.path.join(folder_path, file_path_4)
+
+    _clean_up(tmpdir)
+
+def test_write(tmpdir):
+    manager = _get_test_manager(tmpdir)
+    data = pl.DataFrame({"a": [1, 4], "b": [2, 5], "timestamp": [3, 6]})
+    manager.write("test", data)
+    file_name = manager._create_file_path("test", 3, 6, 2)
+
+    data = pl.read_csv(file_name)
+
+    assert data['a'].to_list() == [1, 4]
+    assert data['b'].to_list() == [2, 5]
+    assert data['timestamp'].to_list() == [3, 6]
+
+    _clean_up(tmpdir)
+
+def test_write_append(tmpdir):
+    manager = _get_test_manager(tmpdir)
+    data = pl.DataFrame({"a": [1, 4], "b": [2, 5], "timestamp": [3, 6]})
+    manager.write("test", data)
+
+    # new data
+    data = pl.DataFrame({"a": [11, 41], "b": [21, 51], "timestamp": [31, 61]})
+    manager.write("test", data)
+
+    file_name = manager._create_file_path("test", 3, 61, 4)
+
+    data = pl.read_csv(file_name)
+
+    assert data['a'].to_list() == [1, 4, 11, 41]
+    assert data['b'].to_list() == [2, 5, 21, 51]
+    assert data['timestamp'].to_list() == [3, 6, 31, 61]
+
+    _clean_up(tmpdir)
