591787e64849afc344257abda05b15e9a42dd198
Trent McConaghy <gtrent@gmail.com>
2024-03-02 19:06:30 +0100

    Fix #729: [Aimodel] ppss for weight_recent, balance_classes, calibrate_probs (#730)
    Fix #729

--- pdr_backend/sim/test/test_sim_engine.py
+++ pdr_backend/sim/test/test_sim_engine.py
@@ -14,18 +14,14 @@ from pdr_backend.sim.sim_engine import SimEngine
 def test_sim_engine(tmpdir):
     s = fast_test_yaml_str(tmpdir)
     ppss = PPSS(yaml_str=s, network="development")
-    input_feeds = ["binanceus BTC/USDT ETH/USDT oc 5m"]
 
     # predictoor ss
-    d = predictoor_ss_test_dict()
-    d["predict_feed"] = "binanceus BTC/USDT c 5m"
-    d["aimodel_ss"] = {
-        "input_feeds": input_feeds,
-        "approach": "LinearLogistic",
-        "max_n_train": 100,
-        "do_weight_recent": True,
-        "autoregressive_n": 3,
-    }
+    predict_feed = "binanceus BTC/USDT c 5m"
+    input_feeds = ["binanceus BTC/USDT ETH/USDT oc 5m"]
+    d = predictoor_ss_test_dict(predict_feed, input_feeds)
+    d["aimodel_ss"]["approach"] = "LinearLogistic"
+    d["aimodel_ss"]["max_n_train"] = 100
+    d["aimodel_ss"]["autoregressive_n"] = 3
     ppss.predictoor_ss = PredictoorSS(d)
 
     # lake ss
