74dd0e9c8cd7999923bad6eaf02b7e4596763a56
trentmc <gtrent@gmail.com>
2024-03-06 19:22:01 +0100

    modular lineplot_tdr_profit_vs_time

--- pdr_backend/sim/sim_plotter.py
+++ pdr_backend/sim/sim_plotter.py
@@ -72,13 +72,12 @@ class SimPlotter:
         # set x
         self.x = list(range(0, N))
         next_x = _slice(self.x, N_done, N)
-        next_hx = [next_x[0], next_x[-1]]  # horizontal x
 
         # plot row 0, col 0: predictoor profit vs time
         y00 = list(np.cumsum(st.pdr_profits_OCEAN))
         next_y00 = _slice(y00, N_done, N)
         ax00.plot(next_x, next_y00, c="g")
-        ax00.plot(next_hx, [0, 0], c="0.2", ls="--", lw=1)
+        ax00.plot(next_hx(next_x), [0, 0], c="0.2", ls="--", lw=1)
         s = f"Predictoor profit vs time. Current:{y00[-1]:.2f} OCEAN"
         _set_title(ax00, s)
         if not self.plotted_before:
@@ -101,7 +100,7 @@ class SimPlotter:
 
         ax01.plot(next_x, next_y01_est, "green")
         ax01.fill_between(next_x, next_y01_l, next_y01_u, color="0.9")
-        ax01.plot(next_hx, [50, 50], c="0.2", ls="--", lw=1)
+        ax01.plot(next_hx(next_x), [50, 50], c="0.2", ls="--", lw=1)
         ax01.set_ylim(bottom=40, top=60)
         now_s = f"{self.y01_est[-1]:.2f}% "
         now_s += f"[{self.y01_l[-1]:.2f}%, {self.y01_u[-1]:.2f}%]"
@@ -121,16 +120,7 @@ class SimPlotter:
                 ax03.margins(0.01, 0.01)
 
         # plot row 1, col 0: trader profit vs time
-        y10 = list(np.cumsum(st.trader_profits_USD))
-        next_y10 = _slice(y10, N_done, N)
-        ax10.plot(next_x, next_y10, c="b")
-        ax10.plot(next_hx, [0, 0], c="0.2", ls="--", lw=1)
-        _set_title(ax10, f"Trader profit vs time. Current: ${y10[-1]:.2f}")
-        if not self.plotted_before:
-            ax10.set_xlabel("time", fontsize=FONTSIZE)
-            ax10.set_ylabel("trader profit (USD)", fontsize=FONTSIZE)
-            _ylabel_on_right(ax10)
-            ax10.margins(0.005, 0.05)
+        self._lineplot_tdr_profit_vs_time(ax10, next_x, N_done, N)
 
         # plot row 1, col 1: 1d scatter of predictoor profits
         self._scatterplot_pdr_profit_vs_ptrue(ax11, N_done, N)
@@ -146,12 +136,19 @@ class SimPlotter:
         plt.pause(0.001)
         self.plotted_before = True
 
-    def _scatterplot_pdr_profit_vs_ptrue(
-            self,
-            ax,
-            N_done: int,
-            N: int,
-    ):
+    def _lineplot_tdr_profit_vs_time(self, ax, next_x, N_done: int, N: int):
+        y10 = list(np.cumsum(self.st.trader_profits_USD))
+        next_y10 = _slice(y10, N_done, N)
+        ax.plot(next_x, next_y10, c="b")
+        ax.plot(next_hx(next_x), [0, 0], c="0.2", ls="--", lw=1)
+        _set_title(ax, f"Trader profit vs time. Current: ${y10[-1]:.2f}")
+        if not self.plotted_before:
+            ax.set_xlabel("time", fontsize=FONTSIZE)
+            ax.set_ylabel("trader profit (USD)", fontsize=FONTSIZE)
+            _ylabel_on_right(ax)
+            ax.margins(0.005, 0.05)
+
+    def _scatterplot_pdr_profit_vs_ptrue(self, ax, N_done: int, N: int):
         stake_amt = self.ppss.predictoor_ss.stake_amount.amt_eth
         mnp, mxp = -stake_amt, stake_amt
         avg = np.average(self.st.pdr_profits_OCEAN)
@@ -171,12 +168,7 @@ class SimPlotter:
             _ylabel_on_right(ax)
             ax.margins(0.05, 0.05)
 
-    def _scatterplot_tdr_profit_vs_ptrue(
-            self,
-            ax,
-            N_done: int,
-            N: int,
-    ):
+    def _scatterplot_tdr_profit_vs_ptrue(self, ax, N_done: int, N: int):
         mnp = min(self.st.trader_profits_USD)
         mxp = max(self.st.trader_profits_USD)
         avg = np.average(self.st.trader_profits_USD)
@@ -202,6 +194,9 @@ def _shift_one_earlier(s: str):
     val = int(s[-1])
     return s[:-1] + str(val - 1)
 
+def next_hx(next_x: np.ndarray) -> List[float]:
+    """horizontal x"""
+    return [next_x[0], next_x[-1]]
 
 def _set_xlabel(ax, s: str):
     ax.set_xlabel(s, fontsize=FONTSIZE)
