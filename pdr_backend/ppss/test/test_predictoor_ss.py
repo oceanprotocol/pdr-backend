c6d129823e6e051d10b6b732537e2da108c425b7
trentmc <gtrent@gmail.com>
2024-03-06 14:33:06 +0100

    reverting most recent commit, it was unwanted

--- pdr_backend/ppss/test/test_predictoor_ss.py
+++ pdr_backend/ppss/test/test_predictoor_ss.py
@@ -62,6 +62,18 @@ def test_predictoor_ss_test_dict():
     assert d["predict_feed"] == "binance ETH/USDT c 1h"
     assert d["aimodel_ss"]["input_feeds"] == ["binance ETH/USDT c 1h"]
 
+    # test s_start_payouts attribute set
+    predictoor_ss = PredictoorSS(d)
+
+    assert (
+        predictoor_ss.s_start_payouts == 0
+    ), "Must be unset in the test dict, so should return 0"
+
+    # let's set it here
+    d["bot_only"]["s_start_payouts"] = 100
+    predictoor_ss = PredictoorSS(d)
+    assert predictoor_ss.s_start_payouts == 100, "Must be set to 100"
+
 
 @enforce_types
 def test_predictoor_ss_bad_approach():
