4803d449cb09b410e5e24df0e882798cc76b8d5d
Idiom <69865342+idiom-bytes@users.noreply.github.com>
2024-03-07 07:52:02 -0800

    Fix #756 - Removing generate_view_name and simplify db.query() (#759)
    * Removing generate_view_name and simplifying how queries can be executed against db
    
    * Fixing pylint

--- pdr_backend/lake/base_data_store.py
+++ pdr_backend/lake/base_data_store.py
@@ -1,4 +1,3 @@
-from hashlib import md5
 from abc import abstractmethod
 from typing import Optional, Literal
 
@@ -20,24 +19,9 @@ class BaseDataStore:
             database=f"{self.base_directory}/duckdb.db"
         )  # Keep a persistent connection
 
-    @enforce_types
-    def _generate_view_name(self, base_path=str) -> str:
-        """
-        Generate a unique view name for a given base path.
-        @arguments:
-            base_path - The base path to generate a view name for.
-        @returns:
-            str - A unique view name.
-        """
-
-        path = f"{self.base_directory}/{base_path}"
-        hash_object = md5(path.encode())
-        return f"dataset_{hash_object.hexdigest()}"
-
     @abstractmethod
     def query_data(
         self,
-        dataset_identifier: str,
         query: str,
         partition_type: Optional[Literal["date", "address"]] = None,
     ):
