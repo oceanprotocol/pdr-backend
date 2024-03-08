728398a99f1e9c14e40a7acc31440668fa575fdc
Trent McConaghy <gtrent@gmail.com>
2024-03-08 13:42:37 +0100

    Fix #724 #674: [Sim] Plot rel importances of vars; plot contours for 2 most impt vars (#761)
    - Plot rel importances of vars #724
    - Plot contour if >2 vars, by using 2 most important vars #674
    - And refactoring as needed to make ^ clean

--- pdr_backend/aimodel/test/test_aimodel_factory.py
+++ pdr_backend/aimodel/test/test_aimodel_factory.py
@@ -1,36 +1,42 @@
-import warnings
+from unittest.mock import patch
 
 import numpy as np
 from numpy.testing import assert_array_equal
 from enforce_typing import enforce_types
 from pytest import approx
 
-from pdr_backend.aimodel.model_plotter import plot_model
 from pdr_backend.aimodel.aimodel_data_factory import AimodelDataFactory
 from pdr_backend.aimodel.aimodel_factory import AimodelFactory
+from pdr_backend.aimodel.aimodel_plotter import (
+    plot_aimodel_response,
+    plot_aimodel_varimps,
+)
+from pdr_backend.aimodel.aimodel_plotdata import AimodelPlotdata
 from pdr_backend.ppss.aimodel_ss import AimodelSS, aimodel_ss_test_dict
 from pdr_backend.util.mathutil import classif_acc
 
-PLOT = False  # only turn on for manual testing
+SHOW_PLOT = False  # only turn on for manual testing
+
+plt_show_path = "pdr_backend.aimodel.aimodel_plotter.plt.show"
 
 
 @enforce_types
-def test_aimodel_factory_LinearLogistic():
-    _test_aimodel_factory_main(approach="LinearLogistic")
+def test_aimodel_factory_2vars_LinearLogistic():
+    _test_aimodel_factory_2vars_main(approach="LinearLogistic")
 
 
 @enforce_types
-def test_aimodel_factory_LinearSVC():
-    _test_aimodel_factory_main(approach="LinearSVC")
+def test_aimodel_factory_2vars_LinearSVC():
+    _test_aimodel_factory_2vars_main(approach="LinearSVC")
 
 
 @enforce_types
-def test_aimodel_factory_Constant():
-    _test_aimodel_factory_main(approach="Constant")
+def test_aimodel_factory_2vars_Constant():
+    _test_aimodel_factory_2vars_main(approach="Constant")
 
 
 @enforce_types
-def _test_aimodel_factory_main(approach):
+def _test_aimodel_factory_2vars_main(approach):
     # settings, factory
     ss = AimodelSS(aimodel_ss_test_dict(approach=approach))
     factory = AimodelFactory(ss)
@@ -45,9 +51,7 @@ def _test_aimodel_factory_main(approach):
     ytrue = ycont > y_thr
 
     # build model
-    with warnings.catch_warnings():
-        warnings.simplefilter("ignore")  # ignore ConvergenceWarning, more
-        model = factory.build(X, ytrue)
+    model = factory.build(X, ytrue, show_warnings=False)
 
     # test predict_true() & predict_ptrue()
     ytrue_hat = model.predict_true(X)
@@ -69,24 +73,15 @@ def _test_aimodel_factory_main(approach):
     assert imps[1] == approx(0.667, abs=0.3)
 
     # plot
-    if PLOT:
-        labels = ("x0", "x1")
-        fancy_title = True
-        leg_loc = "upper right"
-        fig_ax = None
-        xranges = (mn, mx, mn, mx)
-        plot_model(
-            model,
-            X,
-            ytrue,
-            labels,
-            fig_ax=fig_ax,
-            xranges=xranges,
-            fancy_title=fancy_title,
-            legend_loc=leg_loc,
-        )
-
-    assert not PLOT
+    colnames = ["x0", "x1"]
+    slicing_x = np.array([0.0, 1.0])  # arbitrary
+    d = AimodelPlotdata(model, X, ytrue, colnames, slicing_x)
+    if SHOW_PLOT:
+        plot_aimodel_response(d, fig_ax=None, legend_loc="upper left")
+    else:
+        with patch(plt_show_path):
+            plot_aimodel_response(d, fig_ax=None, legend_loc="upper left")
+    assert not SHOW_PLOT
 
 
 @enforce_types
@@ -136,3 +131,136 @@ def test_aimodel_accuracy_from_create_xy():
 
     yptrue_trn_hat = model.predict_ptrue(X_trn)
     assert_array_equal(yptrue_trn_hat > 0.5, ytrue_trn_hat)
+
+
+@enforce_types
+def test_aimodel_factory_1var_main():
+    """1 input var. It will plot that var on both axes"""
+    # settings, factory
+    ss = AimodelSS(aimodel_ss_test_dict(approach="LinearLogistic"))
+    factory = AimodelFactory(ss)
+
+    # data
+    N = 50
+    mn, mx = -10.0, +10.0
+    X = np.random.uniform(mn, mx, (N, 1))
+    ycont = 3.0 + 4.0 * X[:, 0]
+    y_thr = np.average(ycont)  # avg gives good class balance
+    ytrue = ycont > y_thr
+
+    # build model
+    model = factory.build(X, ytrue, show_warnings=False)
+
+    # test variable importances
+    imps = model.importance_per_var()
+    assert_array_equal(imps, np.array([1.0]))
+
+    # plot
+    colnames = ["x0"]
+    slicing_x = np.array([0.1])  # arbitrary
+    aimodel_plotdata = AimodelPlotdata(model, X, ytrue, colnames, slicing_x)
+    if SHOW_PLOT:
+        plot_aimodel_response(aimodel_plotdata)
+    else:
+        with patch(plt_show_path):
+            plot_aimodel_response(aimodel_plotdata)
+    assert not SHOW_PLOT
+
+
+@enforce_types
+def test_aimodel_factory_4vars_response():
+    """4 input vars. It will plot the 2 most important vars"""
+    # settings, factory
+    ss = AimodelSS(aimodel_ss_test_dict(approach="LinearLogistic"))
+    factory = AimodelFactory(ss)
+
+    # data
+    N = 1000
+    mn, mx = -10.0, +10.0
+    X = np.random.uniform(mn, mx, (N, 4))
+    ycont = 3.0 + 4.0 * X[:, 0] + 3.0 * X[:, 1] + 2.0 * X[:, 2] + 1.0 * X[:, 3]
+    y_thr = np.average(ycont)  # avg gives good class balance
+    ytrue = ycont > y_thr
+    colnames = ["x0", "x1", "x3", "x4"]
+
+    # build model
+    model = factory.build(X, ytrue, show_warnings=False)
+
+    # test variable importances
+    imps = model.importance_per_var()
+    assert imps[0] > imps[1] > imps[2] > imps[3] > 0.0
+    assert sum(imps) == approx(1.0, 0.01)
+    assert imps[0] == approx(4.0 / 10.0, abs=0.2)
+    assert imps[1] == approx(3.0 / 10.0, abs=0.2)
+    assert imps[2] == approx(2.0 / 10.0, abs=0.2)
+    assert imps[3] == approx(1.0 / 10.0, abs=0.2)
+
+    # plot model response
+    slicing_x = np.array([0.1, 1.0, 2.0, 3.0])  # arbitrary
+    aimodel_plotdata = AimodelPlotdata(model, X, ytrue, colnames, slicing_x)
+
+    if SHOW_PLOT:
+        plot_aimodel_response(aimodel_plotdata)
+    else:
+        with patch(plt_show_path):
+            plot_aimodel_response(aimodel_plotdata)
+    assert not SHOW_PLOT
+
+
+@enforce_types
+def test_aimodel_factory_1var_varimps():
+    _test_aimodel_factory_nvars_varimps(n=1)
+
+
+@enforce_types
+def test_aimodel_factory_2vars_varimps():
+    _test_aimodel_factory_nvars_varimps(n=2)
+
+
+@enforce_types
+def test_aimodel_factory_3vars_varimps():
+    _test_aimodel_factory_nvars_varimps(n=3)
+
+
+@enforce_types
+def test_aimodel_factory_4vars_varimps():
+    _test_aimodel_factory_nvars_varimps(n=4)
+
+
+@enforce_types
+def test_aimodel_factory_5vars_varimps():
+    _test_aimodel_factory_nvars_varimps(n=5)
+
+
+@enforce_types
+def test_aimodel_factory_10vars_varimps():
+    _test_aimodel_factory_nvars_varimps(n=5)
+
+
+@enforce_types
+def test_aimodel_factory_25vars_varimps():
+    _test_aimodel_factory_nvars_varimps(25)
+
+
+@enforce_types
+def test_aimodel_factory_100vars_varimps():
+    _test_aimodel_factory_nvars_varimps(100)
+
+
+@enforce_types
+def _test_aimodel_factory_nvars_varimps(n: int):
+    varnames = [f"x{i}" for i in range(n)]
+    imps_avg = np.array([n - i + 1 for i in range(n)])
+    assert imps_avg.shape[0] == n
+    imps_stddev = imps_avg / 4.0
+    _sum = sum(imps_avg)
+    imps_avg = imps_avg / _sum
+    imps_stddev = imps_stddev / _sum
+    imps_tup = (imps_avg, imps_stddev)
+
+    if SHOW_PLOT:
+        plot_aimodel_varimps(varnames, imps_tup)
+    else:
+        with patch(plt_show_path):
+            plot_aimodel_varimps(varnames, imps_tup)
+    assert not SHOW_PLOT
