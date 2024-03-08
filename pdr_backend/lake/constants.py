344df7e529b8af310a166827cc5e59966b3cf8b5
V <101472818+graceful-coder@users.noreply.github.com>
2024-03-08 11:57:12 -0500

    For #493 step 2/4 add a new method safe_fetch_ohlcv_dydx() (#736)
    * Added several unit tests for the new safe_fetch_ohlcv_dydx() function to make its testing comprehensive
    * Fixed a bug in the safe_fetch_ohlcv_dydx() function that was returning an incorrect data structure

--- pdr_backend/lake/constants.py
+++ pdr_backend/lake/constants.py
@@ -3,6 +3,7 @@ import polars as pl
 
 OHLCV_COLS = ["open", "high", "low", "close", "volume"]
 OHLCV_DTYPES = [np.float64] * len(OHLCV_COLS)
+BASE_URL_DYDX = "https://indexer.dydx.trade/v4/candles/perpetualMarkets"
 
 TOHLCV_COLS = ["timestamp"] + OHLCV_COLS
 TOHLCV_DTYPES = [np.int64] + OHLCV_DTYPES
