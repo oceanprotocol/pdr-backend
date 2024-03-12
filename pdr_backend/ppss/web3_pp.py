38358da456110251f7d7aa32b4c7a48a7bf2ed0d
trizin <25263018+trizin@users.noreply.github.com>
2024-03-12 12:44:17 +0000

    Remove unused funcs from mock

--- pdr_backend/ppss/web3_pp.py
+++ pdr_backend/ppss/web3_pp.py
@@ -339,20 +339,6 @@ class _MockPredictoorContractWithTracking:
     def set_token(self, web3_pp):
         pass
 
-    @property
-    def token(self):
-        token = Mock(spec=Token)
-        token.balanceOf.return_value = Eth(1000).to_wei()
-        return token
-
-    @property
-    def OCEAN(self):
-        return self.token
-
-    @property
-    def ROSE(self):
-        return self.token
-
     def get_current_epoch_ts(self) -> UnixTimeS:
         """Returns a timestamp"""
         return UnixTimeS(self._w3.eth.timestamp // self.s_per_epoch * self.s_per_epoch)
