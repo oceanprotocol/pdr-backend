4ac985f4dc7b4f931c22f0aa83e9ed05265b911e
Calina Cenan <calina@cenan.net>
2024-03-14 09:34:59 +0000

    First draft/initial commit.

---
+++ pdr.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+from pdr_backend.cli import cli_module
+
+if __name__ == "__main__":
+    cli_module._do_main()
--- pdr_backend/sim/sim_plotter.py
+++ pdr_backend/sim/sim_plotter.py
@@ -16,6 +16,10 @@ from pdr_backend.ppss.ppss import PPSS
 from pdr_backend.sim.sim_state import SimState
 from pdr_backend.util.constants import FONTSIZE
 
+import streamlit as streamlit
+streamlit.set_page_config(layout="wide")
+
+
 HEIGHT = 7.5
 WIDTH = int(HEIGHT * 3.2)
 
@@ -32,26 +36,33 @@ class SimPlotter:
         self.st = st
         self.ppss = ppss
 
+        plt.close("all")
+
         # so labels are above lines. Must be before figure()
         plt.rcParams["axes.axisbelow"] = False
 
-        # figure, subplots
-        fig = plt.figure()
-        self.fig = fig
+        c1, c2, c3 = streamlit.columns((1, 2, 1))
+        c11, c12 = c1.container(), c1.container()
+        c31, c32 = c3.container(), c3.container()
 
-        gs = gridspec.GridSpec(2, 6, width_ratios=[5, 1, 1, 0.6, 2, 3])
+        c4, c5 = streamlit.columns((1, 1))
 
-        self.ax_pdr_profit_vs_time = fig.add_subplot(gs[0, 0])
-        self.ax_trader_profit_vs_time = fig.add_subplot(gs[1, 0])
+        self.canvas = {
+            "pdr_profit_vs_time": c11.empty(),
+            "trader_profit_vs_time": c12.empty(),
+            "accuracy_vs_time": c2.empty(),
+            "pdr_profit_vs_ptrue": c31.empty(),
+            "trader_profit_vs_ptrue": c32.empty(),
+            "aimodel_varimps": c4.empty(),
+            "aimodel_response": c5.empty(),
+        }
 
-        self.ax_accuracy_vs_time = fig.add_subplot(gs[0, 1:3])
-        self.ax_pdr_profit_vs_ptrue = fig.add_subplot(gs[1, 1])
-        self.ax_trader_profit_vs_ptrue = fig.add_subplot(gs[1, 2])
+        self.figs = {}
 
-        # col 3 is empty, for overflow of aimodel_varimps's y-axis labels
-        self.ax_aimodel_varimps = fig.add_subplot(gs[:, 4])
-
-        self.ax_aimodel_response = fig.add_subplot(gs[:, 5])
+        for k in self.canvas.keys():
+            fig, ax = plt.subplots()
+            self.figs[k] = fig
+            setattr(self, f"ax_{k}", ax)
 
         # attributes to help update plots' state quickly
         self.N: int = 0
@@ -62,10 +73,6 @@ class SimPlotter:
         self.y01_l: List[float] = []
         self.y01_u: List[float] = []
         self.plotted_before: bool = False
-
-        # push plot to screen
-        plt.ion()
-        plt.show()
 
     # pylint: disable=too-many-statements
     @enforce_types
@@ -91,11 +98,12 @@ class SimPlotter:
         self._plot_aimodel_response(aimodel_plotdata)
 
         # final pieces
-        self.fig.set_size_inches(WIDTH, HEIGHT)
-        self.fig.tight_layout(pad=0.5, h_pad=1.0, w_pad=1.0)
-        plt.subplots_adjust(wspace=0.3)
+        #plt.subplots_adjust(wspace=0.3)
         plt.pause(0.001)
         self.plotted_before = True
+
+        plt.ion()
+        plt.show()
 
     @property
     def next_x(self) -> List[float]:
@@ -115,11 +123,13 @@ class SimPlotter:
         ax.plot(self.next_hx, [0, 0], c="0.2", ls="--", lw=1)
         s = f"Predictoor profit vs time. Current:{y00[-1]:.2f} OCEAN"
         _set_title(ax, s)
+
+        self.canvas["pdr_profit_vs_time"].pyplot(self.figs["pdr_profit_vs_time"])
+
         if not self.plotted_before:
             ax.set_ylabel("predictoor profit (OCEAN)", fontsize=FONTSIZE)
             ax.set_xlabel("time", fontsize=FONTSIZE)
             _ylabel_on_right(ax)
-            ax.margins(0.005, 0.05)
 
     @enforce_types
     def _plot_trader_profit_vs_time(self):
@@ -129,6 +139,8 @@ class SimPlotter:
         ax.plot(self.next_x, next_y10, c="b")
         ax.plot(self.next_hx, [0, 0], c="0.2", ls="--", lw=1)
         _set_title(ax, f"Trader profit vs time. Current: ${y10[-1]:.2f}")
+
+        self.canvas["trader_profit_vs_time"].pyplot(self.figs["trader_profit_vs_time"])
         if not self.plotted_before:
             ax.set_xlabel("time", fontsize=FONTSIZE)
             ax.set_ylabel("trader profit (USD)", fontsize=FONTSIZE)
@@ -156,6 +168,8 @@ class SimPlotter:
         now_s = f"{self.y01_est[-1]:.2f}% "
         now_s += f"[{self.y01_l[-1]:.2f}%, {self.y01_u[-1]:.2f}%]"
         _set_title(ax, f"% correct vs time. Current: {now_s}")
+
+        self.canvas["accuracy_vs_time"].pyplot(self.figs["accuracy_vs_time"])
         if not self.plotted_before:
             ax.set_xlabel("time", fontsize=FONTSIZE)
             ax.set_ylabel("% correct", fontsize=FONTSIZE)
@@ -177,6 +191,8 @@ class SimPlotter:
         s = f"pdr profit dist. avg={avg:.2f} OCEAN"
         _set_title(ax, s)
         ax.plot([0.5, 0.5], [mnp, mxp], c="0.2", ls="-", lw=1)
+
+        self.canvas["pdr_profit_vs_ptrue"].pyplot(self.figs["pdr_profit_vs_ptrue"])
         if not self.plotted_before:
             ax.plot([0.0, 1.0], [0, 0], c="0.2", ls="--", lw=1)
             _set_xlabel(ax, "prob(up)")
@@ -200,6 +216,8 @@ class SimPlotter:
 
         _set_title(ax, s)
         ax.plot([0.5, 0.5], [mnp, mxp], c="0.2", ls="-", lw=1)
+
+        self.canvas["trader_profit_vs_ptrue"].pyplot(self.figs["trader_profit_vs_ptrue"])
         if not self.plotted_before:
             ax.plot([0.0, 1.0], [0, 0], c="0.2", ls="--", lw=1)
             _set_xlabel(ax, "prob(up)")
@@ -211,14 +229,18 @@ class SimPlotter:
     def _plot_aimodel_varimps(self, d: AimodelPlotdata):
         ax = self.ax_aimodel_varimps
         imps_tups = d.model.importance_per_var(include_stddev=True)
-        plot_aimodel_varimps(d.colnames, imps_tups, (self.fig, ax))
+        plot_aimodel_varimps(d.colnames, imps_tups, (self.figs["aimodel_varimps"], ax))
+
+        self.canvas["aimodel_varimps"].pyplot(self.figs["aimodel_varimps"])
         if not self.plotted_before:
             ax.margins(0.01, 0.01)
 
     @enforce_types
     def _plot_aimodel_response(self, d: AimodelPlotdata):
         ax = self.ax_aimodel_response
-        plot_aimodel_response(d, (self.fig, ax))
+        plot_aimodel_response(d, (self.figs["aimodel_varimps"], ax))
+
+        self.canvas["aimodel_response"].pyplot(self.figs["aimodel_response"])
         if not self.plotted_before:
             ax.margins(0.01, 0.01)
 
