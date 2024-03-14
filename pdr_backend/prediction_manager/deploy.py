7ab4e96cf3d0fb9087f92c53e2739e3fbc3aa83b
trizin <25263018+trizin@users.noreply.github.com>
2024-03-14 11:10:07 +0000

    Fix returned address and formatting

--- pdr_backend/prediction_manager/deploy.py
+++ pdr_backend/prediction_manager/deploy.py
@@ -10,22 +10,23 @@ from pdr_backend.ppss.web3_pp import Web3PP
 def deploy_prediction_manager_contract(web3_pp: Web3PP) -> str:
     web3_config = web3_pp.web3_config
     ocean_addr = web3_pp.get_address("Ocean")
-    
     abi_path = os.path.join(
         os.path.dirname(__file__), "compiled_contracts", "PredictoorMaster_abi.json"
     )
-    
+
     bytecode_path = os.path.join(
         os.path.dirname(__file__), "compiled_contracts", "PredictoorMaster_bytecode.bin"
     )
-    
+
     with open(abi_path, "r") as abi_file:
         abi = json.load(abi_file)
-    
+
     with open(bytecode_path, "r") as bytecode_file:
         bytecode = bytecode_file.read()
-    
+
     PredictoorMaster = web3_config.w3.eth.contract(abi=abi, bytecode=bytecode)
-    tx_hash = PredictoorMaster.constructor(ocean_addr).transact(web3_pp.tx_call_params())
+    tx_hash = PredictoorMaster.constructor(ocean_addr).transact(
+        web3_pp.tx_call_params()
+    )
     tx_receipt = web3_config.w3.eth.wait_for_transaction_receipt(tx_hash)
-    return PredictoorMaster.address
+    return tx_receipt.contractAddress
