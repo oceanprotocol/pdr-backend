22571b6709a03fccff4d28675e4ebb1e916abd25
trizin <25263018+trizin@users.noreply.github.com>
2024-03-14 14:17:42 +0000

    PredictionManager test_transfer_erc20

--- pdr_backend/prediction_manager/test/test_prediction_manager.py
+++ pdr_backend/prediction_manager/test/test_prediction_manager.py
@@ -14,6 +14,16 @@ def test_approve(prediction_manager: PredictionManager, predictoor_contract, pre
     assert ocean_token.allowance(prediction_manager.contract_address, predictoor_contract.contract_address) == 2**256 - 1
     assert ocean_token.allowance(prediction_manager.contract_address, predictoor_contract2.contract_address) == 2**256 - 1
 
+def test_transfer_erc20(prediction_manager: PredictionManager, ocean_token, web3_config):
+    ocean_token.transfer(prediction_manager.contract_address, 100)
+    assert ocean_token.balance_of(prediction_manager.contract_address) == 100
+    before = ocean_token.balance_of(web3_config.owner)
+    prediction_manager.transfer_erc20(ocean_token.contract_address, web3_config.owner, 100)
+    after = ocean_token.balance_of(web3_config.owner)
+    assert after - before == 100
+    assert ocean_token.balance_of(prediction_manager.contract_address) == 0
+    
+
 @pytest.fixture(scope="module")
 def prediction_manager(web3_pp):
     return deploy_prediction_manager_contract(web3_pp)
