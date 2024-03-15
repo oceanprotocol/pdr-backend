15513562cde381a557548a2e43c1b59e2db02e86
trizin <25263018+trizin@users.noreply.github.com>
2024-03-15 13:45:58 +0000

    Tests for the prediction manager

--- pdr_backend/prediction_manager/test/test_prediction_manager.py
+++ pdr_backend/prediction_manager/test/test_prediction_manager.py
@@ -1,3 +1,5 @@
+from web3.types import RPCEndpoint
+
 from pdr_backend.conftest_ganache import *
 from pdr_backend.contract.prediction_manager import (
     PredictionManager,
@@ -53,6 +55,77 @@ def test_transfer(prediction_manager: PredictionManager, web3_config):
     after = web3_config.w3.eth.get_balance(web3_config.owner)
     assert after - before == 100
     assert web3_config.w3.eth.get_balance(prediction_manager.contract_address) == 0
+
+
+def submit_prediction_and_payout(
+    prediction_manager: PredictionManager,
+    web3_config,
+    predictoor_contract: PredictoorContract,
+    predictoor_contract2,
+    ocean_token,
+):
+    # the user transfers 100 OCEAN tokens to the prediction manager
+    ocean_token.transfer(prediction_manager.contract_address, 100)
+
+    # get the next prediction epoch
+    current_epoch = predictoor_contract.get_current_epoch_ts()
+
+    # set prediction epoch
+    prediciton_epoch = current_epoch + S_PER_EPOCH * 2
+
+    # get the OCEAN balance of the contract before submitting
+    bal_before = ocean_token.balance_of(prediction_manager.contract_address)
+    assert (
+        bal_before == 100
+    ), "OCEAN balance of the contract should be 100 before submitting"
+
+    # submit prediction
+    feeds = [
+        predictoor_contract.contract_address,
+        predictoor_contract2.contract_address,
+    ]
+    prediction_manager.submit_prediction(
+        stakes_up=[20, 30],
+        stakes_down=[30, 20],
+        feeds=feeds,
+        epoch=prediciton_epoch,
+        wait_for_receipt=True,
+    )
+
+    # get the OCEAN balance of the contract after submitting
+    bal_after = ocean_token.balance_of(prediction_manager.contract_address)
+    assert bal_after == 0, "OCEAN balance of the contract should be 0 after submitting"
+
+    # fast forward time to get payout
+    web3_config.w3.provider.make_request(
+        RPCEndpoint("evm_increaseTime"), [S_PER_EPOCH * 4]
+    )
+    web3_config.w3.provider.make_request(RPCEndpoint("evm_mine"), [])
+
+    # submit the trueval
+    predictoor_contract.submit_trueval(
+        True, prediciton_epoch, False, True
+    )  # submit True for the first contract
+    predictoor_contract2.submit_trueval(
+        False, prediciton_epoch, False, True
+    )  # submit False for the second contract
+
+    # time to claim payouts
+    # expected payouts are:
+    # 20 OCEAN for the first contract
+    # 20 OCEAN for the second contract
+    # 40 Total
+
+    # get the OCEAN balance of the contract before claiming
+    bal_before = ocean_token.balance_of(prediction_manager.contract_address)
+
+    # claim
+    prediction_manager.get_payout([prediciton_epoch], feeds, wait_for_receipt=True)
+
+    # get the OCEAN balance of the contract after claiming
+    bal_after = ocean_token.balance_of(prediction_manager.contract_address)
+
+    assert bal_after == 40, "OCEAN balance of the contract should be 40 after claiming"
 
 
 @pytest.fixture(scope="module")
