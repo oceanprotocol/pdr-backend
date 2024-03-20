31db3de0ed97fda607cacdbaf0e20c645bd3b25f
trentmc <gtrent@gmail.com>
2024-03-20 12:19:41 +0100

    add missing file

---
+++ pdr_backend/exchange/test/test_mock_exchange.py
@@ -0,0 +1,15 @@
+import ccxt
+from enforce_typing import enforce_types
+
+from pdr_backend.exchange.mock_exchange import MockExchange
+
+
+@enforce_types
+def test_mock_exchange():
+    mock_exchange = MockExchange()
+    assert str(mock_exchange) == "mocked exchange"
+    assert isinstance(mock_exchange, ccxt.Exchange)
+    assert isinstance(mock_exchange.create_market_buy_order("BTC/USDT", 0.1), MockOrder)
+    assert isinstance(
+        mock_exchange.create_market_sell_order("BTC/USDT", 0.1), MockOrder
+    )
