53d53165ed03f9ae35f2be868c1e8857fc459fd7
Mustafa Tuncay <mustafaislev@gmail.com>
2024-03-20 15:37:06 +0300

    check trigger test

--- pdr_backend/lake/test/test_etl.py
+++ pdr_backend/lake/test/test_etl.py
@@ -68,6 +68,7 @@ def test_setup_etl(
         "pdr_payouts": Table(payouts_table_name, payouts_schema, ppss),
     }
 
+
     gql_tables["pdr_predictions"].append_to_storage(preds)
     gql_tables["pdr_truevals"].append_to_storage(truevals)
     gql_tables["pdr_payouts"].append_to_storage(payouts)
