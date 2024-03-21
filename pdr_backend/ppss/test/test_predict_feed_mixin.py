f9c7cb15f341abe209c6150591d0d1435c1ef2db
trizin <25263018+trizin@users.noreply.github.com>
2024-03-21 14:33:26 +0000

    PredictFeedMixin test

---
+++ pdr_backend/ppss/test/test_predict_feed_mixin.py
@@ -0,0 +1,44 @@
+from pdr_backend.cli.arg_feed import ArgFeed
+from pdr_backend.cli.arg_feeds import ArgFeeds
+from pdr_backend.ppss.predict_feed_mixin import PredictFeedMixin
+
+
+def test_predict_feed_mixin():
+    feed_dict = {
+        "feeds": [
+            {
+                "predict": "binance BTC/USDT c 5m",
+                "train_on": [
+                    "binance BTC/USDT ETH/USDT DOT/USDT c 5m",
+                    "kraken BTC/USDT c 5m",
+                ],
+            },
+            {
+                "predict": "binance ETH/USDT c 5m",
+                "train_on": "binance BTC/USDT ETH/USDT DOT/USDT c 5m, kraken BTC/USDT c 5m",
+            },
+            {
+                "predict": "binance BTC/USDT c 1h",
+                "train_on": "binance BTC/USDT ETH/USDT c 5m",
+            },
+        ]
+    }
+    expected = [
+        {
+            "predict": ArgFeed.from_str("binance BTC/USDT c 5m"),
+            "train_on": ArgFeeds.from_str("binance BTC/USDT ETH/USDT DOT/USDT c 5m")
+            + ArgFeeds.from_str("kraken BTC/USDT c 5m"),
+        },
+        {
+            "predict": ArgFeed.from_str("binance ETH/USDT c 5m"),
+            "train_on": ArgFeeds.from_str("binance BTC/USDT ETH/USDT DOT/USDT c 5m")
+            + ArgFeeds.from_str("kraken BTC/USDT c 5m"),
+        },
+        {
+            "predict": ArgFeed.from_str("binance BTC/USDT c 1h"),
+            "train_on": ArgFeeds.from_str("binance BTC/USDT ETH/USDT c 5m"),
+        },
+    ]
+    parser = PredictFeedMixin(feed_dict)
+
+    assert parser.feeds == expected
