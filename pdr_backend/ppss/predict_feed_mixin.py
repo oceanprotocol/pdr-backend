a435448aa1004eea484cddbce2cf0e10cb3d105e
trizin <25263018+trizin@users.noreply.github.com>
2024-03-25 16:51:53 +0000

    Fixes

--- pdr_backend/ppss/predict_feed_mixin.py
+++ pdr_backend/ppss/predict_feed_mixin.py
@@ -1,45 +1,30 @@
-from typing import Dict, List, Union
+from typing import Dict, List, Optional, Union
 from enforce_typing import enforce_types
 from pdr_backend.cli.arg_feeds import ArgFeeds
+from pdr_backend.cli.predict_feeds import PredictFeeds
 
 
 @enforce_types
 class PredictFeedMixin:
     FEEDS_KEY = "feeds"
 
-    def __init__(self, d: dict):
+    def __init__(self, d: dict, assert_feed_attributes: Optional[List] = None):
         self.d = d
 
-    def parse_feed_obj(self, feed_obj: Union[str, list]) -> ArgFeeds:
-        # If feed_obj is a string, convert to list
-        if isinstance(feed_obj, str):
-            # If comma separated string, split
-            # If not comma separated, convert to list
-            if "," in feed_obj:
-                feed_obj = feed_obj.split(",")
-            else:
-                feed_obj = [feed_obj]
+        if assert_feed_attributes:
+            pass  # TODO
+            # missing_attributes = []
+            # for attr in assert_feed_attributes:
+            #     for feed in feeds:
+            #         if not getattr(feed, attr):
+            #             missing_attributes.append(attr)
 
-        if not isinstance(feed_obj, list):
-            raise ValueError(f"feed_obj must be a list, got {feed_obj}")
-
-        parsed_objs = []
-        for feed in feed_obj:
-            # Convert each feed_obj string to ArgFeeds
-            feed_obj = ArgFeeds.from_str(feed)
-            parsed_objs.extend(feed_obj)
-        return parsed_objs
+            # if missing_attributes:
+            #     raise AssertionError(
+            #         f"Missing attributes {missing_attributes} for some feeds."
+            #     )
 
     @property
     def feeds(self) -> List[Dict[str, ArgFeeds]]:
-        feeds_list = []
-        for feed in self.d.get(self.__class__.FEEDS_KEY, []):
-            predict = feed.get("predict", "")
-            train_on = feed.get("train_on", [])
-
-            predict_feeds = self.parse_feed_obj(predict)
-            train_on_feeds = self.parse_feed_obj(train_on)
-
-            feeds_list.append({"predict": predict_feeds, "train_on": train_on_feeds})
-
-        return feeds_list
+        feeds_list = PredictFeeds.from_array(self.d.get(self.__class__.FEEDS_KEY, []))
+        return feeds_list.to_list()
