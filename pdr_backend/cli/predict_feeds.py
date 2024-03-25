52b0134078256bb31030b80e0764e3fdeb9f13b6
trizin <25263018+trizin@users.noreply.github.com>
2024-03-25 15:24:55 +0000

    Add PredictFeed and PredictFeeds classes

---
+++ pdr_backend/cli/predict_feeds.py
@@ -0,0 +1,56 @@
+from typing import List, Union
+from pdr_backend.cli.arg_feeds import ArgFeeds
+
+
+class PredictFeed:
+    def __init__(self, predict: ArgFeeds, train_on: ArgFeeds):
+        self.predict: ArgFeeds = predict
+        self.train_on: ArgFeeds = train_on
+
+    @classmethod
+    def from_feed_objs(cls, predict: Union[str, list], train_on: Union[str, list]):
+        parsed_predict = cls.parse_feed_obj(predict)
+        parsed_train_on = cls.parse_feed_obj(train_on)
+        return cls(parsed_predict, parsed_train_on)
+
+    @staticmethod
+    def parse_feed_obj(feed_obj: Union[str, list]) -> ArgFeeds:
+        # If feed_obj is a string, convert to list
+        if isinstance(feed_obj, str):
+            # If comma separated string, split
+            # If not comma separated, convert to list
+            if "," in feed_obj:
+                feed_obj = feed_obj.split(",")
+            else:
+                feed_obj = [feed_obj]
+
+        if not isinstance(feed_obj, list):
+            raise ValueError(f"feed_obj must be a list, got {feed_obj}")
+
+        parsed_objs = []
+        for feed in feed_obj:
+            # Convert each feed_obj string to ArgFeeds
+            feed_obj = ArgFeeds.from_str(feed)
+            parsed_objs.extend(feed_obj)
+        return parsed_objs
+
+    def to_dict(self):
+        return {
+            "predict": self.predict,
+            "train_on": self.train_on
+        }
+    
+    def from_dict(d):
+        return PredictFeed(d["predict"], d["train_on"])
+        
+
+class PredictFeeds(List[PredictFeed]):
+    def __init__(self, feeds: List[PredictFeed]):
+        super().__init__(feeds)
+
+    @classmethod
+    def from_array(cls, feeds: List[dict]):
+        return cls([PredictFeed.from_feed_objs(feeds["predict"], feeds["train_on"]) for feeds in feeds])
+
+    def to_list(self):
+        return [feed.to_dict() for feed in self]
