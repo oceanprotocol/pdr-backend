9ab125cbf463166468f46eae1cf4fe4c2e4003eb
trizin <25263018+trizin@users.noreply.github.com>
2024-04-01 13:58:45 +0000

    enable assert attributes

--- pdr_backend/ppss/predict_feed_mixin.py
+++ pdr_backend/ppss/predict_feed_mixin.py
@@ -1,4 +1,4 @@
-from typing import Dict, List, Optional, Union
+from typing import Dict, List, Optional
 from enforce_typing import enforce_types
 from pdr_backend.cli.arg_feeds import ArgFeeds
 from pdr_backend.cli.predict_feeds import PredictFeeds
@@ -9,20 +9,21 @@ class PredictFeedMixin:
     FEEDS_KEY = "feeds"
 
     def __init__(self, d: dict, assert_feed_attributes: Optional[List] = None):
+        assert self.__class__.FEEDS_KEY
         self.d = d
+        feeds = PredictFeeds.from_array(self.d.get(self.__class__.FEEDS_KEY, [])).feeds
 
         if assert_feed_attributes:
-            pass  # TODO
-            # missing_attributes = []
-            # for attr in assert_feed_attributes:
-            #     for feed in feeds:
-            #         if not getattr(feed, attr):
-            #             missing_attributes.append(attr)
+            missing_attributes = []
+            for attr in assert_feed_attributes:
+                for feed in feeds:
+                    if not getattr(feed, attr):
+                        missing_attributes.append(attr)
 
-            # if missing_attributes:
-            #     raise AssertionError(
-            #         f"Missing attributes {missing_attributes} for some feeds."
-            #     )
+            if missing_attributes:
+                raise AssertionError(
+                    f"Missing attributes {missing_attributes} for some feeds."
+                )
 
     @property
     def feeds(self) -> List[Dict[str, ArgFeeds]]:
