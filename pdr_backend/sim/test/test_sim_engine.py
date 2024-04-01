22639504c85990c0dfd023ca8b770b15649b0991
trizin <25263018+trizin@users.noreply.github.com>
2024-04-01 14:32:58 +0000

    Update sim engine test

--- pdr_backend/sim/test/test_sim_engine.py
+++ pdr_backend/sim/test/test_sim_engine.py
@@ -2,6 +2,7 @@ import os
 from unittest import mock
 
 from enforce_typing import enforce_types
+from pdr_backend.cli.predict_feeds import PredictFeeds
 
 from pdr_backend.ppss.lake_ss import LakeSS, lake_ss_test_dict
 from pdr_backend.ppss.ppss import PPSS, fast_test_yaml_str
@@ -15,18 +16,19 @@ def test_sim_engine(tmpdir):
     s = fast_test_yaml_str(tmpdir)
     ppss = PPSS(yaml_str=s, network="development")
 
-    predict_feed = "binanceus BTC/USDT c 5m"
-    input_feeds = ["binanceus BTC/USDT ETH/USDT c 5m"]
+    predict_feeds = PredictFeeds.from_array(
+        [{"predict": "binanceus BTC/USDT c 5m", "train_on": "binanceus BTC/USDT c 5m"}]
+    )
 
     # lake ss
     parquet_dir = os.path.join(tmpdir, "parquet_data")
-    d = lake_ss_test_dict(parquet_dir, input_feeds)
+    d = lake_ss_test_dict(parquet_dir, predict_feeds.feeds_str)
     d["st_timestr"] = "2023-06-18"
     d["fin_timestr"] = "2023-06-19"
     ppss.lake_ss = LakeSS(d)
 
     # predictoor ss
-    d = predictoor_ss_test_dict(predict_feed, input_feeds)
+    d = predictoor_ss_test_dict(predict_feeds, predict_feeds.feeds_str)
     d["aimodel_ss"]["approach"] = "LinearLogistic"
     d["aimodel_ss"]["max_n_train"] = 20
     d["aimodel_ss"]["autoregressive_n"] = 1
