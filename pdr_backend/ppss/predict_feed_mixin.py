a6e009cf271f82c2c7c68298c8f64eb0bd1334f4
trizin <25263018+trizin@users.noreply.github.com>
2024-04-02 13:27:51 +0000

    Add feeds property back

--- pdr_backend/ppss/predict_feed_mixin.py
+++ pdr_backend/ppss/predict_feed_mixin.py
@@ -11,7 +11,6 @@ class PredictFeedMixin:
     def __init__(self, d: dict, assert_feed_attributes: Optional[List] = None):
         assert self.__class__.FEEDS_KEY
         self.d = d
-        self.feeds = PredictFeeds.from_array(self.d.get(self.__class__.FEEDS_KEY, []))
         if assert_feed_attributes:
             missing_attributes = []
             for attr in assert_feed_attributes:
@@ -23,3 +22,7 @@ class PredictFeedMixin:
                 raise AssertionError(
                     f"Missing attributes {missing_attributes} for some feeds."
                 )
+
+    @property
+    def feeds(self) -> PredictFeeds:
+        return PredictFeeds.from_array(self.d.get(self.__class__.FEEDS_KEY, []))
