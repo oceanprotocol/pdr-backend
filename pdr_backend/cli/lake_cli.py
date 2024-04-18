45e8e3c6dcc2012120da94df13a7d6a4c0a5f718
Calina Cenan <calina@cenan.net>
2024-04-18 12:04:34 +0000

    Cleanup arguments pt2, support ppss for update commands.

--- pdr_backend/cli/lake_cli.py
+++ pdr_backend/cli/lake_cli.py
@@ -5,11 +5,13 @@ from argparse import ArgumentParser
 from enforce_typing import enforce_types
 
 from pdr_backend.analytics.lakeinfo import LakeInfo
+from pdr_backend.cli.cli_arguments import PPSS_Mixin, NETWORK_Mixin
 from pdr_backend.lake.persistent_data_store import PersistentDataStore
 from pdr_backend.util.time_types import UnixTimeMs
 
 logger = logging.getLogger("cli")
-LAKE_SUBCOMMANDS = ["describe", "query", "raw", "etl", "update"]
+LAKE_SUBCOMMANDS = ["describe", "query", "raw", "etl"]
+SUPPORTS_L2_COMMANDS = ["raw", "etl"]
 
 
 # utilities
@@ -32,26 +34,22 @@ def str_as_abspath(s: str):
 
 
 @enforce_types
-class LakeArgParser(ArgumentParser):
+class LakeArgParser(ArgumentParser, PPSS_Mixin, NETWORK_Mixin):
     def __init__(self, plain_args):
         super().__init__()
         self.add_argument("subcommand", type=str, help="")
 
-        if plain_args[0] in ["raw", "etl"]:
+        if plain_args[0] in SUPPORTS_L2_COMMANDS:
             self.add_argument(
-                "raw_subcommand_type",
+                "l2_subcommand_type",
                 type=str,
                 choices=["drop", "update"],
                 help="drop or update",
             )
 
-        if plain_args[0] in ["etl", "raw"] and plain_args[1] == "update":
-            self.add_argument("PPSS_FILE", type=str, help="PPSS yaml settings file")
-            self.add_argument(
-                "NETWORK",
-                type=str,
-                help="sapphire-testnet|sapphire-mainnet|development|barge-pytest|..",
-            )
+        if plain_args[0] in SUPPORTS_L2_COMMANDS and plain_args[1] == "update":
+            self.add_argument_PPSS()
+            self.add_argument_NETWORK()
         else:
             self.add_argument(
                 "LAKE_DIR", type=str_as_abspath, help="The directory of the lake"
@@ -59,7 +57,7 @@ class LakeArgParser(ArgumentParser):
 
         if plain_args[0] == "query":
             self.add_argument("QUERY", type=str, help="The query to run")
-        elif plain_args[0] in ["raw", "etl"]:
+        elif plain_args[0] in SUPPORTS_L2_COMMANDS:
             self.add_argument("ST", type=timestr, help="Start date yyyy-mm-dd")
             if plain_args[1] == "update":
                 self.add_argument("END", type=timestr, help="End date yyyy-mm-dd")
@@ -153,9 +151,9 @@ def do_lake_raw(args):
     @description
         Drop or update raw data
     """
-    if args.raw_subcommand_type == "drop":
+    if args.l2_subcommand_type == "drop":
         do_raw_drop(args)
-    elif args.raw_subcommand_type == "update":
+    elif args.l2_subcommand_type == "update":
         do_raw_update(args)
 
 
@@ -172,9 +170,9 @@ def do_raw_update(args):
 
 @enforce_types
 def do_lake_etl(args):
-    if args.raw_subcommand_type == "drop":
+    if args.l2_subcommand_type == "drop":
         do_etl_drop(args)
-    elif args.raw_subcommand_type == "update":
+    elif args.l2_subcommand_type == "update":
         do_etl_update(args)
 
 
