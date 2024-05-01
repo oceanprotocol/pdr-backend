d29cc7b894ad03963410f352e56c3d621a3d262b
trizin <25263018+trizin@users.noreply.github.com>
2024-05-01 14:07:58 +0300

    pred_submitter_down_address and up address

--- pdr_backend/predictoor/predictoor_agent.py
+++ pdr_backend/predictoor/predictoor_agent.py
@@ -478,7 +478,7 @@ class PredictoorAgent:
         # Update previous payouts history to avoid claiming for this epoch again
         self.prev_submit_payouts.append(self.cur_unique_epoch)
 
-        up_pred_addr = self.pred_submitter_mgr.predictoor_up_address()
+        up_pred_addr = self.pred_submitter_mgr.pred_submitter_up_address()
         pending_slots = query_pending_payouts(
             self.ppss.web3_pp.subgraph_url, up_pred_addr
         )
