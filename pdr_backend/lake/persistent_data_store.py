e7e5264e95527ccd326820d5b9da3b950aebbbc7
idiom-bytes <idiom.bytes@gmail.com>
2024-05-06 15:32:07 -0700

    cleaning up logging

--- pdr_backend/lake/persistent_data_store.py
+++ pdr_backend/lake/persistent_data_store.py
@@ -129,7 +129,7 @@ class PersistentDataStore(BaseDataStore):
             return
 
         logger.info("create_and_fill_table = %s", table_name)
-        logger.info("\n%s", df)
+        logger.info("%s", df)
         self._create_and_fill_table(df, table_name)
 
     @enforce_types
