ee1cc970298b58d5dbb153ef384d374a945a691d
trentmc <gtrent@gmail.com>
2024-05-14 09:31:33 +0200

    linters happy, new test is passing

--- pdr_backend/aimodel/autocorrelation.py
+++ pdr_backend/aimodel/autocorrelation.py
@@ -1,37 +1,35 @@
-from datetime import datetime
-from typing import List
-
 from enforce_typing import enforce_types
 import numpy as np
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 from statsmodels.tsa.stattools import acf, adfuller, pacf
 
-from pdr_backend.cli.arg_timeframe import ArgTimeframe
-from pdr_backend.util.mathutil import has_nan
 from pdr_backend.util.strutil import compactSmallNum
-from pdr_backend.util.time_types import UnixTimeMs
 
 
 class AutocorrelationPlotdataFactory:
 
     @classmethod
-    def build(cls, y_values, nlags:int) -> "AutocorrelationPlotdata":
+    def build(cls, y_values, nlags: int) -> "AutocorrelationPlotdata":
         assert len(y_values.shape) == 1, "y_values must be 1d array"
 
         N_samples = len(y_values)
         adf_pvalue = adfuller(y_values)[1]
-    
-        target_CI = 0.95 # target 95% confidence interval
+
+        target_CI = 0.95  # target 95% confidence interval
         alpha = 1.0 - target_CI
         acf_results = CorrResults(acf(y_values, nlags=nlags, alpha=alpha))
         pacf_results = CorrResults(pacf(y_values, nlags=nlags, alpha=alpha))
-                
+
         plotdata = AutocorrelationPlotdata(
-            N_samples, adf_pvalue, acf_results, pacf_results,
+            N_samples,
+            adf_pvalue,
+            acf_results,
+            pacf_results,
         )
-    
+
         return plotdata
+
 
 class CorrResults:
     @enforce_types
@@ -43,9 +41,9 @@ class CorrResults:
         ci_diff = ci_upper - ci_lower
 
         self.values = values
-        self.lower_exclusion = - 1 * ci_diff
-        self.upper_exclusion = + 1 * ci_diff
-        
+        self.lower_exclusion = -1 * ci_diff
+        self.upper_exclusion = +1 * ci_diff
+
     @property
     def max_lag(self) -> int:
         return len(self.values)
@@ -60,11 +58,11 @@ class AutocorrelationPlotdata:
     """Simple class to manage many inputs going to plot_autocorrelation."""
 
     def __init__(
-            self,
-            N_samples: int,
-            adf_pvalue: float,
-            acf_results: CorrResults,
-            pacf_results: CorrResults,
+        self,
+        N_samples: int,
+        adf_pvalue: float,
+        acf_results: CorrResults,
+        pacf_results: CorrResults,
     ):
         self.N_samples = N_samples
         self.adf_pvalue = adf_pvalue
@@ -85,7 +83,7 @@ def plot_autocorrelation(autocorrelation_plotdata: AutocorrelationPlotdata):
     d = autocorrelation_plotdata
 
     fig = make_subplots(rows=2, cols=1, vertical_spacing=0.04)
-    
+
     # subplot 1: acf
     _add_corr_traces(d.acf_results, fig, row=1)
     fig.update_yaxes(title_text="autocorrelation (acf)", row=1, col=1)
@@ -113,7 +111,7 @@ def plot_autocorrelation(autocorrelation_plotdata: AutocorrelationPlotdata):
     s += f" for {d.N_samples} points"
     s += f". ADF p-value={compactSmallNum(d.adf_pvalue)}"
     fig.update_layout(title_text=s, showlegend=False)
-    
+
     fig.update_yaxes(nticks=8)
     fig.update_xaxes(nticks=16)
     delta = 0.05 * d.max_lag
@@ -121,19 +119,19 @@ def plot_autocorrelation(autocorrelation_plotdata: AutocorrelationPlotdata):
 
     return fig
 
-    
+
 @enforce_types
-def _add_corr_traces(corr_results: CorrResults, fig, row:int):
+def _add_corr_traces(corr_results: CorrResults, fig, row: int):
     """Worker function for plotting acf or pacf"""
 
-    # exclusion region 
-    # different than CIs! See https://stats.stackexchange.com/questions/518786/what-am-i-misunderstanding-about-the-acf-and-acf-plot
+    # exclusion region
+    # different than CIs! See https://stats.stackexchange.com/questions/518786/what-am-i-misunderstanding-about-the-acf-and-acf-plot # pylint: disable=line-too-long
     fig.add_trace(
         go.Scatter(
             x=corr_results.x_lags,
             y=corr_results.lower_exclusion,
             mode="lines",
-            line={"color":"cornflowerblue", "width":0},
+            line={"color": "cornflowerblue", "width": 0},
         ),
         row=row,
         col=1,
@@ -142,9 +140,9 @@ def _add_corr_traces(corr_results: CorrResults, fig, row:int):
         go.Scatter(
             x=corr_results.x_lags,
             y=corr_results.upper_exclusion,
-            fill="tonexty", # fill area between this trace and previous one
+            fill="tonexty",  # fill area between this trace and previous one
             mode="lines",
-            line={"color":"cornflowerblue", "width":0},
+            line={"color": "cornflowerblue", "width": 0},
         ),
         row=row,
         col=1,
@@ -157,12 +155,12 @@ def _add_corr_traces(corr_results: CorrResults, fig, row:int):
             x=corr_results.x_lags,
             y=corr_results.values,
             mode="markers",
-            marker={"color":"blue", "size":size},
+            marker={"color": "blue", "size": size},
         ),
         row=row,
         col=1,
     )
-    
+
     # vertical bars
     width = _rail(50 // corr_results.max_lag, 1, 10)
     for x, y in zip(corr_results.x_lags, corr_results.values):
@@ -177,6 +175,7 @@ def _add_corr_traces(corr_results: CorrResults, fig, row:int):
             col=1,
         )
 
+
 @enforce_types
-def _rail(val:int, mn_val:int, mx_val:int) -> int:
+def _rail(val: int, mn_val: int, mx_val: int) -> int:
     return max(mn_val, min(mx_val, val))
--- pdr_backend/aimodel/test/test_autocorrelation.py
+++ pdr_backend/aimodel/test/test_autocorrelation.py
@@ -1,23 +1,20 @@
 from enforce_typing import enforce_types
 import pandas as pd
 import numpy as np
-from scipy import stats     
+from scipy import stats
 
 from pdr_backend.aimodel.autocorrelation import (
     AutocorrelationPlotdata,
     AutocorrelationPlotdataFactory,
     plot_autocorrelation,
 )
-from pdr_backend.cli.arg_timeframe import ArgTimeframe
-from pdr_backend.util.mathutil import has_nan
-from pdr_backend.util.time_types import UnixTimeMs
 
 DATA_FILE = (
     "./pdr_backend/lake/test/merged_ohlcv_df_BTC-ETH_2024-02-01_to_2024-03-08.csv"
 )
 BTC_COL = "binance:BTC/USDT:close"
 
-SHOW_PLOT = True  # only turn on for manual testing
+SHOW_PLOT = False  # only turn on for manual testing
 
 
 @enforce_types
@@ -29,20 +26,20 @@ def test_autocorrelation_SHOW_PLOT():
 @enforce_types
 def test_autocorrelation():
     # play with me
-    nlags = 500
+    nlags = 5
     do_boxcox = True
-    differencing_order = 0
+    differencing_order = 1
 
-    # 
-    y = np.array(_get_data()) #[:1000]
+    #
+    y = np.array(_get_data())  # [:1000]
     if do_boxcox:
         y, _ = stats.boxcox(y)
-    for i in range(differencing_order):
+    for _ in range(differencing_order):
         y = y[1:] - y[:-1]
-        
+
     d = AutocorrelationPlotdataFactory.build(y, nlags)
     assert isinstance(d, AutocorrelationPlotdata)
-    
+
     fig = plot_autocorrelation(d)
 
     if SHOW_PLOT:
