4b6fc6c95284c361e7ed6e7a1ed7b69e837c2c67
trizin <25263018+trizin@users.noreply.github.com>
2024-05-17 14:29:43 +0300

    Update tests for pdr manager payouts

--- system_tests/test_ocean_payout.py
+++ system_tests/test_ocean_payout.py
@@ -22,27 +22,33 @@ def test_ocean_payout_test(mock_wait_until_subgraph_syncs, caplog):
     mock_web3_config.owner = "0x00000000000000000000000000000000000c0ffe"
     mock_web3_config.w3 = Mock()
     mock_web3_config.w3.eth.chain_id = SAPPHIRE_MAINNET_CHAINID
+    checksum_mock = lambda _, y: y
+    mock_web3_config.w3.to_checksum_address = lambda x: x
     mock_web3_pp.web3_config = mock_web3_config
+
+    mock_contract = Mock()
+    mock_contract.get_payout = Mock()
+    mock_contract.get_payout.return_value = {"transactionHash": b"0x1", "status": 1}
+    mock_contract.pred_submitter_up_address.return_value = "0x1"
+    mock_contract.pred_submitter_down_address.return_value = "0x1"
 
     mock_token = MagicMock()
     mock_token.balanceOf.return_value = 100 * 1e18
 
     mock_query_pending_payouts = Mock()
     mock_query_pending_payouts.return_value = {"0x1": [1, 2, 3], "0x2": [5, 6, 7]}
-    number_of_slots = 6  # 3 + 3
-
-    mock_feed_contract = Mock(spec=FeedContract)
-    mock_feed_contract.payout_multiple.return_value = None
+    number_of_slots = 2  # 2 addresses
 
     with patch(
         "pdr_backend.payout.payout.query_pending_payouts", mock_query_pending_payouts
     ), patch("pdr_backend.ppss.ppss.Web3PP", return_value=mock_web3_pp), patch(
         "pdr_backend.contract.token.Token", return_value=mock_token
     ), patch(
+        "pdr_backend.payout.payout.to_checksum", checksum_mock
+    ), patch(
         "pdr_backend.payout.payout.WrappedToken", return_value=mock_token
     ), patch(
-        "pdr_backend.payout.payout.FeedContract",
-        return_value=mock_feed_contract,
+        "pdr_backend.payout.payout.PredSubmitterMgr", return_value=mock_contract
     ):
         # Mock sys.argv
         sys.argv = ["pdr", "claim_OCEAN", "ppss.yaml"]
@@ -56,14 +62,12 @@ def test_ocean_payout_test(mock_wait_until_subgraph_syncs, caplog):
         assert "Starting payout" in caplog.text
         assert "Finding pending payouts" in caplog.text
         assert f"Found {number_of_slots} slots" in caplog.text
-        assert "Claiming payouts for 0x1" in caplog.text
-        assert "Claiming payouts for 0x2" in caplog.text
+        assert "Payout tx 1/2: 307831" in caplog.text
+        assert "Payout tx 1/2: 307831" in caplog.text
         assert "Payout done" in caplog.text
 
         # Additional assertions
-        mock_query_pending_payouts.assert_called_with(
-            mock_web3_pp.subgraph_url, mock_web3_config.owner
-        )
-        mock_feed_contract.payout_multiple.assert_any_call([1, 2, 3], True)
-        mock_feed_contract.payout_multiple.assert_any_call([5, 6, 7], True)
-        assert mock_feed_contract.payout_multiple.call_count == 4
+        mock_query_pending_payouts.assert_called_with(mock_web3_pp.subgraph_url, "0x1")
+        mock_contract.get_payout.assert_any_call([1, 2, 3], ["0x1"])
+        mock_contract.get_payout.assert_any_call([5, 6, 7], ["0x2"])
+        assert mock_contract.get_payout.call_count == 2
