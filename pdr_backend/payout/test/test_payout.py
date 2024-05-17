4023ca5e3d98c4e11390f5b47c66ca6353c832b9
trizin <25263018+trizin@users.noreply.github.com>
2024-05-17 14:23:07 +0300

    Add spec

--- pdr_backend/payout/test/test_payout.py
+++ pdr_backend/payout/test/test_payout.py
@@ -83,7 +83,7 @@ def test_do_rose_payout(tmpdir):
     ppss = _ppss(tmpdir)
     web3_config = ppss.web3_pp.web3_config
 
-    mock_contract = Mock()
+    mock_contract = Mock(spec=PredSubmitterMgr)
     mock_contract.get_claimable_rewards = Mock()
     mock_contract.get_claimable_rewards.return_value = Eth(100)
     mock_contract.claim_dfrewards = Mock()
