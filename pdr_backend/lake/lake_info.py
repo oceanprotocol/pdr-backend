1320b2d811b1d6b74a1959dea75c8c26b8bbb6ed
idiom-bytes <idiom.bytes@gmail.com>
2024-05-16 22:59:27 -0700

    fixing linter, etc...

--- pdr_backend/lake/lake_info.py
+++ pdr_backend/lake/lake_info.py
@@ -1,4 +1,5 @@
 from typing import Dict, List
+from datetime import datetime
 
 import polars as pl
 from polars.dataframe.frame import DataFrame
@@ -32,7 +33,9 @@ class LakeInfo:
                 "SELECT * FROM {}".format(view_name)
             )
 
-    def print_table_info(self, source: Dict[str, DataFrame]):
+    def print_table_info(self, source_name: str, source: Dict[str, DataFrame]):
+        table_summary = pl.DataFrame()
+
         for table_name in source:
             print("-" * 80)
             print("Columns for table {}:".format(table_name), end=" ")
@@ -49,28 +52,74 @@ class LakeInfo:
             if has_timestamp:
                 min_timestamp = source[table_name]["timestamp"].min()
                 max_timestamp = source[table_name]["timestamp"].max()
-                
+
+                if isinstance(min_timestamp, (int, float)):
+                    min_datestr = datetime.fromtimestamp(
+                        min_timestamp / 1000.0
+                    ).strftime("%d-%m-%Y %H:%M:%S")
+                else:
+                    min_datestr = None
+
+                if isinstance(max_timestamp, (int, float)):
+                    max_datestr = datetime.fromtimestamp(
+                        max_timestamp / 1000.0
+                    ).strftime("%d-%m-%Y %H:%M:%S")
+                else:
+                    max_datestr = None
+
+                n_rows = len(source[table_name])
+
                 if min_timestamp is not None:
                     print("Min timestamp: " + str(min_timestamp))
                 if max_timestamp is not None:
                     print("Max timestamp: " + str(max_timestamp))
 
+                if min_datestr is not None:
+                    print("Min datestr: " + str(min_datestr))
+                if max_datestr is not None:
+                    print("Max datestr: " + str(max_datestr))
+
+                if n_rows > 0:
+                    print("Number of rows: {}".format(n_rows))
+
+                table_info = pl.DataFrame(
+                    {
+                        "table_name": [table_name],
+                        "n_rows": [n_rows],
+                        "min_timestamp": [min_timestamp],
+                        "max_timestamp": [max_timestamp],
+                        "min_datestr": [min_datestr],
+                        "max_datestr": [max_datestr],
+                    }
+                )
+                table_summary = pl.concat([table_summary, table_info])
             else:
                 print("No timestamp data")
 
-            shape = source[table_name].shape
-            print(f"Number of rows: {shape[0]}")
-
-            print("Preview: \n")
+            print("Preview")
             print(source[table_name])
+
+        with pl.Config(tbl_rows=100):
+            print("=" * 80)
+
+            print("Summary - {}".format(source_name))
+            print(table_summary)
 
     def run(self):
         self.generate()
-        print("Lake Tables:")
-        print(self.all_table_names)
-        self.print_table_info(self.table_info)
+
+        if len(self.all_table_names) == 0:
+            print("No Lake Tables")
+        else:
+            print("Lake Tables:")
+            print(self.all_table_names)
+            self.print_table_info("tables", self.table_info)
 
         print("=" * 80)
-        print("Lake Views:")
-        print(self.all_view_names)
-        self.print_table_info(self.view_info)
+
+        if len(self.all_view_names) == 0:
+            print("Great - No views in Lake.")
+        else:
+            print("Lake Views:")
+            print(self.all_view_names)
+            self.print_table_info("views", self.view_info)
