834176d7b12741b57b17712cd8abe8bd9fa05226
trizin <25263018+trizin@users.noreply.github.com>
2024-05-23 12:36:28 +0300

    Feature #1061 - Set minimum slot threshold for payouts (#1067)

--- pdr_backend/ppss/predictoor_ss.py
+++ pdr_backend/ppss/predictoor_ss.py
@@ -90,6 +90,10 @@ class PredictoorSS(StrMixin):
     def pred_submitter_mgr(self) -> str:
         return self.d["bot_only"]["pred_submitter_mgr"]
 
+    @property
+    def min_payout_slots(self) -> int:
+        return self.d["bot_only"].get("min_payout_slots", 0)
+
     # --------------------------------
     # setters (add as needed)
     @enforce_types
