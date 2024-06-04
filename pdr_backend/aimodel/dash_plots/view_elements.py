38bacd274693bd838a3c754abfd4371da60585fd
Norbert <37236152+KatunaNorbert@users.noreply.github.com>
2024-06-04 12:36:11 +0300

    ARIMA-style feed analysis app (#1078)
    * create dash app scheleton for arima plots
    
    * display autocorrelations plots
    
    * displayed seasonal decomp
    
    * handle chart bar selection event
    
    * integrated input elements and read folder name from ppss
    
    * update charts when clicking on transition
    
    * filter close data and update charts based on date window selection
    
    * display message if ohlcv data is not fetched
    
    * write test for dash ui elements and tweaks
    
    * added tooltips
    
    * add new selectable bars to the transition figure
    
    * add dash bootstrap components to setup dependencies
    
    * moved command to power tools section of the helper

---
+++ pdr_backend/aimodel/dash_plots/view_elements.py
@@ -0,0 +1,154 @@
+import dash_bootstrap_components as dbc
+from dash import dcc, html
+
+
+def display_plots_view(columns):
+    return html.Div(
+        columns,
+        id="plots_container",
+        style={
+            "display": "flex",
+            "flexWrap": "wrap",
+            "justifyContent": "space-around",
+        },
+    )
+
+
+# pylint: disable=line-too-long
+def get_column_graphs(figures: list[dict], title: str, tooltip: str):
+    height_percentage = 80 / (len(figures) if (title != "ADF") else 2)
+    return (
+        html.Div(
+            [
+                html.Span(
+                    children=title + " ⓘ",
+                    id=figures[0]["graph_id"] + "-title",
+                    style={
+                        "textAlign": "center",
+                        "fontSize": "18px",
+                        "margin": "auto",
+                        "cursour": "pointer",
+                    },
+                ),
+                dbc.Tooltip(
+                    dcc.Markdown(tooltip),
+                    target=figures[0]["graph_id"] + "-title",
+                ),
+                *[
+                    dcc.Graph(
+                        figure=fig["fig"],
+                        id=fig["graph_id"],
+                        style={
+                            "width": "100%",
+                            "height": str(fig.get("height", height_percentage)) + "vh",
+                            "padding": "0",
+                        },
+                    )
+                    for fig in figures
+                ],
+            ],
+            style={
+                "display": "flex",
+                "flexDirection": "column",
+                "alignItems": "center",
+                "justifyContent": "center",
+            },
+        ),
+    )
+
+
+def display_on_column_graphs(div_id: str, width: str = "30%"):
+    return html.Div(
+        id=div_id,
+        style={
+            "display": "flex",
+            "flexDirection": "column",
+            "height": "100%",
+            "width": width,
+        },
+    )
+
+
+def get_input_elements():
+    elements = html.Div(
+        [
+            html.Div(
+                [
+                    dcc.Dropdown(
+                        id="feed-dropdown",
+                        options=[],
+                        value="",
+                        style={
+                            "width": "300px",
+                            "fontSize": "22px",
+                            "marginRight": "20px",
+                            "height": "100%",
+                        },
+                    ),
+                    # Date input components
+                    dcc.DatePickerRange(
+                        id="date-picker-range",
+                        start_date=None,
+                        end_date=None,
+                        min_date_allowed=None,
+                        max_date_allowed=None,
+                    ),
+                ],
+                style={
+                    "height": "100%",
+                    "width": "100%",
+                    "display": "flex",
+                    "justifyContent": "flex-start",
+                    "alignItems": "center",
+                    "padding": "10px",
+                },
+            ),
+            html.Div(
+                [
+                    html.Label("Lag", style={"fontSize": "20px"}),
+                    dcc.Input(
+                        id="autocorelation-lag",
+                        type="number",
+                        value="10",
+                        style={
+                            "width": "100px",
+                            "fontSize": "22px",
+                            "marginLeft": "20px",
+                            "height": "100%",
+                        },
+                    ),
+                ],
+                style={
+                    "display": "flex",
+                    "justifyContent": "flex-start",
+                    "alignItems": "center",
+                },
+            ),
+        ],
+        style={
+            "height": "100%",
+            "margin": "auto",
+            "display": "flex",
+            "justifyContent": "space-between",
+            "alignItems": "center",
+            "padding": "10px",
+        },
+    )
+
+    return elements
+
+
+def get_graphs_container():
+    return html.Div(
+        [
+            display_on_column_graphs("transition_column", "22.5%"),
+            display_on_column_graphs("seasonal_column", "50%"),
+            display_on_column_graphs("autocorelation_column", "22.5%"),
+        ],
+        id="arima-graphs",
+        style={
+            "width": "100%",
+            "display": "flex",
+            "justifyContent": "space-between",
+        },
+    )
---
+++ pdr_backend/aimodel/plots_dash.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+import webbrowser
+
+import dash_bootstrap_components as dbc
+from dash import Dash, dcc, html
+from enforce_typing import enforce_types
+
+from pdr_backend.aimodel.dash_plots.callbacks import get_callbacks
+from pdr_backend.aimodel.dash_plots.view_elements import (
+    get_graphs_container,
+    get_input_elements,
+)
+from pdr_backend.ppss.ppss import PPSS
+
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+app.config["suppress_callback_exceptions"] = True
+app.layout = html.Div(
+    [
+        dcc.Store(id="data-folder"),
+        dcc.Store(id="file-data"),
+        dcc.Store(id="window-data"),
+        dcc.Store(id="transition-data"),
+        html.H1(
+            "ARIMA-style feed analysis",
+            id="page_title",
+            style={"width": "100%", "textAlign": "center"},
+        ),
+        html.Div(id="input-elements", children=get_input_elements()),
+        html.Div(id="error-message"),
+        dcc.Loading(
+            id="loading",
+            type="default",
+            children=get_graphs_container(),
+            style={"height": "100%"},
+        ),
+    ]
+)
+
+get_callbacks(app)
+
+
+@enforce_types
+def arima_dash(ppss: PPSS):
+    port = 8050
+    webbrowser.open(f"http://127.0.0.1:{port}/")
+    folder = ppss.lake_ss.parquet_dir
+    app.layout.children[0].data = folder
+    app.run(debug=True, port=port)
