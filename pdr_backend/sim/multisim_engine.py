2a29841020252b7722cdb0d1b7c6dcaed22cbd29
trizin <25263018+trizin@users.noreply.github.com>
2024-06-13 16:03:57 +0300

    Disable state updates in multisim

--- pdr_backend/sim/multisim_engine.py
+++ pdr_backend/sim/multisim_engine.py
@@ -69,6 +69,7 @@ class MultisimEngine:
         feedset = ppss.predictoor_ss.predict_train_feedsets[0]
         multi_id = str(uuid.uuid4())
         sim_engine = SimEngine(ppss, feedset, multi_id)
+        sim_engine.disable_realtime_state()
         sim_engine.run()
         st = sim_engine.st
         recent_metrics = st.recent_metrics()
