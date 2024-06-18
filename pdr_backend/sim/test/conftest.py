51ea29e947f3eccab6f2e3c27da6bdfe64fab5fa
Norbert <katunanorbert@gmail.com>
2024-06-18 17:36:59 +0300

    added step by step commands for mac instalation

--- pdr_backend/sim/test/conftest.py
+++ pdr_backend/sim/test/conftest.py
@@ -25,7 +25,11 @@ def check_chromedriver():
             It seems that you don't have chromedriver installed on your system.
             Please install it by following the instructions at https://sites.google.com/a/chromium.org/chromedriver/.
             For ubuntu, you can install it using the following command: sudo apt-get install chromium-chromedriver.
-            For Mac, you can install it using the following command: brew cask install chromedriver.
+            For Mac:
+                - you can install it using the following command: brew install chromedriver.
+                - go to chromedriver location and move it to /usr/local/bin/ using this command: sudo mv chromedriver /usr/local/bin/
+                - change the file access mode to add execution permision using this command: sudo chmod +x /usr/local/bin/chromedriver
+                - if a popup appears when running the tests, click on open and test should pass from now on
             For Windows, you can download the chromedriver from the link above and add it to your PATH.
             If you have chromedriver installed, make sure it is in your PATH.
         """
