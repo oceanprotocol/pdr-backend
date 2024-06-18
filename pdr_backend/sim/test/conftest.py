ba5bb0b772a57bfb9f782cbdab5906f165dc1bc8
Calina Cenan <calina@cenan.net>
2024-06-18 14:50:44 +0100

    Add check and instructions for chrome webdriver failure.

--- pdr_backend/sim/test/conftest.py
+++ pdr_backend/sim/test/conftest.py
@@ -1,4 +1,11 @@
+import logging
+import pytest
+
+from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import WebDriverException
+
+logger = logging.getLogger("dependencies")
 
 
 def pytest_setup_options():
@@ -6,3 +13,21 @@ def pytest_setup_options():
     options.add_argument("--headless")
     options.add_argument("--disable-gpu")
     return options
+
+
+@pytest.fixture
+def check_chromedriver():
+    try:
+        driver = webdriver.Chrome()
+        driver.quit()
+    except WebDriverException:
+        message = """
+            It seems that you don't have chromedriver installed on your system.
+            Please install it by following the instructions at https://sites.google.com/a/chromium.org/chromedriver/.
+            For ubuntu, you can install it using the following command: sudo apt-get install chromium-chromedriver.
+            For Mac, you can install it using the following command: brew cask install chromedriver.
+            For Windows, you can download the chromedriver from the link above and add it to your PATH.
+            If you have chromedriver installed, make sure it is in your PATH.
+        """
+
+        logger.error(message)
