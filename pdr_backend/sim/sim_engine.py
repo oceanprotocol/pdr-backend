4308292bbf5237c57af320e97629a8e2b0c16351
trizin <25263018+trizin@users.noreply.github.com>
2024-06-21 15:48:31 +0300

    get_highlow

--- pdr_backend/sim/sim_engine.py
+++ pdr_backend/sim/sim_engine.py
@@ -117,7 +117,7 @@ class SimEngine:
         X_train, X_test = X[st_:fin, :], X[fin : fin + 1, :]
         ytran_train, _ = ytran[st_:fin], ytran[fin : fin + 1]
 
-        high_value, low_value, _ = data_f.get_highlowclose(
+        high_value, low_value, _ = data_f.get_highlow(
             mergedohlcv_df, predict_feed, testshift
         )
 
