f987cdd6b160c4cba05a243b05e6539cca265124
Călina Cenan <calina@cenan.net>
2024-07-10 11:17:36 +0300

    Update plots.py

--- pdr_backend/analytics/predictoor_dashboard/dash_components/plots.py
+++ pdr_backend/analytics/predictoor_dashboard/dash_components/plots.py
@@ -6,35 +6,6 @@ from pdr_backend.util.time_types import UnixTimeS
 
 
 @enforce_types
-def create_scatter(
-    name: str, x_data: List[float], y_data: List[int], mode="lines"
-) -> go.Scatter:
-    """Create a scatter plot with the given data.
-    Args:
-        name (str): Name of the trace.
-        x_data (List): List of x values.
-        y_data (List): List of y values.
-        mode (str): Plot mode. Default is 'lines'.
-    Returns:
-        go.Scatter: Scatter plot trace.
-    """
-    return go.Scatter(x=x_data, y=y_data, mode=mode, name=name)
-
-
-@enforce_types
-def create_bar(name: str, x_data: List[float], y_data: List[int]) -> go.Bar:
-    """Create a bar plot with the given data.
-    Args:
-        name (str): Name of the trace.
-        x_data (list): List of x values.
-        y_data (list): List of y values.
-    Returns:
-        go.Bar: Bar plot trace.
-    """
-    return go.Bar(x=x_data, y=y_data, name=name, width=5)
-
-
-@enforce_types
 def process_payouts(payouts: List[dict], predictor: str, feed: str) -> tuple:
     """
     Process payouts data for a given predictor and feed.
@@ -130,22 +101,24 @@ def get_figures(
             slots, accuracies, profits, stakes = process_payouts(
                 payouts, predictor, feed["contract"]
             )
-            if slots:
-                short_name = f"{predictor[:5]} - {feed['feed_name']}"
-                accuracy_scatters.append(
-                    go.Scatter(x=slots, y=accuracies, mode="lines", name=short_name)
-                )
-                profit_scatters.append(
-                    go.Scatter(x=slots, y=profits, mode="lines", name=short_name)
-                )
-                stakes_scatters.append(create_bar(short_name, slots, stakes))
+            if not slots:
+                continue
+                
+            short_name = f"{predictor[:5]} - {feed['feed_name']}"
+            accuracy_scatters.append(
+                go.Scatter(x=slots, y=accuracies, mode="lines", name=short_name)
+            )
+            profit_scatters.append(
+                go.Scatter(x=slots, y=profits, mode="lines", name=short_name)
+            )
+            stakes_scatters.append(go.Bar(x=slots, y=stakes, name=short_name, width=5))
 
     if not accuracy_scatters:
         accuracy_scatters.append(go.Scatter(x=[], y=[], mode="lines", name="accuracy"))
     if not profit_scatters:
         profit_scatters.append(go.Scatter(x=[], y=[], mode="lines", name="profit"))
     if not stakes_scatters:
-        stakes_scatters.append(create_bar("stakes", [], []))
+        stakes_scatters.append(go.Bar(x=[], y=[], name="stakes", width=5))
 
     fig_accuracy = create_figure(
         accuracy_scatters, "Accuracy", "'%' accuracy over time"
