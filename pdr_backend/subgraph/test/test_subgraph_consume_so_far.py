daf81a42dbcc1339f2ec5e6645e7b6a1f90c550a
trizin <25263018+trizin@users.noreply.github.com>
2024-07-25 14:24:46 +0300

    update SAMPLE_CONTRACT_DATA

--- pdr_backend/subgraph/test/test_subgraph_consume_so_far.py
+++ pdr_backend/subgraph/test/test_subgraph_consume_so_far.py
@@ -11,47 +11,25 @@ from pdr_backend.subgraph.info725 import key_to_key725, value_to_value725
 from pdr_backend.subgraph.subgraph_consume_so_far import get_consume_so_far_per_contract
 from pdr_backend.util.time_types import UnixTimeS
 
+
+
 SAMPLE_CONTRACT_DATA = [
-    {
-        "id": "contract1",
-        "token": {
-            "id": "token1",
-            "name": "ether",
-            "symbol": "ETH",
-            "orders": [
-                {
-                    "createdTimestamp": 1695288424,
-                    "consumer": {"id": "0xff8dcdfc0a76e031c72039b7b1cd698f8da81a0a"},
-                    "lastPriceValue": "2.4979184013322233",
-                },
-                {
-                    "createdTimestamp": 1695288724,
-                    "consumer": {"id": "0xff8dcdfc0a76e031c72039b7b1cd698f8da81a0a"},
-                    "lastPriceValue": "2.4979184013322233",
-                },
-            ],
-            "nft": {
-                "owner": {"id": "0xowner1"},
-                "nftData": [
-                    {
-                        "key": key_to_key725("pair"),
-                        "value": value_to_value725("ETH/USDT"),
-                    },
-                    {
-                        "key": key_to_key725("timeframe"),
-                        "value": value_to_value725("5m"),
-                    },
-                    {
-                        "key": key_to_key725("source"),
-                        "value": value_to_value725("binance"),
-                    },
-                ],
-            },
+     {
+        "user": {
+          "id": "0xff8dcdfc0a76e031c72039b7b1cd698f8da81a0a"
         },
-        "secondsPerEpoch": 7,
-        "secondsPerSubscription": 700,
-        "truevalSubmitTimeout": 5,
-    }
+        "predictContract": {
+          "id": "contract1"
+        }
+      },
+      {
+        "user": {
+          "id": "0xff8dcdfc0a76e031c72039b7b1cd698f8da81a0a"
+        },
+        "predictContract": {
+          "id": "contract1"
+        }
+      }
 ]
 
 
