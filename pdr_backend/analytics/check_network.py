5da923e2298422e2db5aaa4add38d2bb38544844
trizin <25263018+trizin@users.noreply.github.com>
2024-07-26 13:49:44 +0300

    Make token_amt float

--- pdr_backend/analytics/check_network.py
+++ pdr_backend/analytics/check_network.py
@@ -211,7 +211,7 @@ def check_network_main(ppss: PPSS, lookback_hours: int):
     # ---------------- dfbuyer ----------------
 
     dfbuyer_addr = addresses["dfbuyer"].lower()
-    token_amt = 44940
+    token_amt = 44940.0
 
     check_dfbuyer(dfbuyer_addr, result, web3_pp.subgraph_url, token_amt)
 
