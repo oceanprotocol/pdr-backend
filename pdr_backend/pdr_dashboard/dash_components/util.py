c5ddfedd0588eb9e9ea6447d3533882c16c1ad54
Călina Cenan <calina@cenan.net>
2024-07-31 11:32:18 +0300

    [Fix #1481] PdrDashboard v0.4 Improve multipage DRY (#1482)
    * Add get callbacks calls to app setup.
    * Objectify pages and move concerns.
    * Add app prices separately.

--- pdr_backend/pdr_dashboard/dash_components/util.py
+++ pdr_backend/pdr_dashboard/dash_components/util.py
@@ -6,10 +6,12 @@ from enforce_typing import enforce_types
 import dash
 from web3 import Web3
 
+from pdr_backend.exchange.fetch_ohlcv import fetch_ohlcv
 from pdr_backend.lake.duckdb_data_store import DuckDBDataStore
 from pdr_backend.lake.payout import Payout
 from pdr_backend.lake.prediction import Prediction
 from pdr_backend.util.currency_types import Eth, Wei
+from pdr_backend.util.time_types import UnixTimeMs
 
 logger = logging.getLogger("predictoor_dashboard_utils")
 
@@ -245,18 +247,23 @@ def calculate_tx_gas_fee_cost_in_OCEAN(web3_pp, feed_contract_addr, prices):
     return tx_fee_price_ocean_prediction
 
 
-def get_feeds_for_favourite_predictoors(app, feed_data):
-    if app.favourite_addresses:
-        feed_ids = get_feed_ids_based_on_predictoors_from_db(
-            app.lake_dir,
-            app.favourite_addresses,
-        )
+def _get_from_exchange(exchange, current_date_ms):
+    rose_usdt = fetch_ohlcv(exchange, "ROSE/USDT", "5m", current_date_ms, 1)
+    fet_usdt = fetch_ohlcv(exchange, "FET/USDT", "5m", current_date_ms, 1)
 
-        if feed_ids:
-            feed_data = [
-                feed for feed in app.feeds_data if feed["contract"] in feed_ids
-            ]
+    return rose_usdt, fet_usdt
 
-        return list(range(len(feed_ids))), feed_data
 
-    return [], feed_data
+def fetch_token_prices() -> Optional[Dict[str, float]]:
+    current_date_ms = UnixTimeMs(int(datetime.now().timestamp()) * 1000 - 300000)
+    rose_usdt, fet_usdt = _get_from_exchange("binance", current_date_ms)
+
+    if rose_usdt and fet_usdt:
+        return {"ROSE": rose_usdt[0][1], "OCEAN": fet_usdt[0][1] * 0.433226}
+
+    rose_usdt, fet_usdt = _get_from_exchange("binanceus", current_date_ms)
+
+    if rose_usdt and fet_usdt:
+        return {"ROSE": rose_usdt[0][1], "OCEAN": fet_usdt[0][1] * 0.433226}
+
+    return None
