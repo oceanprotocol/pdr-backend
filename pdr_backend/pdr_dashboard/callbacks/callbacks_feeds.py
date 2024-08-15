9d32b54f3063a163c6a4cf9aee5ce3e5275ce049
Norbert <katunanorbert@gmail.com>
2024-08-15 16:08:07 +0300

    fix selected row not cleared when modal is closed

--- pdr_backend/pdr_dashboard/callbacks/callbacks_feeds.py
+++ pdr_backend/pdr_dashboard/callbacks/callbacks_feeds.py
@@ -201,8 +201,7 @@ def get_callbacks_feeds(app):
     @app.callback(
         Output("modal", "is_open"),
         Output("feeds_page_table", "selected_rows"),
-        Input("feeds_page_table", "selected_rows"),
-        State("modal", "is_open"),
+        [Input("feeds_page_table", "selected_rows"), Input("modal", "is_open")],
     )
     def toggle_modal(selected_rows, is_open_input):
         ctx = dash.callback_context
