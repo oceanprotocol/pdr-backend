e2daa54904b25079f537777a1ca2af34e04df73b
AmandaZYY <zyamanda01@gmail.com>
2024-08-20 14:42:35 +0100

    add ArgThreshold class for tick,vol and dollar

---
+++ pdr_backend/cli/arg_threshold.py
@@ -0,0 +1,96 @@
+from typing import List, Union
+
+from enforce_typing import enforce_types
+
+class ArgThreshold:
+    def __init__(self, threshold_str: str):
+        """
+        @arguments
+          threshold_str -- e.g. "vb_2100" "db_2090.5" "tb_208"
+        """
+        prefix, value_str = _unpack_threshold_str(threshold_str)
+        
+        if prefix not in ['vb', 'db', 'tb']:
+            raise ValueError('threshold should start with vb, db or tb')
+        
+        try:
+            float(value_str)
+        except ValueError:
+            print(f"Invalid float value: '{value_str}'")
+
+        self.prefix = prefix  # 'db', 'vb', 'tb'
+        self.value = value_str
+
+    def __eq__(self, other):
+        return str(self) == str(other)
+
+    def __str__(self):
+        return self.prefix+'_'+self.value
+
+    def threshold(self) -> float:
+        return float(self.value)
+
+
+class ArgThresholds(List[ArgThreshold]):
+    def __init__(self, thresholds: Union[List[str], List[ArgThreshold]]):
+        if not isinstance(thresholds, list):
+            raise TypeError("hresholds must be a list")
+
+        thres = []
+        for thre in thresholds:
+            thre = ArgThreshold(thre) if isinstance(thre, str) else thre
+            thres.append(thre)
+
+        super().__init__(thres)
+
+    @staticmethod
+    def from_str(thresholds_str: str):
+        """
+        @description
+          Parses a comma-separated string of thresholds, e.g. "vb-105.3,vb-200"
+        """
+        if not isinstance(thresholds_str, str):
+            raise TypeError("thresholds_str must be a string")
+
+        return ArgThresholds(thresholds_str.split(","))
+
+    def __str__(self):
+        if not self:
+            return ""
+
+        return ",".join([str(th) for th in self])
+
+
+@enforce_types
+def threshold_str_ok(s: str) -> bool:
+    try:
+        ArgThresholds.from_str(s)
+        return True
+    except ValueError:
+        return False
+
+
+@enforce_types
+def verify_threshold_str(s: str):
+    """Raise an error if input string is invalid."""
+    _ = ArgThreshold(s)
+
+
+@enforce_types
+def verify_thresholds_str(s: str):
+    """Raise an error if input string is invalid."""
+    _ = ArgThresholds.from_str(s)
+
+
+def _unpack_threshold_str(th_str: str) -> tuple:
+    """
+    Unpacks the vb_str into prefix and value_str.
+
+    Example: Given 'vb_2100.5'
+    Return ('vb', '2100.5')
+    """
+    res = th_str.split("_")
+    if len(res) != 2:
+        raise ValueError('wrong format for threshold')
+    prefix, value_str = res
+    return (prefix, value_str)
---
+++ pdr_backend/cli/test/test_arg_threshold.py
@@ -0,0 +1,67 @@
+#
+# Copyright 2024 Ocean Protocol Foundation
+# SPDX-License-Identifier: Apache-2.0
+#
+import pytest
+from enforce_typing import enforce_types
+
+from pdr_backend.cli.arg_threshold import (
+    threshold_str_ok,
+    verify_threshold_str,
+    verify_thresholds_str,
+)
+
+
+# ==========================================================================
+# conversions
+@enforce_types
+def test_verify_threshold_str():
+    verify_threshold_str("tb_210")
+    verify_threshold_str("db_210.5")
+    verify_threshold_str("vb_210")
+
+    for bad_val in ["", " ", "foo", "vb", "tbab", "tb-210.1", "210", "pb_2015"]:
+        with pytest.raises(ValueError):
+            verify_threshold_str(bad_val)
+
+    for bad_val in [None, 1.1, [], ["tb210"]]:
+        with pytest.raises(TypeError):
+            verify_threshold_str(bad_val)
+
+
+@enforce_types
+def test_verify_thresholds_str():
+    verify_thresholds_str("tb_210")
+    verify_thresholds_str("tb_210.5,tb_214")
+
+    for bad_val in [
+        "",
+        " ",
+        "foo",
+        "tb-",
+        "tb-ab",
+        "tb210",
+        "210",
+        "tb-2",
+        "tb_12,",
+        "tb_12.5",
+    ]:
+        with pytest.raises(ValueError):
+            verify_thresholds_str(bad_val)
+
+    for bad_val in [None, 1.1, [], ["vb210"]]:
+        with pytest.raises(TypeError):
+            verify_thresholds_str(bad_val)
+
+
+@enforce_types
+def test_tb_str_ok():
+    assert threshold_str_ok("tb_210")
+    threshold_str_ok("tb_210,tb_210")
+
+    for bad_val in ["", " ", "foo", "tb-", "tb_ab", "tb210", "210", "tb-2"]:
+        assert not threshold_str_ok(bad_val)
+
+    for bad_val in [None, 1.1, [], ["tb210"]]:
+        with pytest.raises(TypeError):
+            threshold_str_ok(bad_val)
