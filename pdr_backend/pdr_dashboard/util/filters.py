7532b1611787dbbf6211f78ccbec0420bfc254f3
Călina Cenan <calina@cenan.net>
2024-08-21 08:12:34 +0300

    Allow for search with natural values in table. (#1539)

--- pdr_backend/pdr_dashboard/util/filters.py
+++ pdr_backend/pdr_dashboard/util/filters.py
@@ -36,6 +36,165 @@ def table_search_condition(item, search_value):
     )
 
 
+def _float_repr(value):
+    try:
+        return float(value)
+    except ValueError:
+        pass
+
+    denumerized = _denumerize(value)
+    if denumerized != "err":
+        return float(denumerized)
+
+    return value
+
+
+# copied directly from numerize.py, unreleased in a package
+def _denumerize(n):
+    try:
+        sufixes = [
+            "",
+            "K",
+            "M",
+            "B",
+            "T",
+            "Qa",
+            "Qu",
+            "S",
+            "Oc",
+            "No",
+            "D",
+            "Ud",
+            "Dd",
+            "Td",
+            "Qt",
+            "Qi",
+            "Se",
+            "Od",
+            "Nd",
+            "V",
+            "Uv",
+            "Dv",
+            "Tv",
+            "Qv",
+            "Qx",
+            "Sx",
+            "Ox",
+            "Nx",
+            "Tn",
+            "Qa",
+            "Qu",
+            "S",
+            "Oc",
+            "No",
+            "D",
+            "Ud",
+            "Dd",
+            "Td",
+            "Qt",
+            "Qi",
+            "Se",
+            "Od",
+            "Nd",
+            "V",
+            "Uv",
+            "Dv",
+            "Tv",
+            "Qv",
+            "Qx",
+            "Sx",
+            "Ox",
+            "Nx",
+            "Tn",
+            "x",
+            "xx",
+            "xxx",
+            "X",
+            "XX",
+            "XXX",
+            "END",
+        ]
+
+        sci_expr = [
+            1e0,
+            1e3,
+            1e6,
+            1e9,
+            1e12,
+            1e15,
+            1e18,
+            1e21,
+            1e24,
+            1e27,
+            1e30,
+            1e33,
+            1e36,
+            1e39,
+            1e42,
+            1e45,
+            1e48,
+            1e51,
+            1e54,
+            1e57,
+            1e60,
+            1e63,
+            1e66,
+            1e69,
+            1e72,
+            1e75,
+            1e78,
+            1e81,
+            1e84,
+            1e87,
+            1e90,
+            1e93,
+            1e96,
+            1e99,
+            1e102,
+            1e105,
+            1e108,
+            1e111,
+            1e114,
+            1e117,
+            1e120,
+            1e123,
+            1e126,
+            1e129,
+            1e132,
+            1e135,
+            1e138,
+            1e141,
+            1e144,
+            1e147,
+            1e150,
+            1e153,
+            1e156,
+            1e159,
+            1e162,
+            1e165,
+            1e168,
+            1e171,
+            1e174,
+            1e177,
+        ]
+
+        suffix_idx = 0
+
+        for i in n:
+            if not isinstance(i, int):
+                suffix_idx = n.index(i)
+
+        suffix = n[suffix_idx:]
+        num = n[:suffix_idx]
+
+        suffix_val = sci_expr[sufixes.index(suffix)]
+        result = int(num) * suffix_val
+
+        return result
+    except:  # pylint: disable=bare-except
+        return "err"
+
+
 @enforce_types
 def table_column_range_condition(
     item, field, min_value: Optional[str], max_value: Optional[str]
@@ -44,10 +203,10 @@ def table_column_range_condition(
     min_value = min_value or ""
     max_value = max_value or ""
 
-    if min_value and item_value < float(min_value):
+    if min_value and item_value < _float_repr(min_value):
         return False
 
-    if max_value and item_value > float(max_value):
+    if max_value and item_value > _float_repr(max_value):
         return False
 
     return True
