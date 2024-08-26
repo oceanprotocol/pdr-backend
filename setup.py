c56504674b57a0a615dbd7ceb0c99a20583a2a86
Călina Cenan <calina@cenan.net>
2024-08-26 07:47:28 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,6 @@ install_requirements = [
     "dateparser==1.2.0",
     "duckdb==1.0.0",
     "enforce_typing",
-    "eth-account==0.11.0",
     "eth-keys==0.5.1",
     "flask==3.0.3",
     "imblearn",
