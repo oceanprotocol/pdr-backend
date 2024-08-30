58404bcbd21f2fde005665051af378af2b8cf8d8
Mustafa Tunçay <mustafaislev@gmail.com>
2024-08-30 15:17:04 +0300

    Fix #1518: Use lake data fixture instead of mocked data (#1563)
    * issue-1518: sample data fixes
    
    * issue-1518: config fix
    
    * issue-1518: checkbox selection test fix try
    
    * issue-1518: fix the checkbox test
    
    * Fixes feeds filters test.
    
    * Bypass fetch token prices with mock in test resources.
    
    * TEST: add both window size options.
    
    * issue-1518: checkbox fix try
    
    * issue-1518: Update conftest.py for checkbox fix
    
    * issue-1518: Update resources.py for checkbox fix
    
    ---------
    
    Co-authored-by: Calina Cenan <calina@cenan.net>

--- pdr_backend/pdr_dashboard/test/test_callbacks_home.py
+++ pdr_backend/pdr_dashboard/test/test_callbacks_home.py
@@ -1,44 +1,47 @@
 import time
 
+
 from pdr_backend.pdr_dashboard.test.resources import (
     _input_action,
     start_server_and_wait,
 )
 
 
-def test_get_input_data_from_db(setup_app, dash_duo):
-    app = setup_app
+def test_get_input_data_from_db(_sample_app, dash_duo):
+    app = _sample_app
     start_server_and_wait(dash_duo, app)
 
 
-def test_feeds_search_input(setup_app, dash_duo):
+def test_feeds_search_input(_sample_app, dash_duo):
     """
     Test the search input in the "Feeds" table.
     The search input is used to filter the feeds by their name.
     """
 
-    app = setup_app
+    app = _sample_app
     start_server_and_wait(dash_duo, app)
+
     _input_action(dash_duo, "#search-input-Feeds", "#feeds_table", "OCEAN", 1)
-    _input_action(dash_duo, "#search-input-Feeds", "#feeds_table", "BTC", 2)
-    _input_action(dash_duo, "#search-input-Feeds", "#feeds_table", "ADA", 2)
+    _input_action(dash_duo, "#search-input-Feeds", "#feeds_table", "BTC", 3)
+    _input_action(dash_duo, "#search-input-Feeds", "#feeds_table", "ADA", 3)
 
 
 # pylint: disable=too-many-statements
-def test_predictoors_search_input(setup_app, dash_duo):
+def test_predictoors_search_input(_sample_app, dash_duo):
     """
     Test the search input in the "Predictoors" table.
     The search input is used to filter the predictoors by their name.
     """
 
-    app = setup_app
+    app = _sample_app
     start_server_and_wait(dash_duo, app)
 
     _input_action(
         dash_duo, "#search-input-Predictoors", "#predictoors_table", "0xaaa", 1
     )
+
     _input_action(
-        dash_duo, "#search-input-Predictoors", "#predictoors_table", "0xd2", 2
+        dash_duo, "#search-input-Predictoors", "#predictoors_table", "0x768", 2
     )
 
 
@@ -58,8 +61,10 @@ def _predictoor_count(dash_duo):
     return predictoors_rows, predictoors_selected_rows
 
 
-def test_favourite_addresses_search_input(setup_app_with_favourite_addresses, dash_duo):
-    app = setup_app_with_favourite_addresses
+def test_favourite_addresses_search_input(
+    _sample_app_with_favourite_addresses, dash_duo
+):
+    app = _sample_app_with_favourite_addresses
     start_server_and_wait(dash_duo, app)
 
     fav_addr_toggle = dash_duo.find_element("#show-favourite-addresses")
@@ -68,7 +73,7 @@ def test_favourite_addresses_search_input(setup_app_with_favourite_addresses, da
     # default startup: one predictoor selected out of 6
     # and its single feed selected out of 5 total (ADA/USDT)
     p_all, p_sel = _predictoor_count(dash_duo)
-    assert len(p_all) == 6
+    assert len(p_all) == 58
     assert len(p_sel) == 1
     f_all, f_sel = _feed_count(dash_duo)
     assert len(f_all) == 1
@@ -78,33 +83,35 @@ def test_favourite_addresses_search_input(setup_app_with_favourite_addresses, da
     all_feeds_toggle.click()
     time.sleep(2)
     f_all, f_sel = _feed_count(dash_duo)
-    assert len(f_all) == 6
+    assert len(f_all) == 21
     assert len(f_sel) == 1
 
     # click on fav addr toggle to show all predictoors
     fav_addr_toggle.click()
     time.sleep(2)
     p_all, p_sel = _predictoor_count(dash_duo)
-    assert len(p_all) == 6
+    assert len(p_all) == 58
     assert len(p_sel) == 0
 
 
-def test_checkbox_selection(setup_app, dash_duo):
+def test_checkbox_selection(_sample_app, dash_duo):
     """
     Test the selection of checkboxes in the "Feeds" and "Predictoors" tables.
     """
 
-    app = setup_app
+    app = _sample_app
     start_server_and_wait(dash_duo, app)
 
+    time.sleep(2)
     # click on the checkbox in the second row of the "Feeds" table
     dash_duo.find_element("#feeds_table tbody tr:nth-child(2) input").click()
 
+    time.sleep(2)
     # click on the checkbox in the first row of the "Predictoors" table
-    dash_duo.find_element("#predictoors_table tbody tr input").click()
+    dash_duo.find_element("#predictoors_table tbody tr:nth-child(2) input").click()
 
 
-def test_timeframe_metrics(setup_app, dash_duo):
+def test_timeframe_metrics(_sample_app, dash_duo):
     """
     Test the metrics that are displayed when a predictoor is selected.
     It takes the predictoor row from the table and compares with the top metrics.
@@ -112,7 +119,7 @@ def test_timeframe_metrics(setup_app, dash_duo):
     The metrics are: Profit, Accuract, Stake
     """
 
-    app = setup_app
+    app = _sample_app
     start_server_and_wait(dash_duo, app)
 
     dash_duo.find_element("#predictoors_table tbody tr:nth-child(3) input").click()
@@ -140,13 +147,13 @@ def test_timeframe_metrics(setup_app, dash_duo):
     assert table_stake + " OCEAN" == metric_stake
 
 
-def test_predictoors_feed_only_switch(setup_app, dash_duo):
+def test_predictoors_feed_only_switch(_sample_app, dash_duo):
     """
     Test the switch that toggles between showing only the feeds that are
     associated with the selected predictoor and all feeds.
     """
 
-    app = setup_app
+    app = _sample_app
     start_server_and_wait(dash_duo, app)
 
     dash_duo.find_element("#predictoors_table tbody tr:nth-child(3) input").click()
@@ -162,11 +169,11 @@ def test_predictoors_feed_only_switch(setup_app, dash_duo):
     time.sleep(2)
 
     feeds_table_len = len(dash_duo.find_elements("#feeds_table tbody tr"))
-    assert feeds_table_len == 6
+    assert feeds_table_len == 21
 
 
-def test_navigation(setup_app, dash_duo):
-    app = setup_app
+def test_navigation(_sample_app, dash_duo):
+    app = _sample_app
     start_server_and_wait(dash_duo, app)
 
     # Default page is Home
