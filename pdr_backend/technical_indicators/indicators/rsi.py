bec5104be918d640598c865782a6baec04e4630a
trizin <25263018+trizin@users.noreply.github.com>
2024-09-03 11:47:43 +0000

    Add RSI technical indicator class

---
+++ pdr_backend/technical_indicators/indicators/rsi.py
@@ -0,0 +1,18 @@
+import ta
+from pdr_backend.technical_indicators.technical_indicator import TechnicalIndicator
+
+
+class RSI(TechnicalIndicator):
+    """
+    Relative Strength Index (RSI) technical indicator.
+    """
+
+    def calculate(self, window: int = 14):
+        """
+        Calculates the RSI value based on the input data.
+
+        @param:
+            window - The window size for the RSI calculation (default=14).
+        """
+        rsi = ta.momentum.RSIIndicator(close=self._close(), window=window).rsi()
+        return rsi
