7bf15c238ed8f074dfc72477e9fc43e5dd61fc14
trizin <25263018+trizin@users.noreply.github.com>
2024-09-03 11:47:36 +0000

    Add MACD technical indicator class

---
+++ pdr_backend/technical_indicators/indicators/macd.py
@@ -0,0 +1,21 @@
+import ta
+from pdr_backend.technical_indicators.technical_indicator import TechnicalIndicator
+
+
+class MACD(TechnicalIndicator):
+    """
+    Moving Average Convergence Divergence (MACD) technical indicator.
+    """
+
+    def calculate(self, window_fast: int = 12, window_slow: int = 26):
+        """
+        Calculates the MACD value based on the input data.
+
+        @param:
+            window_fast - The window size for the fast EMA calculation (default=12).
+            window_slow - The window size for the slow EMA calculation (default=26).
+        """
+        macd = ta.trend.MACD(
+            close=self._close(), window_fast=window_fast, window_slow=window_slow
+        )
+        return macd.macd()
